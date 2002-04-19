ad60d03ba1efa6d177b985b169102927f0e04889
Petr Kubánek <petr@kubanek.net>
2002-04-19 17:56:42 +0000

    Make clients handling procedures reentrant by passing all required parameters on-site.
    Changes header to match new change, introduce new structures.
    
    
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@50 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/devcli.h
+++ src/utils/devcli.h
@@ -16,12 +16,32 @@
 typedef int (*devcli_handle_response_t) (char *, size_t);
 typedef int (*devcli_handle_data_t) (void *, size_t, void *);
 
-int devcli_init_sockaddr (struct sockaddr_in *name, const char *hostname,
-			  uint16_t port);
+/*! 
+ * Parameters for one communication channel
+ * 
+ * Chanell is in our terminology one communication link, which goes between
+ * client and device server. It has some address, use some socket, have
+ * handler for devdem commands returns, space to hold extra parameters for
+ * such a handler, and some other usefull variables.
+ */
 
-devcli_handle_response_t devcli_set_response_handler (devcli_handle_response_t
-						      new_handler);
-int devcli_write_read (int sock, char *message, int *ret_code);
+struct devcli_channel_t
+{
+  int socket;			//! socket for connection
+  struct sockaddr *address;	//! socket address
+  devcli_handle_response_t handler;	//! handler to handle responses
+  void *response_data;		//! attribute to send to response handler
+  // following are internal variables
+  char buf[MAXMSG + 1];		//! start buffer for commands
+  char *endptr;			//! buffer end ptr
+};
+
+
+int devcli_init_channel (struct devcli_channel_t *channel,
+			 const char *hostname, uint16_t port);
+
+int devcli_write_read (struct devcli_channel_t *channel, char *message,
+		       int *ret_code);
 int devcli_read_data (struct sockaddr *server_addr, pthread_t * thread,
 		      devcli_handle_data_t handler, void *handler_arg);
 
