43cd42a71b60095c1020fc79db77c3f45b7bf8da
Petr Kubánek <petr@kubanek.net>
2002-09-24 21:40:53 +0000

    * adde new (completly different from devdem.h used before, included devser.h)
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@102 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/utils/devdem.h
@@ -0,0 +1,24 @@
+/*! 
+ * @file Device deamon header file.
+ * $Id$
+ *
+ * @author petr
+ */
+
+#ifndef __RTS_DEVDEM__
+#define __RTS_DEVDEM__
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#define DEVDEM_WITH_CLIENT
+
+#include "devcli.h"
+
+int devdem_register (struct devcli_channel *server_channel, char *device_name,
+		     char *server_address, int server_port);
+
+#include "devser.h"
+
+#endif /* !__RTS_DEVDEM__ */
