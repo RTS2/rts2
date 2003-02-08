fd13a56a15ae63af5edadfd7a983c9846c1573e5
Petr Kubánek <petr@kubanek.net>
2003-02-08 21:59:11 +0000

    * added
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@243 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ include/telescope_info.h
@@ -0,0 +1,20 @@
+#ifndef __RTS_TELESCOPE_INFO__
+#define __RTS_TELESCOPE_INFO__
+
+struct telescope_info
+{
+  char name[64];
+  char serial_number[64];
+  double ra;
+  double dec;
+  int moving;
+  double park_dec;
+  // geographic informations
+  double longtitude;
+  double latitude;
+  // time information
+  double siderealtime;
+  double localtime;
+};
+
+#endif /* __RTS_TELESCOPE_INFO__ */
