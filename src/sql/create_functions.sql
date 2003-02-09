abed4c3a50f7ed361d738e1b57995ef0747321ab
Petr Kubánek <petr@kubanek.net>
2003-02-09 20:05:40 +0000

    * float4-> float8, double used
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@246 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sql/create_functions.sql
+++ src/sql/create_functions.sql
@@ -1,21 +1,21 @@
-DROP FUNCTION obj_alt (float4, float4, float4, float4, float4);
+DROP FUNCTION obj_alt (float8, float8, float8, float8, float8);
 
 -- 			ra       dec     JD      lon     lat
-CREATE FUNCTION obj_alt (float4, float4, float4, float4, float4) 
-	RETURNS float4 AS 'pg_astrolib', 'obj_alt' LANGUAGE 'C';
+CREATE FUNCTION obj_alt (float8, float8, float8, float8, float8) 
+	RETURNS float8 AS 'pg_astrolib', 'obj_alt' LANGUAGE 'C';
 
-DROP FUNCTION obj_az (float4, float4, float4, float4, float4);
+DROP FUNCTION obj_az (float8, float8, float8, float8, float8);
 
 -- 			ra       dec     JD      lon     lat
-CREATE FUNCTION obj_az (float4, float4, float4, float4, float4) 
-	RETURNS float4 AS 'pg_astrolib', 'obj_az' LANGUAGE 'C';
+CREATE FUNCTION obj_az (float8, float8, float8, float8, float8) 
+	RETURNS float8 AS 'pg_astrolib', 'obj_az' LANGUAGE 'C';
 
-DROP FUNCTION obj_rise (float4, float4, float4, float4);
+DROP FUNCTION obj_rise (float8, float8, float8, float8);
 
-CREATE FUNCTION obj_rise (float4, float4, float4, float4)
-	RETURNS float4 AS 'pg_astrolib', 'obj_rise' LANGUAGE 'C';
+CREATE FUNCTION obj_rise (float8, float8, float8, float8)
+	RETURNS float8 AS 'pg_astrolib', 'obj_rise' LANGUAGE 'C';
 
-DROP FUNCTION obj_set (float4, float4, float4, float4);
+DROP FUNCTION obj_set (float8, float8, float8, float8);
 
-CREATE FUNCTION obj_set (float4, float4, float4, float4)
-	RETURNS float4 AS 'pg_astrolib', 'obj_set' LANGUAGE 'C';
+CREATE FUNCTION obj_set (float8, float8, float8, float8)
+	RETURNS float8 AS 'pg_astrolib', 'obj_set' LANGUAGE 'C';
