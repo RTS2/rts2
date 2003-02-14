d1cafe6e4548aec012d24483c44d2e184f09c85f
Petr Kubánek <petr@kubanek.net>
2003-02-14 03:02:44 +0000

    * added image_info.h
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@276 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ include/image_info.h
@@ -0,0 +1,19 @@
+#ifndef __RTS_IMAGE_INFO__
+#define __RTS_IMAGE_INFO__
+
+#include "camera_info.h"
+#include "telescope_info.h"
+
+#include <time.h>
+
+struct image_info
+{
+  struct telescope_info telescope;
+  struct camera_info camera;
+  time_t exposure_time;
+  float exposure_length;
+  int target_id;
+  int observation_id;
+};
+
+#endif /* __RTS_IMAGE_INFO__ */
