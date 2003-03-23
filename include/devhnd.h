70357bd5ccf994689c30a7a73098749421e465b6
Petr Kubánek <petr@kubanek.net>
2003-03-23 08:50:53 +0000

    * including serverd_info
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@380 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- include/devhnd.h
+++ include/devhnd.h
@@ -4,41 +4,13 @@
 #include "image_info.h"
 #include "param.h"
 #include "status.h"
-
-union devhnd_info
-{
-  struct telescope_info telescope;
-  struct camera_info camera;
-};
-
-typedef int (*devcli_handle_response_t) (struct param_status * params,
-					 union devhnd_info * devhnd_info);
-typedef int (*devcli_handle_data_t) (int sock, size_t size,
-				     struct image_info * image);
-
-struct devcli_channel_handlers
-{
-  devcli_handle_response_t command_handler;	//! handler to handle responses
-  devcli_handle_response_t message_handler;	//! handler to asynchoronous messages
-  struct image_info image;	//! data about image data
-};
+#include "serverd_info.h"
 
 struct supp_info
 {
   devcli_handle_response_t command_handler;	//! handler to handle responses
   devcli_handle_response_t message_handler;	//! handler to asynchoronous messages
 };
-
-int telescope_command_handler (struct param_status *params,
-			       struct telescope_info *info);
-int telescope_message_handler (struct param_status *params,
-			       struct telescope_info *info);
-
-int camera_command_handler (struct param_status *params,
-			    struct camera_info *info);
-int camera_message_handler (struct param_status *params,
-			    struct camera_info *info);
-int camera_data_handler (int sock, size_t size);
 
 extern struct supp_info devhnd_devices[];
 
