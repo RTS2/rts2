28a56ffddf64dd02e4d57fed9a79f5f8e40e0fb4
Petr Kubánek <petr@kubanek.net>
2003-05-14 23:58:30 +0000

    * bacmail, bacomailparse.pl
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@489 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/grb/bacodine/bacmail.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include "../../db/db.h"
+
+int
+main (int argc, char **argv)
+{
+  int grb_id;
+  int grb_seqn;
+  double grb_ra;
+  double grb_dec;
+  time_t grb_date;
+  char *grb_name;
+  char line_buf[200];
+  int tar_id;
+  int ret;
+  db_connect ();
+  while (fgets (line_buf, 200, stdin))
+    {
+      ret =
+	sscanf (line_buf, "%i %i %lf %lf %li %as", &grb_id, &grb_seqn,
+		&grb_ra, &grb_dec, &grb_date, &grb_name);
+      if (ret == 6)
+	{
+	  db_update_grb (grb_id, grb_seqn, grb_ra, grb_dec, &grb_date,
+			 &tar_id);
+	  printf ("tar_id: %i\n", tar_id);
+	}
+    }
+  db_disconnect ();
+  return 0;
+}
