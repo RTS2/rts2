19d88f173fa415f29f1f5c456d38009ebcc9bcf4
Petr Kubánek <petr@kubanek.net>
2003-07-23 00:19:58 +0000

    * grb client for all cameras
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@534 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/grb/grbc.c
+++ src/grb/grbc.c
@@ -24,13 +24,13 @@
 
 #include "image_info.h"
 
-#define EXPOSURE_TIME		120
-#define EPOCH			"002"
+#define EXPOSURE_TIME		20
 
 struct grb
 {
   int tar_id;
-  int id;
+  int grb_id;
+  int obs_id;
   int seqn;
   time_t created;
   time_t last_update;
@@ -46,39 +46,49 @@ pthread_t iban_thread;
 pthread_mutex_t observing_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t observing_cond = PTHREAD_COND_INITIALIZER;
 
-struct device *camera, *telescope;
+struct device *telescope;
 
 int
-readout (struct grb *object)
+get_info (struct grb *entry, struct device *tel, struct device *cam)
 {
   struct image_info *info =
     (struct image_info *) malloc (sizeof (struct image_info));
+  int ret;
 
-  devcli_wait_for_status (telescope, "telescope", TEL_MASK_MOVING,
-			  TEL_OBSERVING, 0);
-  devcli_command (camera, NULL, "expose 0 1 %i", EXPOSURE_TIME);
-
+  info->camera_name = cam->name;
+  printf ("info camera_name = %s\n", cam->name);
+  info->telescope_name = tel->name;
   info->exposure_time = time (NULL);
   info->exposure_length = EXPOSURE_TIME;
-  info->target_id = object->tar_id;
-  info->observation_id = object->id;
-  info->target_type = -1;
-  info->camera_name = camera->name;
-  info->telescope_name = telescope->name;
-  if (devcli_command (camera, NULL, "info") ||
-      !memcpy (&info->camera, &camera->info, sizeof (struct camera_info)) ||
-      devcli_command (telescope, NULL, "info") ||
-      !memcpy (&info->telescope, &telescope->info,
-	       sizeof (struct telescope_info))
-      || devcli_image_info (camera, info)
-      || devcli_wait_for_status (camera, "img_chip", CAM_MASK_EXPOSE,
-				 CAM_NOEXPOSURE, 0)
-      || devcli_command (camera, NULL, "readout 0"))
+  info->target_id = entry->tar_id;
+  info->observation_id = entry->obs_id;
+  info->target_type = TARGET_LIGHT;
+  if ((ret = devcli_command (cam, NULL, "info")))
     {
-      free (info);
-      return -1;
+      printf ("camera info error\n");
     }
+  memcpy (&info->camera, &cam->info, sizeof (struct camera_info));
+  memcpy (&info->telescope, &tel->info, sizeof (struct telescope_info));
+  devcli_image_info (cam, info);
   free (info);
+  return ret;
+}
+
+
+int
+readout ()
+{
+  struct device *camera;
+  devcli_wait_for_status (telescope, "telescope", TEL_MASK_MOVING,
+			  TEL_OBSERVING, 0);
+  devcli_command_all (DEVICE_TYPE_CCD, "expose 0 1 %i", EXPOSURE_TIME);
+
+  devcli_command (telescope, NULL, "info");
+  for (camera = devcli_devices (); camera; camera = camera->next)
+    get_info (&observing, telescope, camera);
+  devcli_wait_for_status_all (DEVICE_TYPE_CCD, "img_chip",
+			      CAM_MASK_EXPOSE, CAM_NOEXPOSURE, 0);
+  devcli_command_all (DEVICE_TYPE_CCD, "readout 0");
   return 0;
 }
 
@@ -89,14 +99,12 @@ process_grb_event (int id, int seqn, double ra, double dec, time_t * date)
   struct ln_lnlat_posn observer;
   struct ln_hrz_posn hrz;
 
-  object.ra = ra;
-  object.dec = dec;
   observer.lat = 50;
   observer.lng = -14.96;
 
   get_hrz_from_equ (&object, &observer, get_julian_from_sys (), &hrz);
 
-  if (observing.id == id)
+  if (observing.grb_id == id)
     {
       // cause I'm not interested in old updates
       if (observing.seqn < seqn)
@@ -104,24 +112,26 @@ process_grb_event (int id, int seqn, double ra, double dec, time_t * date)
 	  pthread_mutex_lock (&observing_lock);
 	  observing.seqn = seqn;
 	  observing.last_update = time (NULL);
+	  object.ra = ra;
+	  object.dec = dec;
 	  observing.object = object;
 	  pthread_cond_broadcast (&observing_cond);
 	  pthread_mutex_unlock (&observing_lock);
 	}
     }
-
-  if (observing.id < id)	// -1 count as well..
+  else if (observing.grb_id < id)	// -1 count as well..get the latest
     {
-      if (hrz.alt > 10)		// start observation
+      if (hrz.alt > 10)		// start observation - if not above horizont, don't care, we already observe something else
 	{
 	  pthread_mutex_lock (&observing_lock);
-	  observing.id = id;
-	  observing.seqn = -1;	// filled down
+	  db_update_grb (id, &seqn, &ra, &dec, date, &observing.tar_id);
+	  observing.grb_id = id;
 	  observing.seqn = seqn;
 	  observing.created = *date;
 	  observing.last_update = time (NULL);
+	  object.ra = ra;
+	  object.dec = dec;
 	  observing.object = object;
-	  db_update_grb (id, seqn, ra, dec, date, &observing.tar_id);
 	  pthread_cond_broadcast (&observing_cond);
 	  pthread_mutex_unlock (&observing_lock);
 	  return 0;
@@ -129,7 +139,7 @@ process_grb_event (int id, int seqn, double ra, double dec, time_t * date)
     }
 
   // just add to planer
-  return db_update_grb (id, seqn, ra, dec, date, NULL);
+  return db_update_grb (id, &seqn, &ra, &dec, date, NULL);
 }
 
 int
@@ -137,19 +147,17 @@ main (int argc, char **argv)
 {
   uint16_t port = SERVERD_PORT;
   char *server;
-
   struct ln_lnlat_posn observer;
   struct ln_hrz_posn hrz;
-
   int c;
-
   time_t t;
-  int obs_id;
+  int observing_count;
 
 #ifdef DEBUG
   mtrace ();
 #endif
-  observing.id = -1;
+  observing.tar_id = -1;
+  observing.grb_id = -1;
 
   observer.lat = 50;
   observer.lng = -14.96;
@@ -206,8 +214,8 @@ main (int argc, char **argv)
       return -1;
     }
 
-  pthread_create (&iban_thread, NULL, receive_iban,
-		  (void *) process_grb_event);
+//  pthread_create (&iban_thread, NULL, receive_iban,
+//                (void *) process_grb_event);
   pthread_create (&iban_thread, NULL, receive_bacodine,
 		  (void *) process_grb_event);
 
@@ -220,18 +228,9 @@ main (int argc, char **argv)
       exit (EXIT_FAILURE);
     }
 
-  camera = devcli_find ("C0");
+  devcli_device_data_handler (DEVICE_TYPE_CCD, data_handler);
 
-  if (!camera)
-    {
-      printf
-	("**** camera cannot be found.\n**** please check that's connected and camd is running.\n");
-      exit (EXIT_FAILURE);
-    }
-
-  camera->data_handler = data_handler;
-
-  telescope = devcli_find ("T1");
+  telescope = devcli_find ("T0");
 
   if (!telescope)
     {
@@ -242,40 +241,28 @@ main (int argc, char **argv)
 
   devcli_server_command (NULL, "status_txt grbc_waiting");
 
-#define CAMD_WRITE_READ(command) if (devcli_command (camera, NULL, command) < 0) \
-  				{ \
-      		                  perror ("devcli_write_read"); \
-				  continue;\
-				}
+  pthread_create (&image_que_thread, NULL, process_images, NULL);
 
-#define TELD_WRITE_READ(command) if (devcli_command (telescope, NULL, command) < 0) \
-  				{ \
-      		                  perror ("devcli_write_read"); \
-			          continue;\
-				}
   while (1)
     {
       pthread_mutex_lock (&observing_lock);
       printf ("Waiting for GRB event.\n");
       fflush (stdout);
-      while (observing.id == -1)
+      pthread_cond_wait (&observing_cond, &observing_lock);
+      if (devcli_server ()->statutes[0].status != SERVERD_NIGHT
+	  && devcli_server ()->statutes[0].status != SERVERD_DUSK
+	  && devcli_server ()->statutes[0].status != SERVERD_DAWN)
 	{
-	  pthread_cond_wait (&observing_cond, &observing_lock);
-	}
-      if (devcli_server ()->statutes[0].status != SERVERD_NIGHT)
-	{
+	  observing.tar_id = -1;
 	  printf ("serverd not in correct state, continuiing\n");
+	  pthread_mutex_unlock (&observing_lock);
 	  continue;
 	}
-      printf ("Starting observing %i tar_id: %i\n", observing.id,
+      printf ("Starting observing %i tar_id: %i\n", observing.tar_id,
 	      observing.tar_id);
 
-      CAMD_WRITE_READ ("ready");
-      CAMD_WRITE_READ ("info");
-
-      TELD_WRITE_READ ("ready");
-      TELD_WRITE_READ ("info");
-
+      devcli_command_all (DEVICE_TYPE_CCD, "ready;info");
+      devcli_command (telescope, NULL, "ready;base_info;info");
 
       devcli_server_command (NULL, "priority 200");
 
@@ -288,12 +275,16 @@ main (int argc, char **argv)
 
       time (&t);
 
-      db_start_observation (observing.tar_id, &t, &obs_id);
+      db_start_observation (observing.tar_id, &t, &observing.obs_id);
 
       get_hrz_from_equ (&observing.object, &observer, get_julian_from_sys (),
 			&hrz);
 
-      while (hrz.alt > 10)
+      observing_count = 0;
+      while (hrz.alt > 10
+	     && (devcli_server ()->statutes[0].status == SERVERD_NIGHT
+		 || devcli_server ()->statutes[0].status == SERVERD_DUSK
+		 || devcli_server ()->statutes[0].status == SERVERD_DAWN))
 	{
 	  if (devcli_command
 	      (telescope, NULL, "move %f %f", observing.object.ra,
@@ -301,25 +292,30 @@ main (int argc, char **argv)
 	    {
 	      printf ("telescope error\n\n--------------\n");
 	    }
+	  if (observing_count)
+	    devcli_wait_for_status_all (DEVICE_TYPE_CCD, "img_chip",
+					CAM_MASK_READING, CAM_NOTREADING, 0);
+	  observing_count++;
 	  pthread_mutex_unlock (&observing_lock);
 
-	  devcli_wait_for_status (camera, "priority", DEVICE_MASK_PRIORITY,
-				  DEVICE_PRIORITY, 0);
+	  devcli_wait_for_status_all (DEVICE_TYPE_CCD, "priority",
+				      DEVICE_MASK_PRIORITY, DEVICE_PRIORITY,
+				      0);
 
 	  printf ("OK\n");
 	  readout (&observing);
-
-	  pthread_create (&image_que_thread, NULL, process_images, NULL);
 
 	  pthread_mutex_lock (&observing_lock);
 	  get_hrz_from_equ (&observing.object, &observer,
 			    get_julian_from_sys (), &hrz);
 	}
 
+      devcli_server_command (NULL, "priority -1");
+
       t = time (NULL);
 
+      db_end_observation (observing.obs_id, &t);
       pthread_mutex_unlock (&observing_lock);
-      db_end_observation (observing.tar_id, obs_id, &t);
     }
 
   db_disconnect ();
