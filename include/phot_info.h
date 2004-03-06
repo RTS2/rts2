075ac4b20b82660c64adb768c10c7edf78cb2468
Petr Kubánek <petr@kubanek.net>
2004-03-06 15:52:12 +0000

    * added filter
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@792 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- include/phot_info.h
+++ include/phot_info.h
@@ -1,9 +1,12 @@
-#ifndef __RTS_DOME_INFO__
-#define __RTS_DOME_INFO__
+#ifndef __RTS_PHOT_INFO__
+#define __RTS_PHOT_INFO__
 
-struct dome_info
+struct phot_info
 {
   char type[64];
+  int count;
+  int exposure;
+  int filter;
 };
 
-#endif /* __RTS_DOME_INFO__ */
+#endif /* __RTS_PHOT_INFO__ */
