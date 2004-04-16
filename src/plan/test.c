9bb2ae5f8cfa3ff367ca333d3c09a4f7585a887c
Petr Kubánek <petr@kubanek.net>
2004-04-16 17:36:09 +0000

    * libnova 0.10
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@869 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/plan/test.c
+++ src/plan/test.c
@@ -8,25 +8,28 @@
 int
 main (int argc, char **argv)
 {
-  struct target *plan, *last;
+  struct target plan, *last;
   char *dp;
+  int i;
+  time_t t;
   db_connect ();
   read_config (CONFIG_FILE);
   ECPGdebug (1, "log.out");
   db_get_darkfield ("C0", 6000, -50, &dp);
   printf ("dp: %s\n", dp);
-  make_plan (&plan, 120, -15, 50);
-  for (last = plan; last; last = last->next)
+  for (i = 0; i < 10; i++)
     {
-      double jd = get_julian_from_timet (&last->ctime);
+      double jd = ln_get_julian_from_timet (&last->ctime);
       struct ln_equ_posn object;
       struct ln_lnlat_posn observer;
       struct ln_hrz_posn hrz;
+      get_next_plan (&plan, SELECTOR_AIRMASS, &t, 1, 100, 1, -14, 50);
+      last = plan.next;
       object.ra = last->ra;
       object.dec = last->dec;
       observer.lng = 6.733;
       observer.lat = 37.1;
-      get_hrz_from_equ (&object, &observer, jd, &hrz);
+      ln_get_hrz_from_equ (&object, &observer, jd, &hrz);
       if (last->id > 0)
 	printf ("%i\t%f\t%f\t%s\t%f\t%f\n", last->id, last->ra, last->dec,
 		ctime (&last->ctime), hrz.alt, hrz.az);
@@ -34,7 +37,6 @@ main (int argc, char **argv)
 	printf ("%i\t\t\t\t%s** correction %i\n", last->id,
 		ctime (&last->ctime), last->type);
     }
-  free_plan (plan);
 
   printf ("last_image C0: %i\n", db_last_good_image ("C0"));
   printf ("last_image C1: %i\n", db_last_good_image ("C1"));
