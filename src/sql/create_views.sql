2133492a571e07b28536c5e8c891945cfc3bc3ba
Petr Kubánek <petr@kubanek.net>
2004-05-23 20:39:30 +0000

    * re-added
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@907 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sql/create_views.sql
+++ src/sql/create_views.sql
@@ -1,6 +1,11 @@
 DROP VIEW targets_images;
 DROP VIEW targets_noimages;
 DROP VIEW targets_imgcount;
+DROP VIEW observations_imgcount;
+DROP VIEW observations_noimages;
+DROP VIEW observations_images;
+DROP VIEW observations_nights;
+DROP VIEW images_nights;
 
 CREATE VIEW targets_noimages AS 
 SELECT targets.tar_id, 0 AS img_count 
@@ -13,8 +18,8 @@ SELECT targets.tar_id, 0 AS img_count
 CREATE VIEW targets_imgcount AS
 SELECT targets.tar_id, count(*) AS img_count
 	FROM targets, observations, images 
-	WHERE ((images.obs_id = observations.obs_id) AND 
-		(observations.tar_id = targets.tar_id))
+	WHERE images.obs_id = observations.obs_id AND 
+		observations.tar_id = targets.tar_id
 	GROUP BY targets.tar_id ORDER BY count(*) DESC;
 
 CREATE VIEW targets_images AS 
@@ -30,4 +35,60 @@ SELECT targets.*,
 	obj_az (tar_ra, tar_dec, 240000, mount_long, mount_lat) 
 	AS az
 	FROM mounts, targets
-	WHERE mount_name = "T1";
+	WHERE mount_name = 'T1';
+
+CREATE VIEW observations_noimages AS 
+SELECT observations.obs_id, 0 AS img_count 
+	FROM observations 
+	WHERE (NOT (EXISTS 
+		(SELECT * FROM images 
+			WHERE observations.obs_id = images.obs_id)));
+
+CREATE VIEW observations_imgcount AS
+SELECT observations.obs_id, count(*) AS img_count
+	FROM observations, images 
+	WHERE images.obs_id = observations.obs_id
+	GROUP BY observations.obs_id ORDER BY count(*) DESC;
+
+CREATE VIEW observations_images AS 
+SELECT obs_id, img_count 
+	FROM observations_imgcount 
+UNION SELECT obs_id, img_count 
+	FROM observations_noimages;
+
+CREATE VIEW observations_nights AS
+SELECT observations.*, date_part('day', (obs_start - interval '12:00')) AS obs_night, 
+	date_part('month', (obs_start - interval '12:00')) AS obs_month, 
+	date_part('year', (observations.obs_start - interval '12:00')) AS obs_year 
+	FROM observations;
+
+CREATE VIEW images_nights AS
+SELECT images.*, date_part('day', (timestamptz(images.img_date) - interval '12:00')) AS img_night, 
+	date_part('month', (timestamptz(images.img_date) - interval '12:00')) AS img_month, 
+	date_part('year', (timestamptz(images.img_date) - interval '12:00')) AS img_year 
+	FROM images;
+
+GRANT SELECT ON targets_noimages TO "";
+GRANT SELECT ON targets_imgcount TO "";
+GRANT SELECT ON targets_images TO "";
+GRANT SELECT ON targets_altaz TO "";
+
+GRANT SELECT ON observations_noimages TO "";
+GRANT SELECT ON observations_imgcount TO "";
+GRANT SELECT ON observations_images TO "";
+GRANT SELECT ON observations_nights TO "";
+GRANT SELECT ON images_nights TO "";
+
+------------------------------------------------------
+
+GRANT SELECT ON targets_noimages TO GROUP observers;
+GRANT SELECT ON targets_imgcount TO GROUP observers;
+GRANT SELECT ON targets_images TO GROUP observers;
+GRANT SELECT ON targets_altaz TO GROUP observers;
+
+GRANT SELECT ON observations_noimages TO GROUP observers;
+GRANT SELECT ON observations_imgcount TO GROUP observers;
+GRANT SELECT ON observations_images TO GROUP observers;
+GRANT SELECT ON observations_nights TO GROUP observers;
+GRANT SELECT ON images_nights TO GROUP observers;
+
