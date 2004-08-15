a046236cd1481a4efc6176318b9e3ba03c46a509
Petr Kubánek <petr@kubanek.net>
2004-08-15 19:04:27 +0000

    * constants for AP16 - replaces config file
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@1055 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/camd/apogee/apcnst.h
@@ -0,0 +1,40 @@
+/**
+ * These conststanst are used instead of .ini (quick hack)
+ */
+
+#define AP_Interface		"PCI"
+#define AP_Base			0x0
+#define AP_LongCable		false
+#define	High_Priority		false
+#define AP_Data_Bits		14
+#define AP_CCD_Sensor		"CCD"
+#define AP_Mode			0x1
+#define AP_Test			0x4
+#define AP_Shutter_Speed	"Normal"
+#define AP_Shutter_Bits		0x00
+#define AP_MaxBinX		8
+#define AP_MaxBinY		63
+#define AP_Guider_Relays	false
+
+#define AP_Columns		4130
+#define AP_Rows			4110
+#define AP_ImgCols		4090
+#define AP_ImgRows		4090
+#define AP_BIC			26
+#define AP_BIR			6
+#define AP_SkipC		6
+#define AP_SkipR		8
+#define AP_HFlush		8
+#define AP_VFlush		50
+
+#define AP_Control		true
+#define AP_Target		-20.0
+#define AP_Cal			165
+#define AP_Scale		2.1
+
+#define AP_Sensor		"KAF-16800e"
+#define AP_Color		false
+#define AP_Noise		20.0
+#define AP_Gain			8.0
+#define AP_PixelXSize		9
+#define AP_PixelYSize		9
