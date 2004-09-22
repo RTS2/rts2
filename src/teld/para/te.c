6a4671545e1ed6575aba3b3c9d9f06e180517b23
Petr Kubánek <petr@kubanek.net>
2004-09-22 21:24:05 +0000

    * libnova 0.10 issues
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@1113 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/teld/para/te.c
+++ src/teld/para/te.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
-#include <libnova.h>
+#include <libnova/libnova.h>
 #include <math.h>
 #include <malloc.h>
 #include <string.h>
@@ -39,7 +39,7 @@
 // Measured: 29.9039
 #define HA_ZERO 958121		// AFTERNOON ZERO (The other is HA_ZERO + HA_CPD * 360)
 
-//double get_julian_from_sys();
+//double ln_get_julian_from_sys();
 
 //#define DUMMY
 
@@ -112,7 +112,9 @@ sid_time ()
 {
   double s;
 
-  s = 15 * get_apparent_sidereal_time (get_julian_from_sys ()) - LONGITUDE;
+  s =
+    15 * ln_get_apparent_sidereal_time (ln_get_julian_from_sys ()) -
+    LONGITUDE;
   printf ("sid_time() = %f ", s);
 
   in360 (&s);
@@ -121,15 +123,15 @@ sid_time ()
 }
 
 double
-get_refraction (double altitude, double atm_pres, double temp)
+ln_get_refraction (double altitude, double atm_pres, double temp)
 {
   double R;
 
-  //altitude = deg_to_rad (altitude);
+  //altitude = ln_deg_to_rad (altitude);
 
   /* eq. 5.27 (Telescope Control,M.Trueblood & R.M. Genet) */
   R = 1.02 /
-    tan (deg_to_rad (altitude + 10.3 / (altitude + 5.11) + 0.0019279));
+    tan (ln_deg_to_rad (altitude + 10.3 / (altitude + 5.11) + 0.0019279));
 
   // for gnuplot (in degrees)
   // R(a) = 1.02 / tan( (a + 10.3 / (a + 5.11) + 0.0019279) / 57.2958 ) / 60
@@ -165,16 +167,16 @@ ha_dec (struct ln_equ_posn *mean_position,
   // Transform from catalogue position (J2000 ???) to apparent position
   proper_motion.ra = 0;
   proper_motion.dec = 0;
-  get_apparent_posn (mean_position, &proper_motion, JD, app_position);	// Second argument should be proper motion but we ignore it
+  ln_get_apparent_posn (mean_position, &proper_motion, JD, app_position);	// Second argument should be proper motion but we ignore it
   printf ("\033[34mcat: %f %f\033[m\n", mean_position->ra,
 	  mean_position->dec);
   printf ("\033[34mapp: %f %f\033[m\n", app_position->ra, app_position->dec);
   // Transform to horizontal coordinates
-  get_hrz_from_equ (app_position, observer, JD, &h_app_position);
+  ln_get_hrz_from_equ (app_position, observer, JD, &h_app_position);
   printf ("\033[34mhor: %f %f\033[m\n", h_app_position.az,
 	  h_app_position.alt);
   // Correction for refraction
-  refraction = get_refraction (h_app_position.alt, atm_pres, temp);
+  refraction = ln_get_refraction (h_app_position.alt, atm_pres, temp);
   h_app_position.alt = h_app_position.alt + refraction;
   refraction *= 60;
   if (refraction > 2)
@@ -184,11 +186,11 @@ ha_dec (struct ln_equ_posn *mean_position,
     printf ("\033[34mThe atmospheric refraction is: \033[m%f\"\n",
 	    refraction * 60);
   // Convertion to equatorial
-  get_equ_from_hrz (&h_app_position, observer, JD, app_position);
+  ln_get_equ_from_hrz (&h_app_position, observer, JD, app_position);
   printf ("\033[34mcor: %f %f\033[m\n", app_position->ra, app_position->dec);
 
   // Convertion to ha_dec
-  //get_hd_from_equ (&app_position, observer, JD,apparent_position);
+  //ln_get_hd_from_equ (&app_position, observer, JD,apparent_position);
 
 
 
@@ -202,7 +204,7 @@ astrometric_corr (double *ra, double *dec, double atm_pres, double temp)
   struct ln_equ_posn mean_position;	//==> The mean equatorial position in the catalog (libnova)
   struct ln_equ_posn app_position;	//==> The apparent equatorial position (libnova)
 
-  JD = get_julian_from_sys ();
+  JD = ln_get_julian_from_sys ();
 
   observer.lat = 37.1;
   observer.lng = +6.732778;
@@ -659,7 +661,7 @@ telescope_off ()
 
     mount = init("/dev/ttyS0");
 
-    ////homingjd = get_julian_from_sys();
+    ////homingjd = ln_get_julian_from_sys();
 
     //move(mount, 81.600792, 7.1465722); // GRB 030509 Center
 //    move(mount, 81.6460, 7.1492); // GRB 030509 Center
