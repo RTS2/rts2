11029eb6daa274f3123220c2a9501874c9dce21f
Petr Kubánek <petr@kubanek.net>
2004-09-24 18:01:31 +0000

    [bug-fix] cpp isues
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@1119 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/writers/fits.h
+++ src/writers/fits.h
@@ -7,6 +7,11 @@
 #ifndef __RTS_FITS__
 #define __RTS_FITS__
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include <fitsio.h>
 #include <pthread.h>
 
@@ -15,27 +20,31 @@
 /*! 
  * Structure to hold additional fits informations.
  */
-struct fits_receiver_data
-{
-  int offset;
-  size_t size;
-  fitsfile *ffile;
-  char *data;
-  int header_processed;
-  struct image_info *info;
-  pthread_t thread;
+  struct fits_receiver_data
+  {
+    int offset;
+    size_t size;
+    fitsfile *ffile;
+    char *data;
+    int header_processed;
+    struct image_info *info;
+    pthread_t thread;
+  };
+
+  int fits_create (struct fits_receiver_data *receiver, char *filename);
+
+  int fits_write_image_info (struct fits_receiver_data *receiver,
+			     struct image_info *image, char *dark_name);
+
+  int fits_init (struct fits_receiver_data *receiver, size_t expected_size);
+
+  int fits_handler (void *data, size_t size,
+		    struct fits_receiver_data *receiver);
+
+  int fits_close (struct fits_receiver_data *receiver);
+
+#ifdef __cplusplus
 };
-
-int fits_create (struct fits_receiver_data *receiver, char *filename);
-
-int fits_write_image_info (struct fits_receiver_data *receiver,
-			   struct image_info *image, char *dark_name);
-
-int fits_init (struct fits_receiver_data *receiver, size_t expected_size);
-
-int fits_handler (void *data, size_t size,
-		  struct fits_receiver_data *receiver);
-
-int fits_close (struct fits_receiver_data *receiver);
+#endif
 
 #endif /* !__RTS_FITS__ */
