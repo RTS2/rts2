daa490515c9bae21c83de18d00529efbc5dcb849
Petr Kubánek <petr@kubanek.net>
2004-10-05 11:11:02 +0000

    This commit was manufactured by cvs2svn to create tag 'PLAN_PRE_CPP_MOVE'.
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/tags/rts-2/PLAN_PRE_CPP_MOVE@1133 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/camd/focuser.h
+++
@@ -1,6 +0,0 @@
-#ifndef __RTS2_FOCUSER__
-#define __RTS2_FOCUSER__
-
-int focus_set (int steps);
-
-#endif /* !__RTS2_FOCUSER__ */
--- src/serverd/riseset.h
+++
@@ -1,8 +0,0 @@
-#ifndef __RTS_RISESET__
-#define __RTS_RISESET__
-
-#include <libnova/ln_types.h>
-#include <time.h>
-int next_event (struct ln_lnlat_posn *observer, time_t * start_time,
-		int *curr_type, int *type, time_t * ev_time);
-#endif /* __RTS_RISESET__ */
