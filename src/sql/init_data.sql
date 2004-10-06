8ebf0912d087134625302f4e918a6d74c90f7b12
Petr Kubánek <petr@kubanek.net>
2004-10-06 09:45:38 +0000

    [bug-fix] Gama -> Gamma
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@1149 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sql/init_data.sql
+++ src/sql/init_data.sql
@@ -1,6 +1,6 @@
-COPY types (type_id, type_description) FROM stdin;
+COPY types FROM stdin;
 O	Opportunity targets
-G	Gama Ray Burst
+G	Gamma Ray Burst
 S	Sky survey (new one)
 E	Elliptical targets
 P	Galatic Plate Scan
