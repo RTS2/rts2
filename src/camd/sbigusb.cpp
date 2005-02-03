2c4247d03fe31ecfcda46d394459246155102e08
Petr Kubánek <petr@kubanek.net>
2005-02-03 21:46:22 +0000

    * added SBIG USB new modules
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@1207 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/camd/sbigusb.cpp
@@ -0,0 +1,398 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#define MAX_CHIPS 	2	//maximal number of chips
+
+#include <math.h>
+#include <mcheck.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "camera_cpp.h"
+#include "../utils/rts2device.h"
+#include "../utils/rts2block.h"
+
+#include "sbigudrv.h"
+#include "csbigcam.h"
+
+unsigned int
+ccd_c2ad (double t)
+{
+  double r = 3.0 * exp (0.94390589891 * (25.0 - t) / 25.0);
+  unsigned int ret = (unsigned int) (4096.0 / (10.0 / r + 1.0));
+  if (ret > 4096)
+    ret = 4096;
+  return ret;
+}
+
+double
+ccd_ad2c (unsigned int ad)
+{
+  double r;
+  if (ad == 0)
+    return 1000;
+  r = 10.0 / (4096.0 / (double) ad - 1.0);
+  return 25.0 - 25.0 * (log (r / 3.0) / 0.94390589891);
+}
+
+double
+ambient_ad2c (unsigned int ad)
+{
+  double r;
+  r = 3.0 / (4096.0 / (double) ad - 1.0);
+  return 25.0 - 45.0 * (log (r / 3.0) / 2.0529692213);
+}
+
+class CameraSbigChip:public CameraChip
+{
+  ReadoutLineParams rlp;
+  unsigned short *dest;		// for chips..
+  unsigned short *dest_top;
+  char *send_top;
+public:
+    CameraSbigChip (int in_chip_id, int in_width, int in_height,
+		    int in_pixelX, int in_pixelY, float in_gain);
+  virtual int startReadout (Rts2DevConnData * dataConn, Rts2Conn * conn);
+  virtual int readoutOneLine ();
+};
+
+CameraSbigChip::CameraSbigChip (int in_chip_id, int in_width, int in_height,
+				int in_pixelX, int in_pixelY, float in_gain):
+CameraChip (in_chip_id, in_width, in_height, in_pixelX, in_pixelY, in_gain)
+{
+  int size = in_width * in_height *
+			       sizeof (unsigned short);
+  printf ("malloced size: %i\n", size);
+  dest =
+    (unsigned short *) malloc (size);
+};
+
+int
+CameraSbigChip::startReadout (Rts2DevConnData * dataConn, Rts2Conn * conn)
+{
+  rlp.ccd = CCD_IMAGING;
+  rlp.pixelStart = 0;
+  rlp.pixelLength = chipSize->width;
+  dest_top = dest;
+  send_top = (char*) dest;
+  return CameraChip::startReadout (dataConn, conn);
+}
+
+int
+CameraSbigChip::readoutOneLine ()
+{
+  if (readoutLine < 0)
+    return -1;
+  if (readoutLine < chipSize->height)
+    {
+      readoutLine++;
+      SBIGUnivDrvCommand (CC_READOUT_LINE, &rlp, dest_top);
+      dest_top += rlp.pixelLength;
+      return 0;
+    }
+  if (sendLine == 0)
+    {
+      int ret;
+      ret = CameraChip::sendFirstLine ();
+      if (ret)
+	return ret;
+    }
+  if (send_top < (char *) dest_top)
+    {
+      sendLine++;
+      send_top +=
+	readoutConn->send (send_top,
+			   (char *) dest_top - send_top);
+      return 0;
+    }
+  readoutConn->endConnection ();
+  readoutLine = -1;
+  return -2;
+}
+
+class Rts2DevCameraSbig:public Rts2DevCamera
+{
+  CSBIGCam *pcam;
+  int checkSbigHw (PAR_ERROR ret)
+  {
+    if (ret == CE_NO_ERROR)
+      return 0;
+    return -1;
+  }
+public:
+    Rts2DevCameraSbig (int argc, char **argv);
+  ~Rts2DevCameraSbig (void);
+
+  virtual int init ();
+
+  // callback functions for Camera alone
+  virtual int camReady ();
+  virtual int camInfo ();
+  virtual int camBaseInfo ();
+  virtual int camChipInfo (int chip);
+  virtual int camExpose (int chip, int light, float exptime);
+  virtual int camWaitExpose (int chip);
+  virtual int camStopExpose (int chip);
+  virtual int camBox (int chip, int x, int y, int width, int height);
+  virtual int camBinning (int chip, int x_bin, int y_bin);
+  virtual int camStopRead (int chip);
+  virtual int camCoolMax ();
+  virtual int camCoolHold ();
+  virtual int camCoolTemp (float new_temp);
+  virtual int camCoolShutdown ();
+  virtual int camFilter (int new_filter);
+};
+
+Rts2DevCameraSbig::Rts2DevCameraSbig (int argc, char **argv):
+Rts2DevCamera (argc, argv)
+{
+  pcam = NULL;
+}
+
+Rts2DevCameraSbig::~Rts2DevCameraSbig (void)
+{
+  delete pcam;
+}
+
+int
+Rts2DevCameraSbig::init ()
+{
+  pcam = new CSBIGCam (DEV_USB);
+  if (pcam->GetError () != CE_NO_ERROR)
+    {
+      delete pcam;
+      return -1;
+    }
+  pcam->EstablishLink ();
+  if (pcam->GetError () != CE_NO_ERROR)
+    {
+      delete pcam;
+      return -1;
+    }
+  // get device information
+  chipNum = 2;			// hardcoded
+
+  // init chips
+  GetCCDInfoParams req;
+  GetCCDInfoResults0 res;
+  CameraSbigChip *cc;
+  PAR_ERROR ret;
+
+  req.request = CCD_INFO_IMAGING;
+  ret = pcam->SBIGUnivDrvCommand (CC_GET_CCD_INFO, &req, &res);
+  if (ret != CE_NO_ERROR)
+    {
+      return -1;
+    }
+  cc =
+    new CameraSbigChip (0, res.readoutInfo[0].width,
+			res.readoutInfo[0].height,
+			res.readoutInfo[0].pixelWidth,
+			res.readoutInfo[0].pixelHeight,
+			res.readoutInfo[0].gain);
+  chips[0] = cc;
+
+  req.request = CCD_INFO_TRACKING;
+  ret = pcam->SBIGUnivDrvCommand (CC_GET_CCD_INFO, &req, &res);
+  if (ret != CE_NO_ERROR)
+    {
+      return -1;
+    }
+  cc =
+    new CameraSbigChip (0, res.readoutInfo[0].width,
+			res.readoutInfo[0].height / 2,
+			res.readoutInfo[0].pixelWidth,
+			res.readoutInfo[0].pixelHeight,
+			res.readoutInfo[0].gain);
+  chips[1] = cc;
+
+  return Rts2DevCamera::init ();
+}
+
+int
+Rts2DevCameraSbig::camReady ()
+{
+  double ccdTemp;
+  PAR_ERROR ret;
+  ret = pcam->GetCCDTemperature (ccdTemp);
+  return checkSbigHw (ret);
+}
+
+int
+Rts2DevCameraSbig::camInfo ()
+{
+  MY_LOGICAL enabled;
+  double amb, ccd, set, per;
+  if (pcam->QueryTemperatureStatus (enabled, amb, set, per) != CE_NO_ERROR)
+    return -1;
+  fan = enabled;
+  tempAir = amb;
+  tempSet = set;
+  coolingPower = (int) (per * 1000);
+  if (pcam->GetCCDTemperature (ccd) != CE_NO_ERROR)
+    return -1;
+  tempCCD = ccd;
+  tempRegulation = 1;
+  return 0;
+}
+
+int
+Rts2DevCameraSbig::camBaseInfo ()
+{
+  GetDriverInfoResults0 gccdir0;
+  pcam->GetDriverInfo (DRIVER_STD, gccdir0);
+  if (pcam->GetError () != CE_NO_ERROR)
+    return -1;
+  sprintf (ccdType, "SBIG %i", pcam->GetCameraType ());
+  // get serial number
+
+  GetCCDInfoParams req;
+  GetCCDInfoResults2 res;
+  PAR_ERROR ret;
+
+  req.request = CCD_INFO_EXTENDED2_IMAGING;
+  ret = pcam->SBIGUnivDrvCommand (CC_GET_CCD_INFO, &req, &res);
+  if (ret != CE_NO_ERROR)
+    return -1;
+  strncpy (serialNumber, res.serialNumber, 10);
+  canDF = 1;
+  return 0;
+}
+
+int
+Rts2DevCameraSbig::camChipInfo (int chip)
+{
+  return 0;
+}
+
+int
+Rts2DevCameraSbig::camExpose (int chip, int light, float exptime)
+{
+  PAR_ERROR ret;
+  if (!pcam->CheckLink ())
+    return -1;
+
+  StartExposureParams sep;
+  sep.ccd = chip;
+  sep.exposureTime = (unsigned long) (exptime * 100.0 + 0.5);
+  if (sep.exposureTime < 1)
+    sep.exposureTime = 1;
+  sep.abgState = ABG_LOW7;
+  sep.openShutter = light ? SC_OPEN_SHUTTER : SC_CLOSE_SHUTTER;
+
+  ret = pcam->SBIGUnivDrvCommand (CC_START_EXPOSURE, &sep, NULL);
+  return checkSbigHw (ret);
+}
+
+int
+Rts2DevCameraSbig::camWaitExpose (int chip)
+{
+  int ret;
+  ret = Rts2DevCamera::camWaitExpose (chip);
+  if (ret == 0)
+    {
+      return camStopExpose (chip);	// SBIG devices are strange, there is same command for wait and stop
+    }
+  return -1;
+}
+
+int
+Rts2DevCameraSbig::camStopExpose (int chip)
+{
+  PAR_ERROR ret;
+  if (!pcam->CheckLink ())
+    {
+      return DEVDEM_E_HW;
+    }
+
+  EndExposureParams eep;
+  eep.ccd = chip;
+
+  ret = pcam->SBIGUnivDrvCommand (CC_END_EXPOSURE, &eep, NULL);
+  return checkSbigHw (ret);
+}
+
+int
+Rts2DevCameraSbig::camBox (int chip, int x, int y, int width, int height)
+{
+  return -1;
+}
+
+int
+Rts2DevCameraSbig::camBinning (int chip, int x_bin, int y_bin)
+{
+  return 0;
+}
+
+int
+Rts2DevCameraSbig::camStopRead (int chip)
+{
+  return -1;
+}
+
+int
+Rts2DevCameraSbig::camCoolMax ()
+{
+  SetTemperatureRegulationParams temp;
+  PAR_ERROR ret;
+  temp.regulation = REGULATION_OVERRIDE;
+  temp.ccdSetpoint = 255;
+  ret = pcam->SBIGUnivDrvCommand (CC_SET_TEMPERATURE_REGULATION, &temp, NULL);
+  return checkSbigHw (ret);
+}
+
+int
+Rts2DevCameraSbig::camCoolHold ()
+{
+  return camCoolTemp (-5);
+}
+
+int
+Rts2DevCameraSbig::camCoolTemp (float new_temp)
+{
+  SetTemperatureRegulationParams temp;
+  PAR_ERROR ret;
+  temp.regulation = REGULATION_ON;
+  printf ("setTemp: %f\n", new_temp);
+  temp.ccdSetpoint = ccd_c2ad (new_temp);
+  ret = pcam->SBIGUnivDrvCommand (CC_SET_TEMPERATURE_REGULATION, &temp, NULL);
+  return checkSbigHw (ret);
+}
+
+int
+Rts2DevCameraSbig::camCoolShutdown ()
+{
+  SetTemperatureRegulationParams temp;
+  PAR_ERROR ret;
+  temp.regulation = REGULATION_OFF;
+  temp.ccdSetpoint = 0;
+  ret = pcam->SBIGUnivDrvCommand (CC_SET_TEMPERATURE_REGULATION, &temp, NULL);
+  return checkSbigHw (ret);
+}
+
+int
+Rts2DevCameraSbig::camFilter (int new_filter)
+{
+
+
+}
+
+int
+main (int argc, char **argv)
+{
+  mtrace ();
+
+  Rts2DevCameraSbig *device = new Rts2DevCameraSbig (argc, argv);
+
+  int ret;
+  ret = device->init ();
+  if (ret)
+    {
+      fprintf (stderr, "Cannot initialize camera - exiting!\n");
+      exit (0);
+    }
+  device->run ();
+  delete device;
+}
