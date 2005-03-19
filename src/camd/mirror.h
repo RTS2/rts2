45171d79253e95b08b166163deb6d4cc178926ba
Petr Kubánek <petr@kubanek.net>
2005-03-19 16:54:05 +0000

    * mirror control
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@1311 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/camd/mirror.h
+++ src/camd/mirror.h
@@ -1,19 +1,70 @@
 #ifndef __RTS2_MIRROR__
 #define __RTS2_MIRROR__
 
-#ifdef __cplusplus
-extern "C"
+#include "camera_cpp.h"
+
+
+class Rts2DevCameraMirror:public Rts2DevCamera
 {
-#endif
+  char *mirror_dev;
+  int mirror_fd;
+  FILE *mirr_log;
 
-  int mirror_open_dev (char *dev);
+  int mirror_command (char cmd, int arg, char *ret_cmd, int *ret_arg);
+  int mirror_get (int *pos);
+  // need to set steps before calling this one
+  int mirror_set ();
 
-  int mirror_open ();
+  int steps;
+  int set_ret;
+  int cmd;
 
-  int mirror_close ();
+  int expChip;
+  int expLight;
+  float expExpTime;
 
-#ifdef __cplusplus
+public:
+    Rts2DevCameraMirror (int argc, char **argv);
+    virtual ~ Rts2DevCameraMirror (void);
+  virtual int processOption (int in_op);
+  virtual int init ();
+  virtual int idle ();
+
+  virtual Rts2Conn *createConnection (int in_sock, int conn_num);
+
+  virtual int startOpen ();
+  virtual int isOpened ();
+  virtual int endOpen ();
+
+  virtual int startClose ();
+  virtual int isClosed ();
+  virtual int endClose ();
+
+  // return 1, when mirror is (still) moving
+  virtual int isMoving ()
+  {
+    return !!cmd;
+  }
+
+  virtual int camExpose (int chip, int light, float exptime);
+
+  virtual long camWaitExpose (int chip);
 };
-#endif
+
+class Rts2DevConnCameraMirror:public Rts2DevConnCamera
+{
+private:
+  Rts2DevCameraMirror * master;
+protected:
+  virtual int commandAuthorized ();
+public:
+    Rts2DevConnCameraMirror (int in_sock,
+			     Rts2DevCameraMirror *
+			     in_master_device):Rts2DevConnCamera (in_sock,
+								  in_master_device)
+  {
+    master = in_master_device;
+  }
+};
 
 #endif /* ! __RTS2_MIRROR__ */
