92a7a25a7cc5b77e819b9b4630634970b65719a7
Petr Kubánek <petr@kubanek.net>
2005-08-03 22:18:57 +0000

    * focusing support finished & bug-fixed
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@1769 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/writers/rts2devclifoc.cpp
+++ src/writers/rts2devclifoc.cpp
@@ -19,12 +19,45 @@ Rts2DevClientCameraImage (in_connection)
     {
       exe - NULL;
     }
+  isFocusing = 0;
 }
 
 Rts2DevClientCameraFoc::~Rts2DevClientCameraFoc (void)
 {
   if (exe)
     delete exe;
+}
+
+void
+Rts2DevClientCameraFoc::queExposure ()
+{
+  if (isFocusing)
+    return;
+  Rts2DevClientCameraImage::queExposure ();
+}
+
+void
+Rts2DevClientCameraFoc::postEvent (Rts2Event * event)
+{
+  Rts2DevClientFocusFoc *focuser;
+  const char *focName;
+  char *cameraFoc;
+  switch (event->getType ())
+    {
+    case EVENT_FOCUSING_END:
+      focuser = (Rts2DevClientFocusFoc *) event->getArg ();
+      focName = focuser->getName ();
+      cameraFoc = getValueChar ("focuser");
+      if (focName && cameraFoc
+	  && !strcmp (focuser->getName (), getValueChar ("focuser")))
+	{
+	  isFocusing = 0;
+	  exposureCount = 1;
+	  queExposure ();
+	}
+      break;
+    }
+  Rts2DevClientCameraImage::postEvent (event);
 }
 
 void
@@ -48,10 +81,52 @@ Rts2DevClientCameraFoc::processImage (Rts2Image * image)
     }
 }
 
+void
+Rts2DevClientCameraFoc::changeFocus (int steps)
+{
+  Rts2Conn *focuser;
+  focuser = getMaster ()->getOpenConnection (getValueChar ("focuser"));
+  if (focuser)
+    {
+      focuser->
+	postEvent (new Rts2Event (EVENT_START_FOCUSING, (void *) &steps));
+      isFocusing = 1;
+    }
+}
+
+Rts2DevClientFocusFoc::Rts2DevClientFocusFoc (Rts2Conn * in_connection):Rts2DevClientFocus
+  (in_connection)
+{
+}
+
+void
+Rts2DevClientFocusFoc::postEvent (Rts2Event * event)
+{
+  switch (event->getType ())
+    {
+    case EVENT_START_FOCUSING:
+      connection->
+	queCommand (new
+		    Rts2CommandChangeFocus (this,
+					    *((int *) event->getArg ())));
+      break;
+    }
+  Rts2DevClientFocus::postEvent (event);
+}
+
+void
+Rts2DevClientFocusFoc::focusingEnd ()
+{
+  Rts2DevClientFocus::focusingEnd ();
+  connection->getMaster ()->
+    postEvent (new Rts2Event (EVENT_FOCUSING_END, (void *) this));
+}
+
 Rts2ConnFocus::Rts2ConnFocus (Rts2DevClientCameraFoc * in_camera,
 			      Rts2Image * in_image, const char *in_exe):
 Rts2ConnFork (in_camera->getMaster (), in_exe)
 {
+  camera = in_camera;
   img_path = new char[strlen (in_image->getImageName ()) + 1];
   strcpy (img_path, in_image->getImageName ());
 }
@@ -85,6 +160,7 @@ Rts2ConnFocus::processLine ()
   if (ret == 2)
     {
       std::cout << "Get change: " << id << " " << change << std::endl;
+      camera->changeFocus (change);
       // post it to focuser
     }
   return -1;
--- src/writers/rts2devclifoc.h
+++ src/writers/rts2devclifoc.h
@@ -4,19 +4,39 @@
 #include "rts2devcliimg.h"
 #include "../utils/rts2connfork.h"
 
+#define EVENT_START_FOCUSING	RTS2_LOCAL_EVENT + 500
+#define EVENT_FOCUSING_END	RTS2_LOCAL_EVENT + 501
+
 class Rts2DevClientCameraFoc:public Rts2DevClientCameraImage
 {
+private:
+  int isFocusing;
 protected:
   char *exe;
+
+  virtual void queExposure ();
 public:
     Rts2DevClientCameraFoc (Rts2Conn * in_connection, const char *in_exe);
     virtual ~ Rts2DevClientCameraFoc (void);
+  virtual void postEvent (Rts2Event * event);
   virtual void processImage (Rts2Image * image);
+  // will cause camera to change focus by given steps BEFORE exposition
+  void changeFocus (int steps);
+};
+
+class Rts2DevClientFocusFoc:public Rts2DevClientFocus
+{
+protected:
+  virtual void focusingEnd ();
+public:
+    Rts2DevClientFocusFoc (Rts2Conn * in_connection);
+  virtual void postEvent (Rts2Event * event);
 };
 
 class Rts2ConnFocus:public Rts2ConnFork
 {
   char *img_path;
+  Rts2DevClientCameraFoc *camera;
 public:
     Rts2ConnFocus (Rts2DevClientCameraFoc * in_camera, Rts2Image * in_image,
 		   const char *in_exe);
