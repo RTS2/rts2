f71d6bbcbbb64f391a7477f4f5d8e0ed02100717
Petr Kubánek <petr@kubanek.net>
2005-11-04 15:37:01 +0000

    [bug-fix] nan values
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@2074 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/libnova_cpp.cpp
+++ src/utils/libnova_cpp.cpp
@@ -1,41 +1,63 @@
 #include "libnova_cpp.h"
 
+#include <math.h>
 #include <iomanip>
 
 std::ostream & operator << (std::ostream & _os, LibnovaRa l_ra)
 {
+  if (isnan (l_ra.ra))
+    {
+      _os << std::setw (11) << "nan";
+      return _os;
+    }
   struct ln_hms ra_hms;
   ln_deg_to_hms (l_ra.ra, &ra_hms);
   char old_fill = _os.fill ('0');
+  int old_precison = _os.precision (2);
   _os << std::setw (2) << ra_hms.hours << ":"
     << std::setw (2) << ra_hms.minutes << ":"
     << std::setw (5) << ra_hms.seconds;
+  _os.precision (old_precison);
   _os.fill (old_fill);
   return _os;
 }
 
 std::ostream & operator << (std::ostream & _os, LibnovaDeg l_deg)
 {
+  if (isnan (l_deg.deg))
+    {
+      _os << std::setw (13) << "nan";
+      return _os;
+    }
   struct ln_dms deg_dms;
   ln_deg_to_dms (l_deg.deg, &deg_dms);
   char old_fill = _os.fill ('0');
+  int old_precison = _os.precision (2);
   _os << (deg_dms.neg ? '-' : '+')
     << std::setw (3) << deg_dms.degrees << ":"
     << std::setw (2) << deg_dms.minutes << ":"
     << std::setw (5) << deg_dms.seconds;
+  _os.precision (old_precison);
   _os.fill (old_fill);
   return _os;
 }
 
 std::ostream & operator << (std::ostream & _os, LibnovaDeg90 l_deg)
 {
+  if (isnan (l_deg.deg))
+    {
+      _os << std::setw (12) << "nan";
+      return _os;
+    }
   struct ln_dms deg_dms;
   ln_deg_to_dms (l_deg.deg, &deg_dms);
   char old_fill = _os.fill ('0');
+  int old_precison = _os.precision (2);
   _os << (deg_dms.neg ? '-' : '+')
     << std::setw (2) << deg_dms.degrees << ":"
     << std::setw (2) << deg_dms.minutes << ":"
     << std::setw (5) << deg_dms.seconds;
+  _os.precision (old_precison);
   _os.fill (old_fill);
   return _os;
 }
