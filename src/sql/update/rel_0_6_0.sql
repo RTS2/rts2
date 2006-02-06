8c7077a5679d2f35a88b6e431f270b5b04cab75d
Petr Kubánek <petr@kubanek.net>
2006-02-06 16:52:28 +0000

    * add plan stuff to DB
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@2254 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/sql/update/rel_0_6_0.sql
@@ -0,0 +1,30 @@
+CREATE TABLE plan (
+	plan_id		integer PRIMARY KEY NOT NULL,
+	tar_id		integer REFERENCES targets(tar_id) NOT NULL,
+	prop_id		integer REFERENCES props(prop_id),
+	obs_id		integer REFERENCES observations(obs_id),
+	plan_start	timestamp NOT NULL,
+	plan_status	integer NOT NULL DEFAULT 0
+);
+
+CREATE SEQUENCE plan_id;
+
+CREATE INDEX plan_plan_start ON plan (plan_start);
+
+CREATE TABLE props (
+	prop_id		integer PRIMARY KEY NOT NULL,
+	tar_id		integer REFERENCES targets(tar_id),
+	user_id		integer REFERENCES users (usr_id),
+	prop_start	timestamp,
+	prop_end	timestamp
+);
+
+CREATE SEQUENCE prop_id;
+
+COPY types FROM stdin;
+p	Plan target
+\.
+
+COPY targets (tar_id, type_id, tar_name, tar_ra, tar_dec, tar_comment, tar_enabled, tar_priority, tar_bonus, tar_bonus_time) FROM stdin;
+7	p	Master plan	0	0	Observe by plan	t	100	0	\N
+\.
