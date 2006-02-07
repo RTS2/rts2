10311b43de0de7689af774272b2c1e6125cbbd98
Petr Kubánek <petr@kubanek.net>
2006-02-07 14:46:49 +0000

    * prop_id sequence
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@2264 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sql/update/rel_0_6_0.sql
+++ src/sql/update/rel_0_6_0.sql
@@ -1,3 +1,14 @@
+CREATE TABLE props (
+	prop_id		integer PRIMARY KEY NOT NULL,
+	tar_id		integer REFERENCES targets(tar_id),
+	user_id		integer REFERENCES users (usr_id),
+	prop_start	timestamp,
+	prop_end	timestamp
+);
+
+CREATE SEQUENCE prop_id;
+
+
 CREATE TABLE plan (
 	plan_id		integer PRIMARY KEY NOT NULL,
 	tar_id		integer REFERENCES targets(tar_id) NOT NULL,
@@ -10,16 +21,6 @@ CREATE TABLE plan (
 CREATE SEQUENCE plan_id;
 
 CREATE INDEX plan_plan_start ON plan (plan_start);
-
-CREATE TABLE props (
-	prop_id		integer PRIMARY KEY NOT NULL,
-	tar_id		integer REFERENCES targets(tar_id),
-	user_id		integer REFERENCES users (usr_id),
-	prop_start	timestamp,
-	prop_end	timestamp
-);
-
-CREATE SEQUENCE prop_id;
 
 COPY types FROM stdin;
 p	Plan target
