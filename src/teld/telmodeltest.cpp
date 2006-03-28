869ca859dfccc172e69a596dd77628732117bfed
Petr Kubánek <petr@kubanek.net>
2006-03-28 14:32:11 +0000

    * fixed model test
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@2437 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/teld/telmodeltest.cpp
+++ src/teld/telmodeltest.cpp
@@ -1,12 +1,23 @@
 #include "model/telmodel.h"
+#include "../utils/libnova_cpp.h"
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 class Rts2DevTelescopeModelTest:public Rts2DevTelescope
 {
 public:
   Rts2DevTelescopeModelTest ():Rts2DevTelescope (0, NULL)
   {
+    telLongtitude = Rts2Config::instance ()->getObserver ()->lng;
+    telLatitude = Rts2Config::instance ()->getObserver ()->lat;
+  }
+
+  void setObserverLat (double in_lat)
+  {
+    telLatitude = in_lat;
   }
 };
 
@@ -15,14 +26,19 @@ class TelModelTest:public Rts2App
 private:
   char *modelFile;
   Rts2TelModel *model;
+    std::vector < std::string > runFiles;
+  Rts2DevTelescopeModelTest *telescope;
+  int verbose;
 
   void test (double ra, double dec);
-
+  void runOnFile (std::string filename, std::ostream & os);
 protected:
     virtual int processOption (int in_opt);
+  virtual int processArgs (const char *arg);
 
 public:
     TelModelTest (int in_argc, char **in_argv);
+    virtual ~ TelModelTest (void);
 
   virtual int init ();
   virtual int run ();
@@ -35,7 +51,15 @@ Rts2App (in_argc, in_argv)
   config = Rts2Config::instance ();
   modelFile = NULL;
   model = NULL;
+  verbose = 0;
   addOption ('m', "model-file", 1, "Model file to use");
+  addOption ('v', "verbose", 0, "Report model progress");
+}
+
+TelModelTest::~TelModelTest (void)
+{
+  delete telescope;
+  runFiles.clear ();
 }
 
 int
@@ -46,9 +70,19 @@ TelModelTest::processOption (int in_opt)
     case 'm':
       modelFile = optarg;
       break;
+    case 'v':
+      verbose++;
+      break;
     default:
       return Rts2App::processOption (in_opt);
     }
+  return 0;
+}
+
+int
+TelModelTest::processArgs (const char *in_arg)
+{
+  runFiles.push_back (in_arg);
   return 0;
 }
 
@@ -60,7 +94,9 @@ TelModelTest::init ()
   if (ret)
     return ret;
 
-  model = new Rts2TelModel (new Rts2DevTelescopeModelTest (), modelFile);
+  telescope = new Rts2DevTelescopeModelTest ();
+
+  model = new Rts2TelModel (telescope, modelFile);
   ret = model->load ();
   return ret;
 }
@@ -76,11 +112,119 @@ TelModelTest::test (double ra, double dec)
     ra << " " << pos.dec << std::endl;
 }
 
+void
+TelModelTest::runOnFile (std::string filename, std::ostream & os)
+{
+  char caption[81];
+  double temp;
+  double press;
+  std::ifstream is (filename.c_str ());
+  char firstChar;
+  bool latLine = false;
+
+  is.getline (caption, 80);
+  os << caption << std::endl;
+  while (!is.eof ())
+    {
+      // ignore
+      firstChar = is.get ();
+      if (firstChar == ':')
+	{
+	  is.getline (caption, 80);
+	  os << firstChar << caption << std::endl;
+	}
+      else if (firstChar == 'E')
+	{
+	  std::string nd;
+	  is >> nd;
+	  if (nd == "ND")
+	    {
+	      os << "END" << std::endl;
+	      return;
+	    }
+	  std::cerr << "Invalid end? E" << nd << std::endl;
+	}
+      // first line contains lat
+      else if (!latLine)
+	{
+	  struct ln_dms lat;
+	  struct ln_date date;
+	  date.hours = 0;
+	  date.minutes = 0;
+	  date.seconds = 0;
+	  // get sign
+	  is >> firstChar >> lat.degrees >> lat.minutes >> lat.
+	    seconds >> date.years >> date.months >> date.
+	    days >> temp >> press;
+	  if (is.fail ())
+	    return;
+	  is.ignore (2000, is.widen ('\n'));
+	  if (firstChar == '-')
+	    lat.neg = 1;
+	  else
+	    lat.neg = 0;
+	  telescope->setObserverLat (ln_dms_to_deg (&lat));
+	  latLine = true;
+	  os << " " << LibnovaDeg (&lat) << " "
+	    << date.years << " "
+	    << date.months << " "
+	    << date.days << " " << temp << " " << press << " " << std::endl;
+	}
+      // data lines
+      else
+	{
+	  double aux1;
+	  double aux2;
+	  int m, d;
+	  double epoch;
+
+	  LibnovaRaDec _in;
+	  LibnovaRaDec _out;
+	  LibnovaHaM lst;
+
+	  is >> _in >> m >> d >> epoch >> _out >> lst >> aux1 >> aux2;
+	  is.ignore (2000, is.widen ('\n'));
+	  if (is.fail ())
+	    {
+	      std::cerr << "Failed during file read" << std::endl;
+	      return;
+	    }
+	  // calculate model position, output it,..
+	  struct ln_equ_posn pos;
+	  _out.getPos (&pos);
+	  pos.ra = ln_range_degrees (lst.getRa () - pos.ra);
+	  if (verbose > 0)
+	    model->applyVerbose (&pos);
+	  else
+	    model->apply (&pos);
+	  // print it out
+	  pos.ra = ln_range_degrees (lst.getRa () - pos.ra);
+	  std::cout.precision (1);
+	  LibnovaRaDec _out_in (&pos);
+	  std::cout << "  " << _out_in << " "
+	    << m << " "
+	    << d << " " << epoch << "   " << _out << "   " << lst << " ";
+	  std::cout.precision (6);
+	  std::cout << aux1 << " " << aux2 << std::endl;
+	}
+    }
+}
+
 int
 TelModelTest::run ()
 {
-  test (10, 20);
-  test (30, 50);
+  if (!runFiles.empty ())
+    {
+      for (std::vector < std::string >::iterator iter = runFiles.begin ();
+	   iter != runFiles.end (); iter++)
+	runOnFile ((*iter), std::cout);
+    }
+  else
+    {
+      // some generic tests
+      test (10, 20);
+      test (30, 50);
+    }
   return 0;
 }
 
