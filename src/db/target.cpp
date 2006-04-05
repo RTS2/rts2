9e13b67de5c25aa84f594c67faff6b0ab5114d50
Petr Kubánek <petr@kubanek.net>
2006-04-05 23:51:08 +0000

    * -p option (target priority) * -C and -s options (script)
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@2482 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/db/target.cpp
+++ src/db/target.cpp
@@ -9,23 +9,42 @@
 #define OP_ENABLE	0x03
 #define OP_DISABLE	0x01
 #define OP_MASK_EN	0x03
-#define OP_BONUS	0x04
-#define OP_BONUS_TIME	0x08
+#define OP_PRIORITY	0x04
+#define OP_BONUS	0x08
+#define OP_BONUS_TIME	0x10
+#define OP_SCRIPT	0x20
 
-class Rts2Target:public Rts2AppDb
+class CamScript
+{
+public:
+  const char *cameraName;
+  const char *script;
+
+    CamScript (const char *in_cameraName, const char *in_script)
+  {
+    cameraName = in_cameraName;
+    script = in_script;
+  }
+};
+
+class Rts2TargetApp:public Rts2AppDb
 {
 private:
   int op;
     std::list < int >tar_ids;
   Rts2TargetSet *target_set;
 
+  float new_priority;
   float new_bonus;
   time_t new_bonus_time;
 
+  const char *camera;
+    std::list < CamScript > new_scripts;
+
   int runInteractive ();
 public:
-    Rts2Target (int argc, char **argv);
-    virtual ~ Rts2Target (void);
+    Rts2TargetApp (int argc, char **argv);
+    virtual ~ Rts2TargetApp (void);
 
   virtual int processOption (int in_opt);
 
@@ -34,27 +53,33 @@ public:
   virtual int run ();
 };
 
-Rts2Target::Rts2Target (int in_argc, char **in_argv):
+Rts2TargetApp::Rts2TargetApp (int in_argc, char **in_argv):
 Rts2AppDb (in_argc, in_argv)
 {
   target_set = NULL;
   op = OP_NONE;
+  new_priority = nan ("f");
   new_bonus = nan ("f");
+
+  camera = NULL;
 
   addOption ('e', "enable", 0, "enable given targets");
   addOption ('d', "disable", 0,
 	     "disable given targets (they will not be picked up by selector");
+  addOption ('p', "priority", 1, "set target (fixed) priority");
   addOption ('b', "bonus", 1, "set target bonus to this value");
   addOption ('B', "bonus_time", 1, "set target bonus time to this value");
+  addOption ('C', "camera", 1, "next script will be set for given camera");
+  addOption ('s', "script", 1, "set script for target and camera");
 }
 
-Rts2Target::~Rts2Target ()
+Rts2TargetApp::~Rts2TargetApp ()
 {
   delete target_set;
 }
 
 int
-Rts2Target::processOption (int in_opt)
+Rts2TargetApp::processOption (int in_opt)
 {
   int ret;
   struct tm tm_ret;
@@ -64,23 +89,39 @@ Rts2Target::processOption (int in_opt)
       if (op & OP_DISABLE)
 	return -1;
       op |= OP_ENABLE;
-      return 0;
+      break;
     case 'd':
       if (op & OP_ENABLE)
 	return -1;
       op |= OP_DISABLE;
-      return 0;
+      break;
+    case 'p':
+      new_priority = atof (optarg);
+      op |= OP_PRIORITY;
+      break;
     case 'b':
       new_bonus = atof (optarg);
       op |= OP_BONUS;
-      return 0;
+      break;
     case 'B':
       ret = parseDate (optarg, &tm_ret);
       if (ret)
 	return ret;
       new_bonus_time = mktime (&tm_ret);
       op |= OP_BONUS_TIME;
-      return 0;
+      break;
+    case 'C':
+      if (camera)
+	return -1;
+      camera = optarg;
+      break;
+    case 's':
+      if (!camera)
+	return -1;
+      new_scripts.push_back (CamScript (camera, optarg));
+      camera = NULL;
+      op |= OP_SCRIPT;
+      break;
     default:
       return Rts2AppDb::processOption (in_opt);
     }
@@ -88,7 +129,7 @@ Rts2Target::processOption (int in_opt)
 }
 
 int
-Rts2Target::processArgs (const char *arg)
+Rts2TargetApp::processArgs (const char *arg)
 {
   int new_tar_id;
   char *endptr;
@@ -102,7 +143,7 @@ Rts2Target::processArgs (const char *arg)
 }
 
 int
-Rts2Target::init ()
+Rts2TargetApp::init ()
 {
   int ret;
 
@@ -117,7 +158,7 @@ Rts2Target::init ()
 }
 
 int
-Rts2Target::runInteractive ()
+Rts2TargetApp::runInteractive ()
 {
   Rts2AskChoice selection = Rts2AskChoice (this);
   selection.addChoice ('e', "Enable target(s)");
@@ -157,7 +198,7 @@ Rts2Target::runInteractive ()
 }
 
 int
-Rts2Target::run ()
+Rts2TargetApp::run ()
 {
   target_set = new Rts2TargetSet (tar_ids);
   if ((op & OP_MASK_EN) == OP_ENABLE)
@@ -168,6 +209,10 @@ Rts2Target::run ()
     {
       target_set->setTargetEnabled (false);
     }
+  if (op & OP_PRIORITY)
+    {
+      target_set->setTargetPriority (new_priority);
+    }
   if (op & OP_BONUS)
     {
       target_set->setTargetBonus (new_bonus);
@@ -176,6 +221,15 @@ Rts2Target::run ()
     {
       target_set->setTargetBonusTime (&new_bonus_time);
     }
+  if (op & OP_SCRIPT)
+    {
+      for (std::list < CamScript >::iterator iter = new_scripts.begin ();
+	   iter != new_scripts.end (); iter++)
+	{
+	  target_set->setTargetScript (iter->cameraName, iter->script);
+	}
+    }
+
   if (op == OP_NONE)
     {
       return runInteractive ();
@@ -188,8 +242,8 @@ int
 main (int argc, char **argv)
 {
   int ret;
-  Rts2Target *app;
-  app = new Rts2Target (argc, argv);
+  Rts2TargetApp *app;
+  app = new Rts2TargetApp (argc, argv);
   ret = app->init ();
   if (ret)
     return 1;
