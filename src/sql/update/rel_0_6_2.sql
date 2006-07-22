47106a6195e3d137921960df6e38e93898575ed4
Petr Kubánek <petr@kubanek.net>
2006-07-22 15:27:55 +0000

    * flats & auger
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@2692 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/sql/update/rel_0_6_2.sql
@@ -0,0 +1,17 @@
+CREATE TABLE flats ( 
+        obs_id		integer REFERENCES observations(obs_id),
+	img_id		integer NOT NOT,
+	flat_date	timestamp,
+	flat_date_usec  integer,
+	flat_exposure	integer,
+	flat_temperature integer,
+	epoch_id	integer NOT NULL REFERENCES epoch(epoch_id),
+	camera_name	varchar(8) REFERENCES cameras(camera_name),
+CONSTRAINT flats_prim_key PRIMARY KEY (obs_id, img_id)
+);
+
+ALTER TABLE auger ADD COLUMN auger_ra   	float8;
+ALTER TABLE auger ADD COLUMN auger_dec		float8;
+
+ALTER TABLE auger ALTER auger_ra  SET NOT NULL;
+ALTER TABLE auger ALTER auger_dec SET NOT NULL;
