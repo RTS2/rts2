dc00386c852ccda3aedcfdafde300e733a7703aa
Petr Kubánek <petr@kubanek.net>
2007-01-20 19:22:05 +0000

    * add filters
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@3067 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sql/update/rel_0_7_0.sql
+++ src/sql/update/rel_0_7_0.sql
@@ -11,3 +11,25 @@ CREATE INDEX message_time ON message (message_time);
 CREATE INDEX message_oname ON message (message_oname);
 
 CREATE INDEX message_type ON message (message_type);
+
+-- filters infomration
+CREATE TABLE filters (
+	filter_id	varchar(3) PRIMARY KEY,
+	offset_ra	float8,
+	offset_dec	float8,
+	-- standart name - UBVR & others
+	standart_name	varchar(10),
+	description	varchar(2000),
+	-- medium wavelenght in nm
+	medium_wl	float,
+	-- filter width in nm
+	width		float
+);
+
+UPDATE images SET img_filter = 'UNK' WHERE img_filter is NULL;
+
+INSERT INTO filters (filter_id, standart_name) SELECT DISTINCT img_filter, img_filter FROM images;
+
+ALTER TABLE images ADD CONSTRAINT "fk_images_filters" FOREIGN KEY (img_filter) REFERENCES filters(filter_id);
+
+CREATE INDEX images_filters ON images (img_filter);
