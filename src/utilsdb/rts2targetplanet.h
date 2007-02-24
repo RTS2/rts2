5f8a255bd10f6284f3baa1c0af6012ede65c8b54
Petr Kubánek <petr@kubanek.net>
2007-02-24 18:04:33 +0000

    * earth, solar distance & more
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@3220 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utilsdb/rts2targetplanet.h
+++ src/utilsdb/rts2targetplanet.h
@@ -4,11 +4,18 @@
 #include "target.h"
 
 typedef void (*get_equ_t) (double, struct ln_equ_posn *);
+typedef double (*get_double_val_t) (double);
 
 typedef struct planet_info_t
 {
   char *name;
-  get_equ_t function;
+  get_equ_t rst_func;
+  get_double_val_t earth_func;
+  get_double_val_t sun_func;
+  get_double_val_t mag_func;
+  get_double_val_t sdiam_func;
+  get_double_val_t phase_func;
+  get_double_val_t disk_func;
 };
 
 class TargetPlanet:public Target
@@ -25,6 +32,13 @@ public:
 
   virtual int isContinues ();
   virtual void printExtra (std::ostream & _os, double JD);
+
+  virtual double getEarthDistance (double JD);
+  virtual double getSolarDistance (double JD);
+  virtual double getMagnitude (double JD);
+  double getSDiam (double JD);
+  double getPhase (double JD);
+  double getDisk (double JD);
 };
 
 #endif /*! __RTS2_TARGETPLANET__ */
