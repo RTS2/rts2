6e8270733ab5686f0170eb511dfb2173d8c290a2
Petr Kubánek <petr@kubanek.net>
2007-03-27 10:43:47 +0000

    * readout value set when not exposing
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@3319 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/camd/dummy.cpp
+++ src/camd/dummy.cpp
@@ -95,7 +95,7 @@ Rts2DevCameraDummy (int in_argc, char **in_argv):Rts2DevCamera (in_argc,
   {
     supportFrameT = false;
     infoSleep = 0;
-    createValue (readoutSleep, "readout", "readout sleep in sec", true,
+    createValue (readoutSleep, "readout", "readout sleep in sec", true, 0,
 		 CAM_EXPOSING | CAM_READING | CAM_DATA);
     readoutSleep->setValueDouble (0);
 
