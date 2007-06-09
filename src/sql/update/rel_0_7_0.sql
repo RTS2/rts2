ab2c059464da5e6f6afff7924485a57f1994fb1e
Petr Kubánek <petr@kubanek.net>
2007-06-09 02:11:47 +0000

    * remove planets
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@3654 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sql/update/rel_0_7_0.sql
+++ src/sql/update/rel_0_7_0.sql
@@ -37,16 +37,3 @@ CREATE INDEX images_filters ON images (img_filter);
 COPY types FROM stdin;
 L	Planets
 \.
-
-COPY targets (tar_id, type_id, tar_name, tar_ra, tar_dec, tar_comment, tar_enabled, tar_priority, tar_bonus, tar_bonus_time) FROM stdin;
-190	L	Sun	0	0	Sun (planet)	t	0	0	\N
-191	L	Mercury	0	0	Mercury (planet)	t	0	0	\N
-192	L	Venus	0	0	Venus (planet)	t	0	0	\N
-193	L	Moon	0	0	Moon (Earth)	t	0	0	\N
-194	L	Mars	0	0	Mars (planet)	t	0	0	\N
-195	L	Jupiter	0	0	Jupiter (planet)	t	0	0	\N
-196	L	Saturn	0	0	Saturn (planet)	t	0	0	\N
-197	L	Urans	0	0	Uran (planet)	t	0	0	\N
-198	L	Neptune	0	0	Neptun (planet)	t	0	0	\N
-199	L	Pluto	0	0	Pluto (planet)	t	0	0	\N
-\.
