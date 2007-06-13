aeb003f83101ff4f3c0159f824e4148d3bbe30d0
Petr Kubánek <petr@kubanek.net>
2007-06-13 20:48:12 +0000

    [bug-fix] move works??
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@3692 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sensord/a3200.cpp
+++ src/sensord/a3200.cpp
@@ -10,6 +10,8 @@ class Rts2DevSensorA3200:public Rts2DevSensor
 {
 private:
   HAERCTRL hAerCtrl;
+  AXISINDEX mAxis;
+
   Rts2ValueDouble *ax1;
   Rts2ValueDouble *ax2;
   Rts2ValueDouble *ax3;
@@ -17,6 +19,9 @@ private:
   LPCTSTR initFile;
 
   void logErr (char *proc, AERERR_CODE eRc);
+
+  int home ();
+  int moveAxis (AXISINDEX ax, LONG tar);
 
 protected:
     virtual int setValue (Rts2Value * old_value, Rts2Value * new_value);
@@ -40,8 +45,73 @@ Rts2DevSensorA3200::logErr (char *proc, AERERR_CODE eRc)
 }
 
 int
+Rts2DevSensorA3200::home ()
+{
+  AERERR_CODE eRc;
+  eRc = AerMoveMEnable (hAerCtrl, mAxis);
+  if (eRc != AERERR_NOERR)
+    {
+      logErr ("home MoveMEnable", eRc);
+      return -1;
+    }
+  eRc = AerMoveMWaitDone (hAerCtrl, mAxis, 100, 0);
+  if (eRc != AERERR_NOERR)
+    {
+      logErr ("home MoveMWait for Enable", eRc);
+      return -1;
+    }
+  logStream (MESSAGE_DEBUG) << "All axis enabled, homing" << sendLog;
+  eRc = AerMoveMHome (hAerCtrl, mAxis);
+  if (eRc != AERERR_NOERR)
+    {
+      logErr ("home MHome for Enable", eRc);
+      return -1;
+    }
+  eRc = AerMoveMWaitDone (hAerCtrl, mAxis, 100, 0);
+  if (eRc != AERERR_NOERR)
+    {
+      logErr ("home MoveMWait for Home", eRc);
+      return -1;
+    }
+  logStream (MESSAGE_DEBUG) << "All axis homed properly" << sendLog;
+  return 0;
+}
+
+int
+Rts2DevSensorA3200::moveAxis (AXISINDEX ax, LONG tar)
+{
+  AERERR_CODE eRc;
+  eRc = AerMoveAbsolute (hAerCtrl, ax, tar, 100000);
+  if (eRc != AERERR_NOERR)
+    {
+      logErr ("moveAxis MoveAbsolute", eRc);
+      return -1;
+    }
+  logStream (MESSAGE_DEBUG) << "Moving " << ax << " to " << tar << sendLog;
+  eRc = AerMoveWaitDone (hAerCtrl, ax, 10000, 0);
+  if (eRc != AERERR_NOERR)
+    {
+      logErr ("home MoveMWait for Home", eRc);
+      return -1;
+    }
+  return 0;
+}
+
+int
 Rts2DevSensorA3200::setValue (Rts2Value * old_value, Rts2Value * new_value)
 {
+  if (old_value == ax1)
+    {
+      return moveAxis (AXISINDEX_1, new_value->getValueLong ());
+    }
+  if (old_value == ax2)
+    {
+      return moveAxis (AXISINDEX_2, new_value->getValueLong ());
+    }
+  if (old_value == ax3)
+    {
+      return moveAxis (AXISINDEX_3, new_value->getValueLong ());
+    }
   return Rts2DevSensor::setValue (old_value, new_value);
 }
 
@@ -49,6 +119,8 @@ Rts2DevSensorA3200::Rts2DevSensorA3200 (int in_argc, char **in_argv):Rts2DevSens
 	       in_argv)
 {
   hAerCtrl = NULL;
+
+  mAxis = AXISMASK_1 | AXISMASK_2 | AXISMASK_3;
 
   createValue (ax1, "AX1", "first axis", true);
   createValue (ax2, "AX2", "second axis", true);
@@ -92,13 +164,13 @@ Rts2DevSensorA3200::init ()
       logErr ("init AerSysInitialize", eRc);
       return -1;
     }
-  return 0;
+  ret = home ();
+  return ret;
 }
 
 int
 Rts2DevSensorA3200::info ()
 {
-  DWORD mAxis;
   DWORD dwUnits;
   DWORD dwDriveStatus[MAX_REQUESTED];
   DWORD dwAxisStatus[MAX_REQUESTED];
@@ -109,7 +181,6 @@ Rts2DevSensorA3200::info ()
 
   AERERR_CODE eRc;
 
-  mAxis = AXISMASK_1 | AXISMASK_2 | AXISMASK_3;	// three axis
   dwUnits = 0;			// we want counts as units for the pos/vels
 
   eRc = AerStatusGetAxisInfoEx (hAerCtrl, mAxis, dwUnits,
@@ -125,7 +196,7 @@ Rts2DevSensorA3200::info ()
   ax1->setValueDouble (dPosition[0]);
   ax2->setValueDouble (dPosition[1]);
   ax3->setValueDouble (dPosition[2]);
-  return 0;
+  return Rts2DevSensor::info ();
 }
 
 int
