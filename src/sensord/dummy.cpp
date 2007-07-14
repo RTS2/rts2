6cf5cb0208f1ee18b3cbc4c838064cc7e98a2d0f
Petr Kubánek <petr@kubanek.net>
2007-07-14 00:13:04 +0000

    * add Stat value
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@3838 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sensord/dummy.cpp
+++ src/sensord/dummy.cpp
@@ -2,14 +2,28 @@
 
 class Rts2DevSensorDummy:public Rts2DevSensor
 {
+private:
+  Rts2ValueDoubleStat * statTest;
 public:
   Rts2DevSensorDummy (int in_argc, char **in_argv):Rts2DevSensor (in_argc,
 								  in_argv)
   {
+    createValue (statTest, "test_stat", "test stat value", true);
   }
-  virtual int ready ()
+
+  virtual int commandAuthorized (Rts2Conn * conn)
   {
-    return -1;
+    if (conn->isCommand ("add"))
+      {
+	double aval;
+	if (conn->paramNextDouble (&aval) || !conn->paramEnd ())
+	  return -2;
+	statTest->addValue (aval);
+	statTest->calculate ();
+	infoAll ();
+	return 0;
+      }
+    return Rts2DevSensor::commandAuthorized (conn);
   }
 };
 
