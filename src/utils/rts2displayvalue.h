048fcb1c51b21ea7eb11165920c31076c9ce4ace
Petr Kubánek <petr@kubanek.net>
2007-07-29 20:24:40 +0000

    * change of displayvalue
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@3896 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2displayvalue.h
+++ src/utils/rts2displayvalue.h
@@ -4,32 +4,6 @@
 #include "rts2value.h"
 #include "libnova_cpp.h"
 
-std::string
-getDisplayValue (Rts2Value * value)
-{
-  std::ostringstream _os;
-  const char *tmp_val;
-  switch (value->getValueDisplayType ())
-    {
-    case RTS2_DT_RA:
-      _os << LibnovaRa (value->getValueDouble ());
-      break;
-    case RTS2_DT_DEC:
-      _os << LibnovaDeg90 (value->getValueDouble ());
-      break;
-    case RTS2_DT_DEGREES:
-      _os << LibnovaDeg (value->getValueDouble ());
-      break;
-    case RTS2_DT_DEG_DIST:
-      _os << LibnovaDegDist (value->getValueDouble ());
-      break;
-    default:
-      tmp_val = value->getDisplayValue ();
-      if (tmp_val)
-	return std::string (tmp_val);
-      return std::string ("");
-    }
-  return _os.str ();
-}
+std::string getDisplayValue (Rts2Value * value);
 
 #endif /* !__RTS2_DISPLAYVALUE__ */
