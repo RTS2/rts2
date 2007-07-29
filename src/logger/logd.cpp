978b97245466d9dceccde54fffe88b1d9c4602fb
Petr Kubánek <petr@kubanek.net>
2007-07-29 23:05:37 +0000

    * logd completed and tested
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@3899 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/logger/logd.cpp
+++ src/logger/logd.cpp
@@ -3,13 +3,64 @@
 
 class Rts2Logd:public Rts2Device, public Rts2LoggerBase
 {
+private:
+  Rts2ValueString * logConfig;
+  int setLogConfig (const char *new_config);
+protected:
+    virtual int setValue (Rts2Value * old_value, Rts2Value * new_value);
+  virtual int processArgs (const char *arg);
+  virtual int willConnect (Rts2Address * in_addr);
 public:
-  Rts2Logd (int in_argc, char **in_argv);
+    Rts2Logd (int in_argc, char **in_argv);
+  virtual Rts2DevClient *createOtherType (Rts2Conn * conn,
+					  int other_device_type);
 };
 
 Rts2Logd::Rts2Logd (int in_argc, char **in_argv):
 Rts2Device (in_argc, in_argv, DEVICE_TYPE_LOGD, "LOGD")
 {
+  setTimeout (USEC_SEC);
+  createValue (logConfig, "log_config", "logging configuration file", false);
+}
+
+int
+Rts2Logd::setLogConfig (const char *new_config)
+{
+  std::ifstream * istream = new std::ifstream (new_config);
+  int ret = readDevices (*istream);
+  delete istream;
+  if (ret)
+    return ret;
+  logConfig->setValueString (new_config);
+  return ret;
+}
+
+int
+Rts2Logd::setValue (Rts2Value * old_value, Rts2Value * new_value)
+{
+  if (old_value == logConfig)
+    {
+      return (setLogConfig (new_value->getValue ()) == 0) ? 0 : -2;
+    }
+  return Rts2Device::setValue (old_value, new_value);
+}
+
+int
+Rts2Logd::processArgs (const char *arg)
+{
+  return setLogConfig (arg);
+}
+
+int
+Rts2Logd::willConnect (Rts2Address * in_addr)
+{
+  return Rts2LoggerBase::willConnect (in_addr);
+}
+
+Rts2DevClient *
+Rts2Logd::createOtherType (Rts2Conn * conn, int other_device_type)
+{
+  return Rts2LoggerBase::createOtherType (conn, other_device_type);
 }
 
 int
