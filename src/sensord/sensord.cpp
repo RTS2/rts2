7c076a04989d335401575da1052f5167fd1bb5be
Petr Kubánek <petr@kubanek.net>
2007-09-03 10:04:29 +0000

    * set timeout to 60 seconds
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@3946 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sensord/sensord.cpp
+++ src/sensord/sensord.cpp
@@ -3,7 +3,7 @@
 Rts2DevSensor::Rts2DevSensor (int in_argc, char **in_argv):
 Rts2Device (in_argc, in_argv, DEVICE_TYPE_SENSOR, "S1")
 {
-  setIdleInfoInterval (5);
+  setIdleInfoInterval (60);
 }
 
 Rts2DevSensor::~Rts2DevSensor (void)
