b78f743f139697790e90faa303af393b12932c03
Petr Kubánek <petr@kubanek.net>
2007-10-26 15:19:14 +0000

    * comments added
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_5_0_exp@4113 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2connfork.h
+++ src/utils/rts2connfork.h
@@ -1,8 +1,39 @@
+/* 
+ * Forking connection handling.
+ * Copyright (C) 2003-2007 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #ifndef __RTS2_CONN_FORK__
 #define __RTS2_CONN_FORK__
 
 #include "rts2connnosend.h"
 
+/**
+ * Class representing forked connection, used to controll process which runs in
+ * paraller with current execution line.
+ *
+ * This object is ussually created and added to connections list. It's standard
+ * write and read methods are called when some data become available from the
+ * pipe to which new process writes standard output.
+ *
+ * @author Petr Kubanek <petr@kubanek.net>
+ *
+ * @addgroup RTS2Block
+ */
 class Rts2ConnFork:public Rts2ConnNoSend
 {
 	private:
@@ -22,6 +53,15 @@ class Rts2ConnFork:public Rts2ConnNoSend
 			0);
 		virtual ~ Rts2ConnFork (void);
 
+		/**
+		 * Create and execute processing.
+		 *
+		 * BIG FAT WARNINIG: This method is called after sucessfull
+		 * fork call. Due to the fact that fork keeps all sockets from
+		 * parent process opened, you may not use any DB call in
+		 * newProcess or call any call which operates with DB (executes
+		 * ECPG statements).
+		 */
 		virtual int newProcess ();
 
 		virtual int init ();
