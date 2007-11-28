3451b2ba242dcf15cd76c97e44eea77a4babaada
Petr Kubánek <petr@kubanek.net>
2007-11-28 14:25:04 +0000

    * added getObStart()..
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_7_3@4165 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utilsdb/rts2obs.h
+++ src/utilsdb/rts2obs.h
@@ -1,3 +1,23 @@
+/* 
+ * Observation entry.
+ * Copyright (C) 2005-2007 Petr Kubanek <petr@kubanek.net>
+ * Copyright (C) 2007 Stanislav Vitek <standa@iaa.es>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #ifndef __RTS2_OBS_DB__
 #define __RTS2_OBS_DB__
 
@@ -18,7 +38,7 @@
  * This class contains observation. They are (optionaly) linked to images.
  * It can calculate (and display) various statistic on images it contains.
  *
- * @author petr
+ * @author Petr Kubanek <petr@kubanek.net>
  */
 class Rts2Obs
 {
@@ -96,6 +116,26 @@ class Rts2Obs
 		int getObsId ()
 		{
 			return obs_id;
+		}
+
+		double getObsStart ()
+		{
+			return obs_start;
+		}
+
+		double getObsEnd ()
+		{
+			return obs_end;
+		}
+
+		double getObsRa ()
+		{
+			return obs_ra;
+		}
+
+		double getObsDec ()
+		{
+			return obs_dec;
 		}
 
 		Rts2ImgSet *getImageSet ()
