0cc86d8fe7a5f1bbca3bbcd26f735aa554ece9d2
Petr Kubánek <petr@kubanek.net>
2007-11-28 19:25:58 +0000

    * prints information about target
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_7_3@4171 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/plan/targetinfo.cpp
+++ src/plan/targetinfo.cpp
@@ -1,3 +1,22 @@
+/* 
+ * Prints informations about target.
+ * Copyright (C) 2003-2007 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include "imgdisplay.h"
 #include "../utilsdb/rts2appdb.h"
 #include "../utilsdb/rts2camlist.h"
@@ -235,7 +254,8 @@ Rts2TargetInfo::printTargetInfo ()
 {
 	if (!(printImages & DISPLAY_FILENAME))
 	{
-		target->sendInfo (std::cout, JD);
+		Rts2InfoValOStream ivos (&std::cout);
+		target->sendInfo (ivos, JD);
 		// print scripts..
 		Rts2CamList::iterator cam_names;
 		if (printExtendet)
@@ -246,7 +266,7 @@ Rts2TargetInfo::printTargetInfo ()
 
 				std::cout << "==================================" << std::
 					endl << "Date: " << LibnovaDate (JD) << std::endl;
-				target->sendPositionInfo (std::cout, JD);
+				target->sendPositionInfo (ivos, JD);
 			}
 		}
 		for (cam_names = cameras.begin (); cam_names != cameras.end ();
