c1f180285e7ba8c6dd3e25be222c726d2bcfbca8
Petr Kubánek <petr@kubanek.net>
2007-11-30 10:02:56 +0000

    * added license
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_7_3@4180 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2app.h
+++ src/utils/rts2app.h
@@ -1,3 +1,22 @@
+/* 
+ * Application sceleton.
+ * Copyright (C) 2003-2007 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #ifndef __RTS2_APP__
 #define __RTS2_APP__
 
@@ -44,56 +63,57 @@ class Rts2App:public Rts2Object
 		virtual void help ();
 		virtual int init ();
 
-	public:  Rts2App (int in_argc, char **in_argv);
-	virtual ~ Rts2App ();
+	public:
+		Rts2App (int in_argc, char **in_argv);
+		virtual ~ Rts2App ();
 
-	/**
-	 * Run method of the application.
-	 *
-	 * Application is responsible for calling init () method to read
-	 * variables etc..
-	 *
-	 * Both Rts2Daemon and Rts2AppBase define run method, as this method is
-	 * pure virtual.
-	 */
-	virtual int run () = 0;
+		/**
+		 * Run method of the application.
+		 *
+		 * Application is responsible for calling init () method to read
+		 * variables etc..
+		 *
+		 * Both Rts2Daemon and Rts2AppBase define run method, as this method is
+		 * pure virtual.
+		 */
+		virtual int run () = 0;
 
-	/**
-	 * If running in loop, caused end of loop.
-	 */
-	virtual void endRunLoop ()
-	{
-		exit (0);
-	}
+		/**
+		 * If running in loop, caused end of loop.
+		 */
+		virtual void endRunLoop ()
+		{
+			exit (0);
+		}
 
-	bool getEndLoop ()
-	{
-		return end_loop;
-	}
+		bool getEndLoop ()
+		{
+			return end_loop;
+		}
 
-	void setEndLoop (bool in_end_loop)
-	{
-		end_loop = in_end_loop;
-	}
+		void setEndLoop (bool in_end_loop)
+		{
+			end_loop = in_end_loop;
+		}
 
-	/**
-	 * Ask user for char, used to ask for chair in choice question.
-	 */
-	int askForChr (const char *desc, char &out);
+		/**
+		 * Ask user for char, used to ask for chair in choice question.
+		 */
+		int askForChr (const char *desc, char &out);
 
-	virtual void sendMessage (messageType_t in_messageType,
-		const char *in_messageString);
-	inline void sendMessage (messageType_t in_messageType,
-		std::ostringstream & _os);
+		virtual void sendMessage (messageType_t in_messageType,
+			const char *in_messageString);
+		inline void sendMessage (messageType_t in_messageType,
+			std::ostringstream & _os);
 
-	virtual Rts2LogStream logStream (messageType_t in_messageType);
+		virtual Rts2LogStream logStream (messageType_t in_messageType);
 
-	// send mail to recepient; requires /usr/bin/mail binary; if master object is specified, that object
-	// method forkedInstance will be called, which might close whenever descriptors are needed
-	virtual int sendMailTo (const char *subject, const char *text,
-		const char *in_mailAddress);
+		// send mail to recepient; requires /usr/bin/mail binary; if master object is specified, that object
+		// method forkedInstance will be called, which might close whenever descriptors are needed
+		virtual int sendMailTo (const char *subject, const char *text,
+			const char *in_mailAddress);
 
-	virtual void sigHUP (int sig);
+		virtual void sigHUP (int sig);
 };
 
 Rts2App *getMasterApp ();
