ed6ff46824fcdc6c4a540a04c85f927ef595d2c6
Petr Kubánek <petr@kubanek.net>
2007-12-18 00:04:14 +0000

    * ask for queEmpty before finishing script
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_7_3@4237 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/plan/rts2devscript.cpp
+++ src/plan/rts2devscript.cpp
@@ -1,3 +1,22 @@
+/*
+ * Client for script devices.
+ * Copyright (C) 2004-2007 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include "rts2devscript.h"
 #include "rts2execcli.h"
 
@@ -495,7 +514,8 @@ Rts2DevScript::haveNextCommand ()
 		return 0;
 	}
 	ret = nextPreparedCommand ();
-	if (ret < 0)
+	// only end when we do not have any commands in que
+	if (ret < 0 && script_connection->queEmpty ())
 	{
 		deleteScript ();
 		#ifdef DEBUG_EXTRA
