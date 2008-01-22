e4029ad29aae3f991b7f166ca42eaebfd98ba801
Petr Kubánek <petr@kubanek.net>
2008-01-22 14:32:57 +0000

    * new copyright notice
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_7_3@4315 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sensord/a3200.cpp
+++ src/sensord/a3200.cpp
@@ -1,3 +1,22 @@
+/* 
+ * Driver for Aerotech asix controller.
+ * Copyright (C) 2007-2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include "sensord.h"
 
 #include <Windows.h>
@@ -154,8 +173,8 @@ Rts2DevSensorA3200::setValue (Rts2Value * old_value, Rts2Value * new_value)
 }
 
 
-Rts2DevSensorA3200::Rts2DevSensorA3200 (int in_argc, char **in_argv):Rts2DevSensor (in_argc,
-in_argv)
+Rts2DevSensorA3200::Rts2DevSensorA3200 (int in_argc, char **in_argv)
+:Rts2DevSensor (in_argc, in_argv)
 {
 	hAerCtrl = NULL;
 
