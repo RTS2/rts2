a42a57a939e3c251b3c2e604ae69d7fb4ee08799
Petr Kubánek <petr@kubanek.net>
2008-01-29 13:45:49 +0000

    * better indent of rts2event.h * EVENT_NUMBER_OF_IMAGES
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_7_3@4338 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2event.h
+++ src/utils/rts2event.h
@@ -1,33 +1,55 @@
+/* 
+ * Basic event definition.
+ * Copyright (C) 2003-2007 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #ifndef __RTS2_EVENT__
 #define __RTS2_EVENT__
 
-#define EVENT_SET_TARGET_ID 1
-#define EVENT_WRITE_TO_IMAGE    2
-#define EVENT_WRITE_TO_IMAGE_ENDS 13
-#define EVENT_SET_TARGET  3
-#define EVENT_OBSERVE           4
-#define EVENT_IMAGE_OK    5
+#define EVENT_SET_TARGET_ID         1
+#define EVENT_WRITE_TO_IMAGE        2
+#define EVENT_WRITE_TO_IMAGE_ENDS  13
+#define EVENT_SET_TARGET            3
+#define EVENT_OBSERVE               4
+#define EVENT_IMAGE_OK              5
 
-#define EVENT_QUERY_WAIT  6
-#define EVENT_ENTER_WAIT  7
-#define EVENT_CLEAR_WAIT  8
+#define EVENT_QUERY_WAIT            6
+#define EVENT_ENTER_WAIT            7
+#define EVENT_CLEAR_WAIT            8
 
-#define EVENT_GET_RADEC   10
-#define EVENT_MOUNT_CHANGE  11
+#define EVENT_GET_RADEC            10
+#define EVENT_MOUNT_CHANGE         11
 
-#define EVENT_QUICK_ENABLE  12
+#define EVENT_QUICK_ENABLE         12
 
 // info failed/sucess calls
-#define EVENT_INFO_DEVCLI_OK  14
-#define EVENT_INFO_DEVCLI_FAILED  15
+#define EVENT_INFO_DEVCLI_OK       14
+#define EVENT_INFO_DEVCLI_FAILED   15
 
 /** Event issued when command return with OK state. */
-#define EVENT_COMMAND_OK  16
+#define EVENT_COMMAND_OK           16
 /** Event issued when command failed. */
-#define EVENT_COMMAND_FAILED  17
+#define EVENT_COMMAND_FAILED       17
+
+/** Number of images which waits for processing. */
+#define EVENT_NUMBER_OF_IMAGES     18
 
 // events number below that number shoudl be considered RTS2-reserved
-#define RTS2_LOCAL_EVENT   1000
+#define RTS2_LOCAL_EVENT         1000
 
 // local events are defined in local headers!
 // so far those offeset ranges (from RTS2_LOCAL_EVENT) are reserved for:
