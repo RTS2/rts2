86c946a8c36d335bc434782353a1f041e386340e
Petr Kubánek <petr@kubanek.net>
2008-02-11 09:28:26 +0000

    * copyright
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_7_3@4390 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/db/rts2targetapp.h
+++ src/db/rts2targetapp.h
@@ -1,3 +1,22 @@
+/* 
+ * Skeleton for applications which works with targets.
+ * Copyright (C) 2006-2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #ifndef __RTS2_TARGETAPP__
 #define __RTS2_TARGETAPP__
 
@@ -6,9 +25,19 @@
 #include "../utilsdb/rts2appdb.h"
 #include "../utilsdb/target.h"
 
+/**
+ * Base of the target application.
+ *
+ * Provides routines for asking for target and resolving its name using Simbad.
+ *
+ * @author Petr Kubanek <petr@kubanek.net>
+ */
 class Rts2TargetApp:public Rts2AppDb
 {
 	protected:
+		/**
+		 * Pointer to Target structure holding actual target.
+		 */
 		Target * target;
 		struct ln_lnlat_posn *obs;
 		int askForDegrees (const char *desc, double &val);
