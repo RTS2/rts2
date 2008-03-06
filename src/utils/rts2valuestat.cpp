26792f133d3b36306b0dbfab444f4e6b8c39046a
Petr Kubánek <petr@kubanek.net>
2008-03-06 14:50:12 +0000

    * changed stat base type to deque
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_7_3@4464 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2valuestat.cpp
+++ src/utils/rts2valuestat.cpp
@@ -1,3 +1,22 @@
+/*
+ * Variables which do some basic statistics computations.
+ * Copyright (C) 2007 - 2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include "rts2valuestat.h"
 #include "rts2conn.h"
 
@@ -18,13 +37,13 @@ Rts2ValueDoubleStat::calculate ()
 {
 	if (valueList.size () == 0)
 		return;
-	std::vector < double >sorted = valueList;
+	std::deque < double >sorted = valueList;
 	std::sort (sorted.begin (), sorted.end ());
 	min = *(sorted.begin ());
 	max = *(--sorted.end ());
 	numMes = valueList.size ();
 	double sum = 0;
-	for (std::vector < double >::iterator iter = sorted.begin ();
+	for (std::deque < double >::iterator iter = sorted.begin ();
 		iter != sorted.end (); iter++)
 	{
 		sum += (*iter);
@@ -32,7 +51,7 @@ Rts2ValueDoubleStat::calculate ()
 	setValueDouble (sum / numMes);
 	// calculate stdev
 	stdev = 0;
-	for (std::vector < double >::iterator iter = sorted.begin ();
+	for (std::deque < double >::iterator iter = sorted.begin ();
 		iter != sorted.end (); iter++)
 	{
 		sum = *iter - getValueDouble ();
--- src/utils/rts2valuestat.h
+++ src/utils/rts2valuestat.h
@@ -1,12 +1,34 @@
+/*
+ * Variables which do some basic statistics computations.
+ * Copyright (C) 2007 - 2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #ifndef __RTS2_VALUESTAT__
 #define __RTS2_VALUESTAT__
 
 #include "rts2value.h"
 
-#include <vector>
+#include <deque>
 
 /**
- * This class holds values which were obtained from quick measurements.
+ * This class holds values which were obtained from series
+ * of measurements.
+ *
+ * @author Petr Kubanek <petr@kubanek.net>
  */
 class Rts2ValueDoubleStat:public Rts2ValueDouble
 {
@@ -16,12 +38,20 @@ class Rts2ValueDoubleStat:public Rts2ValueDouble
 		double min;
 		double max;
 		double stdev;
-		std::vector < double >valueList;
+		std::deque < double >valueList;
 	public:
 		Rts2ValueDoubleStat (std::string in_val_name);
 		Rts2ValueDoubleStat (std::string in_val_name, std::string in_description,
 			bool writeToFits = true, int32_t flags = 0);
+
+		/**
+		 * Clear values in value list.
+		 */
 		void clearStat ();
+
+		/**
+		 * Calculate values statistics.
+		 */
 		void calculate ();
 
 		virtual int setValue (Rts2Conn * connection);
@@ -40,11 +70,21 @@ class Rts2ValueDoubleStat:public Rts2ValueDouble
 			return mode;
 		}
 
+		/**
+		 * Return minimal value.
+		 *
+		 * @return Minimal value.
+		 */
 		double getMin ()
 		{
 			return min;
 		}
 
+		/**
+		 * Return maximal value.
+		 *
+		 * @return Maximal value.
+		 */
 		double getMax ()
 		{
 			return max;
@@ -55,11 +95,16 @@ class Rts2ValueDoubleStat:public Rts2ValueDouble
 			return stdev;
 		}
 
-		std::vector < double >&getMesList ()
+		std::deque < double >&getMesList ()
 		{
 			return valueList;
 		}
 
+		/**
+		 * Add value to the measurement values.
+		 *
+		 * @param in_val Value which will be added.
+		 */
 		void addValue (double in_val)
 		{
 			valueList.push_back (in_val);
