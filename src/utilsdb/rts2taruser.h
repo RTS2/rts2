4d64df9280711c251c457a023f7387db677ff876
Petr Kubánek <petr@kubanek.net>
2008-03-26 10:52:33 +0000

    * verifyUser
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@4559 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utilsdb/rts2taruser.h
+++ src/utilsdb/rts2taruser.h
@@ -1,3 +1,22 @@
+/* 
+ * User access.
+ * Copyright (C) 2005-2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #ifndef __RTS2_TARUSER__
 #define __RTS2_TARUSER__
 
@@ -62,4 +81,15 @@ class Rts2TarUser
 		// returns users for given event
 		std::string getUsers (int in_event_mask, int &count);
 };
+
+/**
+ * Verify username and password combination.
+ *
+ * @param username   User login name.
+ * @param pass       User password.
+ *
+ * @return True if login and password is correct, false otherwise.
+ */
+bool verifyUser (std::string username, std::string pass);
+
 #endif							 /* !__RTS2_TARUSER__ */
