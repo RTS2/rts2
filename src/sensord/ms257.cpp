c2675d2762940946969449b571eb3fb39b47851f
Petr Kubánek <petr@kubanek.net>
2008-04-03 22:59:57 +0000

    * changed to Rts2ConnSerial
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@4611 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sensord/ms257.cpp
+++ src/sensord/ms257.cpp
@@ -18,12 +18,7 @@
  */
 
 #include "sensord.h"
-
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <termios.h>
-#include <unistd.h>
+#include "../utils/rts2connserial.h"
 #include <errno.h>
 
 class Rts2DevSensorMS257:public Rts2DevSensor
@@ -41,7 +36,7 @@ class Rts2DevSensorMS257:public Rts2DevSensor
 		Rts2ValueInteger *msteps;
 		Rts2ValueInteger *grat;
 
-		int dev_port;
+		Rts2ConnSerial *ms257Dev;		
 
 		void resetDevice ();
 		int writePort (const char *str);
@@ -75,7 +70,7 @@ void
 Rts2DevSensorMS257::resetDevice ()
 {
 	sleep (5);
-	tcflush (dev_port, TCIOFLUSH);
+	ms257Dev->flushPortIO ();
 	logStream (MESSAGE_DEBUG) << "Device " << dev << " reseted." << sendLog;
 }
 
@@ -84,7 +79,7 @@ int
 Rts2DevSensorMS257::writePort (const char *str)
 {
 	int ret;
-	ret = write (dev_port, str, strlen (str));
+	ret = ms257Dev->writePort (str, strlen (str));
 	#ifdef DEBUG_EXTRA
 	logStream (MESSAGE_DEBUG) << "Writing '" << str << "' (" << strlen (str) <<
 		")" << sendLog;
@@ -94,7 +89,7 @@ Rts2DevSensorMS257::writePort (const char *str)
 		resetDevice ();
 		return -1;
 	}
-	ret = write (dev_port, "\n", 1);
+	ret = ms257Dev->writePort ("\n", 1);
 	if (ret != 1)
 	{
 		resetDevice ();
@@ -111,27 +106,10 @@ Rts2DevSensorMS257::readPort (char **rstr, const char *cmd)
 	int i = 0;
 	int ret;
 
-	while (true)
-	{
-		ret = read (dev_port, buf + i, 1);
-		if (ret != 1)
-		{
-			logStream (MESSAGE_ERROR) << "Cannot read from device: " << errno <<
-				" (" << strerror (errno) << ")" << sendLog;
-			resetDevice ();
-			return -1;
-		}
-		if (buf[i] == '>')
-			break;
-		i++;
-		if (i >= 20)
-		{
-			logStream (MESSAGE_ERROR) <<
-				"Runaway reply string (more then 20 characters)" << sendLog;
-			resetDevice ();
-			return -1;
-		}
-	}
+	ret = ms257Dev->readPort (buf, 50, '>');
+	if (ret == -1)
+		return ret;
+
 	// check that we match \cr\lf[E|value]>
 	if (buf[0] != '\n' || buf[1] != '\n')
 	{
@@ -264,7 +242,7 @@ Rts2ValueInteger * val)
 Rts2DevSensorMS257::Rts2DevSensorMS257 (int in_argc, char **in_argv):
 Rts2DevSensor (in_argc, in_argv)
 {
-	dev_port = -1;
+	ms257Dev = NULL;
 	dev = "/dev/ttyS0";
 
 	createValue (msVer, "version", "version of MS257", false);
@@ -292,7 +270,7 @@ Rts2DevSensor (in_argc, in_argv)
 
 Rts2DevSensorMS257::~Rts2DevSensorMS257 ()
 {
-	close (dev_port);
+	delete ms257Dev;
 }
 
 
@@ -366,76 +344,18 @@ Rts2DevSensorMS257::processOption (int in_opt)
 int
 Rts2DevSensorMS257::init ()
 {
-	struct termios term_options; /* structure to hold serial port configuration */
 	int ret;
 
 	ret = Rts2DevSensor::init ();
 	if (ret)
 		return ret;
 
-	dev_port = open (dev, O_RDWR | O_NOCTTY | O_NDELAY);
-
-	if (dev_port == -1)
-	{
-		logStream (MESSAGE_ERROR) << "filter ifw init cannot open: " << dev
-			<< strerror (errno) << sendLog;
-		return -1;
-	}
-	ret = fcntl (dev_port, F_SETFL, 0);
+	ms257Dev = new Rts2ConnSerial (dev, this, BS9600, C8, NONE, 200);
+	ret = ms257Dev->init ();
 	if (ret)
-	{
-		logStream (MESSAGE_ERROR) << "filter ifw init cannot fcntl " <<
-			strerror (errno) << sendLog;
-	}
-	/* get current serial port configuration */
-	if (tcgetattr (dev_port, &term_options) < 0)
-	{
-		logStream (MESSAGE_ERROR) <<
-			"filter ifw init error reading serial port configuration: " <<
-			strerror (errno) << sendLog;
-		return -1;
-	}
+		return ret;
 
-	/*
-	 * Set the baud rates to 9600
-	 */
-	if (cfsetospeed (&term_options, B9600) < 0
-		|| cfsetispeed (&term_options, B9600) < 0)
-	{
-		logStream (MESSAGE_ERROR) << "filter ifw init error setting baud rate: "
-			<< strerror (errno) << sendLog;
-		return -1;
-	}
-
-	/*
-	 * Enable the receiver and set local mode...
-	 */
-	term_options.c_cflag |= (CLOCAL | CREAD);
-
-	/* Choose raw input */
-	term_options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-
-	/* set port to 8 data bits, 1 stop bit, no parity */
-	term_options.c_cflag &= ~PARENB;
-	term_options.c_cflag &= ~CSTOPB;
-	term_options.c_cflag &= ~CSIZE;
-	term_options.c_cflag |= CS8;
-
-	/* set timeout  to 20 seconds */
-	term_options.c_cc[VTIME] = 200;
-	term_options.c_cc[VMIN] = 0;
-
-	tcflush (dev_port, TCIOFLUSH);
-
-	/*
-	 * Set the new options for the port...
-	 */
-	if (tcsetattr (dev_port, TCSANOW, &term_options))
-	{
-		logStream (MESSAGE_ERROR) << "filter ifw init tcsetattr " <<
-			strerror (errno) << sendLog;
-		return -1;
-	}
+	ms257Dev->flushPortIO ();
 
 	// char *rstr;
 
