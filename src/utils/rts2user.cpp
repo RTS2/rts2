0580834ba3dd031d48f7f2443d505798879a4466
Petr Kubánek <petr@kubanek.net>
2008-04-06 12:57:22 +0000

    changed class name to Rts2ConnUser
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@4634 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2user.cpp
+++ src/utils/rts2user.cpp
@@ -1,9 +1,28 @@
+/* 
+ * Management class for connection users.
+ * Copyright (C) 2005-2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include <string.h>
 
 #include "rts2user.h"
 #include "status.h"
 
-Rts2User::Rts2User (int in_centralId, int in_priority, char in_priority_have,
+Rts2ConnUser::Rts2ConnUser (int in_centralId, int in_priority, char in_priority_have,
 const char *in_login)
 {
 	centralId = in_centralId;
@@ -14,13 +33,13 @@ const char *in_login)
 }
 
 
-Rts2User::~Rts2User (void)
+Rts2ConnUser::~Rts2ConnUser (void)
 {
 }
 
 
 int
-Rts2User::update (int in_centralId, int new_priority, char new_priority_have,
+Rts2ConnUser::update (int in_centralId, int new_priority, char new_priority_have,
 const char *new_login)
 {
 	if (in_centralId != centralId)
--- src/utils/rts2user.h
+++ src/utils/rts2user.h
@@ -1,9 +1,33 @@
-#ifndef __RTS2_USER__
-#define __RTS2_USER__
+/* 
+ * Management class for connection users.
+ * Copyright (C) 2005-2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __RTS2_CONN_USER__
+#define __RTS2_CONN_USER__
 
 #include "status.h"
 
-class Rts2User
+/**
+ * Holder class for users on Rts2Conn.
+ *
+ * @author Petr Kubanek <petr@kubanek.net>
+ */
+class Rts2ConnUser
 {
 	private:
 		int centralId;
@@ -11,10 +35,10 @@ class Rts2User
 		char havePriority;
 		char login[DEVICE_NAME_SIZE];
 	public:
-		Rts2User (int in_centralId, int in_priority, char in_priority_have,
+		Rts2ConnUser (int in_centralId, int in_priority, char in_priority_have,
 			const char *in_login);
-		virtual ~ Rts2User (void);
+		virtual ~ Rts2ConnUser (void);
 		int update (int in_centralId, int new_priority, char new_priority_have,
 			const char *new_login);
 };
-#endif							 /* !__RTS2_USER__ */
+#endif							 /* !__RTS2_CONN_USER__ */
