8b4ad6d638ec45d6b5cbf155a1b049d5ef3bd631
Petr Kubánek <petr@kubanek.net>
2008-04-08 17:28:26 +0000

    updates for rts2-user
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@4649 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2app.h
+++ src/utils/rts2app.h
@@ -1,6 +1,6 @@
 /* 
  * Application sceleton.
- * Copyright (C) 2003-2007 Petr Kubanek <petr@kubanek.net>
+ * Copyright (C) 2003-2008 Petr Kubanek <petr@kubanek.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -108,8 +108,7 @@ class Rts2App:public Rts2Object
 		 * @param in_help_msg      Help message printed to the user.
 		 * @return -1 on failure, 0 on success.
 		 */
-		int addOption (int in_short_option, char *in_long_option, int in_has_arg,
-			char *in_help_msg);
+		int addOption (int in_short_option, char *in_long_option, int in_has_arg, char *in_help_msg);
 
 		/**
 		 * Ask user for integer.
--- src/utils/rts2askchoice.cpp
+++ src/utils/rts2askchoice.cpp
@@ -1,3 +1,22 @@
+/* 
+ * Interface to ask users among diferent options.
+ * Copyright (C) 2003-2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include "rts2askchoice.h"
 
 std::ostream & operator << (std::ostream & _os, Rts2Choice & choice)
@@ -15,14 +34,13 @@ Rts2AskChoice::Rts2AskChoice (Rts2App * in_app)
 
 Rts2AskChoice::~Rts2AskChoice (void)
 {
-	choices.clear ();
 }
 
 
 void
 Rts2AskChoice::addChoice (char key, const char *desc)
 {
-	choices.push_back (Rts2Choice (key, desc));
+	push_back (Rts2Choice (key, desc));
 }
 
 
@@ -30,8 +48,8 @@ char
 Rts2AskChoice::query (std::ostream & _os)
 {
 	char selection;
-	for (std::list < Rts2Choice >::iterator iter = choices.begin ();
-		iter != choices.end (); iter++)
+	for (Rts2AskChoice::iterator iter = begin ();
+		iter != end (); iter++)
 	{
 		_os << (*iter);
 	}
