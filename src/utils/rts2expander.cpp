3b5f20e32b59eb0cf2798957ebdc9700d9c12836
Petr Kubánek <petr@kubanek.net>
2008-04-17 11:55:14 +0000

    setExpanderDate () method
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@4684 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2expander.cpp
+++ src/utils/rts2expander.cpp
@@ -1,3 +1,22 @@
+/* 
+ * Expanding mechanism.
+ * Copyright (C) 2007-2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include "rts2block.h"
 #include "rts2expander.h"
 #include "rts2config.h"
@@ -7,10 +26,8 @@
 
 Rts2Expander::Rts2Expander ()
 {
-	struct timeval tv;
-	gettimeofday (&tv, NULL);
 	epochId = -1;
-	setExpandDate (&tv);
+	setExpandDate ();
 }
 
 
@@ -206,6 +223,15 @@ std::string Rts2Expander::expand (std::string expression)
 		}
 	}
 	return ret;
+}
+
+
+void
+Rts2Expander::setExpandDate ()
+{
+	struct timeval tv;
+	gettimeofday (&tv, NULL);
+	setExpandDate (&tv);
 }
 
 
--- src/utils/rts2expander.h
+++ src/utils/rts2expander.h
@@ -1,9 +1,29 @@
+/* 
+ * Expanding mechanism.
+ * Copyright (C) 2007-2008 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #ifndef __RTS2_EXPANDER__
 #define __RTS2_EXPANDER__
 
 #include <string>
 #include <time.h>
 #include <sys/time.h>
+
 /**
  * This class is common ancestor to expending mechanism.
  * Short one-letter variables are prefixed with %, two letters and longer
@@ -55,6 +75,9 @@ class Rts2Expander
 		}
 
 	protected:
+		/**
+		 * ID of current epoch.
+		 */
 		int epochId;
 		virtual std::string expandVariable (char var);
 		virtual std::string expandVariable (std::string expression);
@@ -64,6 +87,17 @@ class Rts2Expander
 		Rts2Expander (Rts2Expander * in_expander);
 		virtual ~ Rts2Expander (void);
 		std::string expand (std::string expression);
+		/**
+		 * Sets expanding date to current sysdate.
+		 */
+		void setExpandDate ();
+		
+		/**
+		 * Sets expanding date. This date is used in construction of
+		 * the filename (for %y%d.. expansions).
+		 *
+		 * @param tv Timeval holding date to set.
+		 */
 		void setExpandDate (const struct timeval *tv);
 		double getExpandDateCtime ();
 		const struct timeval *getExpandDate ();
