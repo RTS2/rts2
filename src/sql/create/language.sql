04a8228efc0d4b63f54090f2ea2fb7b6f5df4032
Petr Kubánek <petr@kubanek.net>
2008-10-16 01:00:04 +0000

    removed $id$
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@5133 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sql/create/language.sql
+++ src/sql/create/language.sql
@@ -1,5 +1,4 @@
 -- creates PLPGSQL language
--- $Id: language.sql 1934 2005-09-06 09:16:26Z petr $
 
 CREATE FUNCTION plpgsql_call_handler () RETURNS LANGUAGE_HANDLER AS
    '$libdir/plpgsql' LANGUAGE C;
