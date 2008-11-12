191eee34b7491048ddf4f451aeb69d896c7fee07
Petr Kubánek <petr@kubanek.net>
2008-11-12 23:53:00 +0000

    removed SNOW finishing transition to new dome model
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@5177 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/dome/ir.cpp
+++ src/dome/ir.cpp
@@ -23,12 +23,15 @@
 
 #include <libnova/libnova.h>
 
+namespace rts2dome
+{
+
 /**
  * Driver for Bootes IR dome.
  *
  * @author Petr Kubanek <petr@kubanek.net>
  */
-class Rts2DevDomeIR:public Rts2DevDome
+class Ir:public Dome
 {
 	private:
 		std::string ir_ip;
@@ -46,10 +49,6 @@ class Rts2DevDomeIR:public Rts2DevDome
 
 		Rts2ValueBool *domeAutotrack;
 
-		enum { D_OPENED, D_OPENING, D_CLOSING, D_CLOSED } dome_state;
-
-		int getSNOW (float *temp, float *humi, float *wind);
-
 		/**
 		 * Set dome autotrack.
 		 *
@@ -66,25 +65,27 @@ class Rts2DevDomeIR:public Rts2DevDome
 		virtual int setValue (Rts2Value *old_value, Rts2Value *new_value);
 
 	public:
-		Rts2DevDomeIR (int argc, char **argv);
-		virtual ~ Rts2DevDomeIR (void);
+		Ir (int argc, char **argv);
+		virtual ~ Ir (void);
 		virtual int init ();
-		virtual int idle ();
 
-		virtual int ready ();
-		virtual int off ();
-		virtual int standby ();
-		virtual int observing ();
-		virtual int baseInfo ();
 		virtual int info ();
 
-		virtual int openDome ();
-		virtual int closeDome ();
+		virtual int startOpen ();
+		virtual long isOpened ();
+		virtual int endOpen ();
+
+		virtual int startClose ();
+		virtual long isClosed ();
+		virtual int endClose ();
 };
 
+}
+
+using namespace rts2dome;
 
 int
-Rts2DevDomeIR::setValue (Rts2Value *old_value, Rts2Value *new_value)
+Ir::setValue (Rts2Value *old_value, Rts2Value *new_value)
 {	
 	int status = TPL_OK;
 	if (old_value == domeAutotrack)
@@ -128,40 +129,88 @@ Rts2DevDomeIR::setValue (Rts2Value *old_value, Rts2Value *new_value)
 		return 0;
 
 	}
-	return Rts2DevDome::setValue (old_value, new_value);
+	return Dome::setValue (old_value, new_value);
 }
 
 
 int
-Rts2DevDomeIR::openDome ()
+Ir::startOpen ()
 {
 	int status = TPL_OK;
-	dome_state = D_OPENING;
 	status = irConn->tpl_set ("DOME[1].TARGETPOS", 1, &status);
 	status = irConn->tpl_set ("DOME[2].TARGETPOS", 1, &status);
 	logStream (MESSAGE_INFO) << "opening dome, status " << status << sendLog;
 	if (status != TPL_OK)
 		return -1;
-	return Rts2DevDome::openDome ();
+	return 0;
+}
+
+
+long
+Ir::isOpened ()
+{
+	int status = TPL_OK;
+	double pos1, pos2;
+	status = irConn->tpl_get ("DOME[1].CURRPOS", pos1, &status);
+	status = irConn->tpl_get ("DOME[2].CURRPOS", pos2, &status);
+
+	if (status != TPL_OK)
+		return -1;
+
+	if (pos1 == 1 && pos2 == 1)
+		return -2;
+
+	return USEC_SEC;
 }
 
 
 int
-Rts2DevDomeIR::closeDome ()
+Ir::endOpen ()
+{
+	return 0;
+}
+
+
+int
+Ir::startClose ()
 {
 	int status = TPL_OK;
-	dome_state = D_CLOSING;
 	status = irConn->tpl_set ("DOME[1].TARGETPOS", 0, &status);
 	status = irConn->tpl_set ("DOME[2].TARGETPOS", 0, &status);
 	logStream (MESSAGE_INFO) << "closing dome, status " << status << sendLog;
 	if (status != TPL_OK)
 		return -1;
-	return Rts2DevDome::closeDome ();
+	return 0;
+}
+
+
+long
+Ir::isClosed ()
+{
+	int status = TPL_OK;
+	double pos1, pos2;
+	status = irConn->tpl_get ("DOME[1].CURRPOS", pos1, &status);
+	status = irConn->tpl_get ("DOME[2].CURRPOS", pos2, &status);
+
+	if (status != TPL_OK)
+		return -1;
+
+	if (pos1 == 0 && pos2 == 0)
+		return -2;
+
+	return USEC_SEC;
 }
 
 
 int
-Rts2DevDomeIR::processOption (int in_opt)
+Ir::endClose ()
+{
+	return 0;
+}
+
+
+int
+Ir::processOption (int in_opt)
 {
 	switch (in_opt)
 	{
@@ -172,16 +221,17 @@ Rts2DevDomeIR::processOption (int in_opt)
 			ir_port = atoi (optarg);
 			break;
 		default:
-			return Rts2DevDome::processOption (in_opt);
+			return Dome::processOption (in_opt);
 	}
 	return 0;
 }
 
 
-Rts2DevDomeIR::Rts2DevDomeIR (int argc, char **argv)
-:Rts2DevDome (argc, argv)
+Ir::Ir (int argc, char **argv)
+:Dome (argc, argv)
 {
-	domeModel = "IR";
+	ir_ip = std::string ("");
+	ir_port = 0;
 
 	createValue (domeAutotrack, "dome_auto_track", "dome auto tracking", false);
 	domeAutotrack->setValueBool (true);
@@ -196,109 +246,17 @@ Rts2DevDomeIR::Rts2DevDomeIR (int argc, char **argv)
 
 	addOption ('I', "ir_ip", 1, "IR TCP/IP address");
 	addOption ('N', "ir_port", 1, "IR TCP/IP port number");
-
-	dome_state = D_OPENING;
 }
 
 
-Rts2DevDomeIR::~Rts2DevDomeIR (void)
+Ir::~Ir (void)
 {
 
 }
 
 
 int
-Rts2DevDomeIR::getSNOW (float *temp, float *humi, float *wind)
-{
-	int sockfd, bytes_read, ret, i, j = 0;
-	struct sockaddr_in dest;
-	char buffer[300], buff[300];
-	fd_set rfds;
-	struct timeval tv;
-	double wind1, wind2;
-	char *token;
-
-	if ((sockfd = socket (PF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		fprintf (stderr, "unable to create socket");
-		return 1;
-	}
-
-	bzero (&dest, sizeof (dest));
-	dest.sin_family = PF_INET;
-	dest.sin_addr.s_addr = inet_addr ("193.146.151.70");
-	dest.sin_port = htons (6341);
-	ret = connect (sockfd, (struct sockaddr *) &dest, sizeof (dest));
-	if (ret)
-	{
-		fprintf (stderr, "unable to connect to SNOW");
-		return 1;
-	}
-
-	/* send command to SNOW */
-	sprintf (buffer, "%s", "RCD");
-	ret = write (sockfd, buffer, strlen (buffer));
-	if (!ret)
-	{
-		fprintf (stderr, "unable to send command");
-		return 1;
-	}
-
-	FD_ZERO (&rfds);
-	FD_SET (sockfd, &rfds);
-
-	tv.tv_sec = 10;
-	tv.tv_usec = 0;
-
-	ret = select (FD_SETSIZE, &rfds, NULL, NULL, &tv);
-
-	if (ret == 1)
-	{
-		bytes_read = read (sockfd, buffer, sizeof (buffer));
-		if (bytes_read > 0)
-		{
-			for (i = 5; i < bytes_read; i++)
-			{
-				if (buffer[i] == ',')
-					buff[j++] = '.';
-				else
-					buff[j++] = buffer[i];
-			}
-		}
-	}
-	else
-	{
-		fprintf (stderr, ":-( no data");
-		return 1;
-	}
-
-	close (sockfd);
-
-	token = strtok ((char *) buff, "|");
-	for (j = 1; j < 40; j++)
-	{
-		token = (char *) strtok (NULL, "|#");
-		if (j == 3)
-			wind1 = atof (token);
-		if (j == 6)
-			wind2 = atof (token);
-		if (j == 18)
-			*temp = atof (token);
-		if (j == 24)
-			*humi = atof (token);
-	}
-
-	if (wind1 > wind2)
-		*wind = wind1;
-	else
-		*wind = wind2;
-
-	return 0;
-}
-
-
-int
-Rts2DevDomeIR::setDomeTrack (bool new_auto)
+Ir::setDomeTrack (bool new_auto)
 {
 	int status = TPL_OK;
 	int old_track;
@@ -316,7 +274,7 @@ Rts2DevDomeIR::setDomeTrack (bool new_auto)
 
 
 int
-Rts2DevDomeIR::initIrDevice ()
+Ir::initIrDevice ()
 {
 	Rts2Config *config = Rts2Config::instance ();
 	config->loadFile (NULL);
@@ -350,95 +308,34 @@ Rts2DevDomeIR::initIrDevice ()
 
 
 int
-Rts2DevDomeIR::info ()
+Ir::info ()
 {
-	double dome_curr_az, dome_target_az, dome_tar_dist, dome_power;
+	double dome_curr_az, dome_target_az, dome_tar_dist, dome_power, dome_up, dome_down;
 	int status = TPL_OK;
 	status = irConn->tpl_get ("DOME[0].CURRPOS", dome_curr_az, &status);
 	status = irConn->tpl_get ("DOME[0].TARGETPOS", dome_target_az, &status);
 	status = irConn->tpl_get ("DOME[0].TARGETDISTANCE", dome_tar_dist, &status);
 	status = irConn->tpl_get ("DOME[0].POWER", dome_power, &status);
+	status = irConn->tpl_get ("DOME[1].CURRPOS", dome_up, &status);
+	status = irConn->tpl_get ("DOME[2].CURRPOS", dome_down, &status);
 	if (status == TPL_OK)
 	{
 		domeCurrAz->setValueDouble (ln_range_degrees (dome_curr_az + 180));
 		domeTargetAz->setValueDouble (ln_range_degrees (dome_target_az + 180));
 		domeTarDist->setValueDouble (dome_tar_dist);
 		domePower->setValueBool (dome_power == 1);
-	}
-
-	if (dome_state == D_CLOSING && dome_state == D_OPENING)
-	{
-		status = TPL_OK;
-		double dome_up, dome_down;
-		status = irConn->tpl_get ("DOME[1].CURRPOS", dome_up, &status);
-		status = irConn->tpl_get ("DOME[2].CURRPOS", dome_down, &status);
-		if (status != TPL_OK)
-		{
-			logStream (MESSAGE_ERROR) << "unknow dome state" << sendLog;
-			return -1;
-		}
 		domeUp->setValueFloat (dome_up);
 		domeDown->setValueFloat (dome_down);
-		if (dome_up == 1.0 && dome_down == 1.0)
-			dome_state = D_OPENED;
-		if (dome_up == 0.0 && dome_down == 0.0)
-			dome_state = D_CLOSED;
 	}
 
-	//rain = weatherConn->getRain ();
-	float t_temp;
-	float t_hum;
-	float t_wind;
-
-	getSNOW (&t_temp, &t_hum, &t_wind);
-	setTemperature (t_temp);
-	setHumidity (t_hum);
-	setWindSpeed (t_wind);
-	return Rts2DevDome::info ();
+	return Dome::info ();
 }
 
 
 int
-Rts2DevDomeIR::baseInfo ()
+Ir::init ()
 {
-	return 0;
-}
-
-
-int
-Rts2DevDomeIR::off ()
-{
-	closeDome ();
-	return 0;
-}
-
-
-int
-Rts2DevDomeIR::standby ()
-{
-	closeDome ();
-	return 0;
-}
-
-
-int
-Rts2DevDomeIR::ready ()
-{
-	return 0;
-}
-
-
-int
-Rts2DevDomeIR::idle ()
-{
-	return Rts2DevDome::idle ();
-}
-
-
-int
-Rts2DevDomeIR::init ()
-{
-	int ret = Rts2DevDome::init ();
+	int ret = Dome::init ();
 	if (ret)
 		return ret;
 	return initIrDevice ();
@@ -446,17 +343,8 @@ Rts2DevDomeIR::init ()
 
 
 int
-Rts2DevDomeIR::observing ()
-{
-	if ((getState () & DOME_DOME_MASK) == DOME_CLOSED)
-		return openDome ();
-	return 0;
-}
-
-
-int
 main (int argc, char **argv)
 {
-	Rts2DevDomeIR device = Rts2DevDomeIR (argc, argv);
+	Ir device = Ir (argc, argv);
 	return device.run ();
 }
