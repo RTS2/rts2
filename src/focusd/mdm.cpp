b05796a1cdca5d8b20c1431e4d0cc9414dfe2943
Petr Kubánek <petr@kubanek.net>
2009-08-21 17:13:02 +0000

    MDM focuser
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_8_0@6165 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/focusd/mdm.cpp
@@ -0,0 +1,144 @@
+/*
+ * MDM focuser driver.
+ * Copyright (C) 2009 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "focusd.h"
+
+#include "tcsutils.h"
+
+
+namespace rts2focusd
+{
+
+/**
+ * MDM focuser driver. This is for MDM (Kitt Peak, Arizona) telescope.
+ *
+ * @author Petr Kubanek <petr@kubanek.net>
+ */
+class MDM:public Focusd
+{
+	public:
+		MDM (int argc, char **argv);
+		virtual ~ MDM (void);
+
+	protected:
+		virtual int isFocusing ();
+		virtual bool isAtStartPosition ();
+
+		virtual int processOption (int opt);
+		virtual int init ();
+		virtual int initValues ();
+		virtual int info ();
+		virtual int setTo (int num);
+
+	private:
+
+};
+
+};
+
+using namespace rts2focusd;
+
+MDM::MDM (int argc, char **argv):Focusd (argc, argv)
+{
+	tcssock = -1;
+	tcshost = "localhost";
+
+	addOption (OPT_TCSHOST, "tcshost", 1, "TCS host name at MDM (default to localhost)");
+}
+
+
+MDM::~MDM (void)
+{
+	close (tcssock);
+}
+
+
+int MDM::processOption (int opt)
+{
+	switch (opt)
+	{
+		case OPT_TCSHOST:
+			tcshost = optarg;
+			break;
+		default:
+			return Focusd::processOption (opt);
+	}
+	return 0;
+}
+
+
+int MDM::init ()
+{
+	int ret;
+	ret = Focusd::init ();
+	if (ret)
+		return ret;
+
+	tcssock = tcss_opensock_clnt ((char *) tcshost);	
+	if (tcssock < 0)
+	{
+		logStream (MESSAGE_ERROR) << "Cannot open connection to MDM TCS at " << tcshost << ", error " << strerror (errno) << sendLog;
+		return -1;
+	}
+	return info ();
+}
+
+
+int MDM::initValues ()
+{
+	focType = std::string ("MDM focuser");
+	return Focusd::initValues ();
+}
+
+
+int MDM::info ()
+{
+	int ret;
+	tcsinfo_t tcsi;
+	ret = tcss_reqcoords (tcssock, &tcsi, 0, 1);
+	if (ret < 0)
+		return ret;
+
+	position->setValueInteger (tcsi.focus);
+
+	return Focusd::info ();
+}
+
+
+int MDM::setTo (int num)
+{
+	return 0;
+}
+
+int MDM::isFocusing ()
+{
+	return 0;
+}
+
+
+bool MDM::isAtStartPosition ()
+{
+	return true;
+}
+
+int main (int argc, char **argv)
+{
+	MDM device = MDM (argc, argv);
+	return device.run ();
+}
