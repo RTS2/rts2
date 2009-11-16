1e0c41383c57feda01640740dc0e4871d419c324
Petr Kubánek <petr@kubanek.net>
2009-11-16 14:08:14 +0000

    added TempChannels
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@6545 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sensord/lakeshore-325.cpp
+++ src/sensord/lakeshore-325.cpp
@@ -1,3 +1,22 @@
+/* 
+ * Driver for Lakeshore 325 temperature controller.
+ * Copyright (C) 2009 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include "sensorgpib.h"
 
 #include "../utils/error.h"
@@ -5,6 +24,24 @@
 namespace rts2sensord
 {
 
+class Lakeshore;
+
+/**
+ * Values for a single channel.
+ *
+ * @author Petr Kubanek <petr@kubanek.net>
+ */
+class TempChannel:public std::map < const char*, Rts2Value *>
+{
+	public:
+		TempChannel () {};
+};
+
+/**
+ * Driver for Lakeshore 325 temperature controller.
+ *
+ * @author Petr Kubanek <petr@kubanek.net>
+ */
 class Lakeshore:public Gpib
 {
 	public:
@@ -13,15 +50,25 @@ class Lakeshore:public Gpib
 
 		virtual int info ();
 
-	private:
-		Rts2ValueFloat *tempA;
-		Rts2ValueFloat *tempB;
-
 	protected:
 		virtual int init ();
 		virtual int initValues ();
 
 		virtual int setValue (Rts2Value * oldValue, Rts2Value * newValue);
+
+	private:
+		TempChannel *temps[2];
+
+		Rts2ValueBool *tunest;
+
+		template < typename T> Rts2Value *tempValue (T *&val, const char *prefix, char chan, const char *_info, bool writeToFits)
+		{
+			std::ostringstream _os_n, _os_i;
+			_os_n << prefix << "_" << chan;
+			_os_i << chan << " " << _info;
+			createValue (val, _os_n.str ().c_str (), _os_i.str ().c_str (), writeToFits);
+			return val;
+		}
 };
 
 }
@@ -30,21 +77,44 @@ using namespace rts2sensord;
 
 Lakeshore::Lakeshore (int in_argc, char **in_argv):Gpib (in_argc, in_argv)
 {
-	createValue (tempA, "TEMP_A", "A channell temperature", true);
-	createValue (tempB, "TEMP_B", "B channell temperature", true);
+	for (int i = 0; i < 2; i++)
+	{
+		char chan = 'A' + i;
+		temps[i] = new TempChannel ();
+		Rts2ValueDouble *vd;
+		Rts2ValueInteger *vi;
+		(*(temps[i]))["CRDG"] = tempValue (vd, "TEMP", chan, "channel temperature", true);
+		(*(temps[i]))["INCRV"] = tempValue (vi, "INCRV", chan, "channel input curve number", true);
+	}
+
+	createValue (tunest, "TUNEST", "if either Loop 1 or Loop 2 is actively tuning.", true);
 }
 
 
 Lakeshore::~Lakeshore (void)
 {
+	for (int i = 0; i < 2; i++)
+	{
+		delete temps[i];
+		temps[i] = NULL;
+	}
 }
 
 int Lakeshore::info ()
 {
 	try
 	{
-		readValue ("CRDG? A", tempA);
-		readValue ("CRDG? B", tempB);
+		for (int i = 0; i < 2; i++)
+		{
+			char chan = 'A' + i;
+			for (std::map <const char*, Rts2Value*>::iterator iter = temps[i]->begin (); iter != temps[i]->end (); iter++)
+			{
+				std::ostringstream _os;
+				_os << iter->first << "? " << chan;
+				readValue (_os.str ().c_str (), iter->second);
+			}
+		}
+		readValue ("TUNEST?", tunest);
 	}
 	catch (rts2core::Error er)
 	{
@@ -82,6 +152,6 @@ int Lakeshore::setValue (Rts2Value * oldValue, Rts2Value * newValue)
 
 int main (int argc, char **argv)
 {
-	Lakeshore device = Lakeshore (argc, argv);
+	Lakeshore device (argc, argv);
 	return device.run ();
 }
