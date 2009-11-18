85473c4ba1f5b2ee9bf7d840ce1dbd8e393c78f1
Petr Kubánek <petr@kubanek.net>
2009-11-18 10:55:08 +0000

    Added elemetary driver for Phillips Web Cams - shows dark and light via AGC
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@6560 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/sensord/pwc.cpp
@@ -0,0 +1,116 @@
+/* 
+ * Extract gain and other parameters from Phillips Web Cam.
+ * Copyright (C) 2009 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "sensord.h"
+#include "pwc-ioctl.h"
+
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+namespace rts2sensord
+{
+
+/**
+ * Phillips web cam driver.
+ *
+ * @author Petr Kubanek <petr@kubanek.net>
+ */
+class PWC:public Sensor
+{
+	public:
+		PWC (int argc, char **argv);
+		virtual ~PWC ();
+
+		virtual int info ();
+	protected:
+		virtual int processOption (int opt);
+		virtual int init ();
+	private:
+		const char *videoDevice;
+		int videoFd;
+		Rts2ValueInteger *gain;
+};
+
+};
+
+using namespace rts2sensord;
+
+PWC::PWC (int argc, char ** argv):Sensor (argc, argv)
+{
+	createValue (gain, "AGC", "Automatic Gain Controll", true);
+	
+	videoDevice = "/dev/video0";
+	videoFd = -1;
+	addOption ('f', NULL, 1, "video device file (default to /dev/video0)");
+}
+
+PWC::~PWC ()
+{
+	if (videoFd > 0)
+		close (videoFd);
+}
+
+int PWC::info ()
+{
+	int dummy;
+	if ( ioctl (videoFd, VIDIOCPWCGAGC, &dummy) == -1)
+	{
+		logStream (MESSAGE_ERROR) << "cannot get AGC " << strerror (errno) << sendLog;
+		return -1;
+	}
+	gain->setValueInteger (dummy);
+	return Sensor::info ();
+}
+
+int PWC::processOption (int opt)
+{
+	switch (opt)
+	{
+		case 'f':
+			videoDevice = optarg;
+			break;
+		default:
+			return Sensor::processOption (opt);
+	}
+	return 0;
+}
+
+int PWC::init ()
+{
+	int ret;
+	ret = Sensor::init ();
+	if (ret)
+		return ret;
+	videoFd = open (videoDevice, O_RDWR);
+	if (videoFd < 0)
+	{
+		logStream (MESSAGE_ERROR) << "Cannot open file " << videoDevice << ":" << strerror (errno) << sendLog;
+		return -1;
+	}
+	return 0;
+}
+
+int
+main (int argc, char **argv)
+{
+	PWC device = PWC (argc, argv);
+	return device.run ();
+}
