374b2cd838ba32e130f37b227ba747e2c3cd4e5d
Petr Kubánek <petr@kubanek.net>
2009-12-21 22:19:41 +0000

    MODE is writable
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@6683 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2device.cpp
+++ src/utils/rts2device.cpp
@@ -643,7 +643,7 @@ Rts2Device::loadModefile ()
 	if (ret)
 		return ret;
 
-	createValue (modesel, "MODE", "mode name", true, RTS2_VALUE_DEVPREFIX, 0, true);
+	createValue (modesel, "MODE", "mode name", true, RTS2_VALUE_DEVPREFIX | RTS2_VALUE_WRITABLE, 0, true);
 
 	for (Rts2ConfigRaw::iterator iter = modeconf->begin ();
 		iter != modeconf->end (); iter++)
