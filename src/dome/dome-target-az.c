c848a4eaa5246d81d72d4022fb0011f0c8060c2f
Markus Wildi <markus.wildi@one-arcsec.org>
2010-01-10 09:13:14 +0000

    location now an argument
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@6738 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/dome/dome-target-az.c
+++ src/dome/dome-target-az.c
@@ -1,31 +1,31 @@
-#if 0
-   Library to calculate dome target AZ and ZD 
-   Copyright (C) 2006-2010, Markus Wildi
 
-   The transformations are based on the paper Matrix Method for 
-   Coodinates Transformation written by Toshimi Taki 
-   (http://www.asahi-net.or.jp/~zs3t-tk). 
+/* Library to calculate dome target AZ and ZD  */
+/* Copyright (C) 2006-2010, Markus Wildi */
 
-   Documentation:
-   https://azug.minpet.unibas.ch/wikiobsvermes/index.php/Robotic_ObsVermes#Telescope
+/* The transformations are based on the paper Matrix Method for  */
+/* Coodinates Transformation written by Toshimi Taki  */
+/* (http://www.asahi-net.or.jp/~zs3t-tk).  */
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+/* Documentation: */
+/* https://azug.minpet.unibas.ch/wikiobsvermes/index.php/Robotic_ObsVermes#Telescope */
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+/* This library is free software; you can redistribute it and/or */
+/* modify it under the terms of the GNU Lesser General Public */
+/* License as published by the Free Software Foundation; either */
+/* version 2.1 of the License, or (at your option) any later version. */
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301  USA
-#endif
+/* This library is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU */
+/* Lesser General Public License for more details. */
+
+/* You should have received a copy of the GNU Lesser General Public */
+/* License along with this library; if not, write to the Free Software */
+/* Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301  USA */
+
 
 /* Standard headers */
-
+#include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <unistd.h>
@@ -38,23 +38,15 @@
 #define IS_EAST 1
 #define IS_WEST -1
 #define IS_NOT_WEST -2
+#define UNDEFINED_DEC_AXIS_POSITION 4
 #define BELOW -1
 
 
 double LDRAtoHA( double RA, double longitude) ;
-double LDHAtoRA( double HA, double longitude) ;
 int    LDRAtoDomeAZ( double longitude, double latitude, double RA, double dec, double *Az, double *ZD) ;
 int    LDRAtoStarAZ( double longitude, double latitude, double RA, double dec, double *Az, double *ZD) ;
 int    LDHAtoDomeAZ( double latitude, double HA, double dec, double *Az, double *ZD) ;
 int    LDHAtoStarAZ( double latitude, double HA, double dec, double *Az, double *ZD) ;
-void   LDUUpdateDomeSetpoint( double interval, char *msg) ;
-void   LDSetMovementTypeOnSlew( char *msg) ;
-void   LDSetMovementTypeOnTrack( char *msg) ;
-void   LDSetMovementTypeOnZero( char *msg) ;
-void   LDSetMovementTypeOnTrackMode( int switchnr, char *msg) ;
-void   LDSetMovementTypeOnEvent( char *msg) ;
-void   LDUpdateMovementType() ;
-void   ISUpdateDomeSetpoint() ;
 double LDStarOnDomeTEX( double HA, double dec, double phi, double Rdome,  double Rdec, double xd , double zd) ;
 double LDStarOnDomeTWX( double HA, double dec, double phi, double Rdome,  double Rdec, double xd , double zd) ;
 double LDStarOnDomeTEY( double HA, double dec, double phi, double Rdome,  double Rdec, double xd , double zd) ;
@@ -63,128 +55,81 @@ double LDStarOnDomeZ( double HA, double dec, double phi, double Rdome,  double R
 int    LDCheckHorizon( double HA, double dec, double phi) ;
 
 
-    struct Decaxis_plus_minus {
-      int east ;
-      int west ;
+struct Decaxis_plus_minus {
+  int east ;
+  int west ;
+  
+} dapm ;
 
-    } dapm ;
-
-/* static ISwitch CollisionS[] = {   */
-/*   {"COLLIDING", "colliding", ISS_OFF, 0, 0},                          */
-/*   {"NOW", "now", ISS_OFF, 0, 0}, */
-/* }; */
-
-/* Stars equatorial coordinates in the dome azimutal system */
-/* INumber StarHAN[] = { */
-/*     {"HA",  "HA  H:M:S", "%10.6m",  0.,  24., 0., 19., 0, 0, 0}, */
-/*     {"Dec", "Dec D:M:S", "%10.6m", -90., 90., 0., 60., 0, 0, 0}, */
-/* }; */
-
-/* INumberVectorProperty StarHANP = { */
-/*     mydev, "HOURANGLE", "Star s Hour Angle", CONTROL_GROUP, IP_RW, 0, IPS_IDLE, StarHAN, NARRAY(StarHAN), "", 0}; */
-
-/* INumber StarAZN[] = { */
-/*     {"Az",  "Az  D:M:S", "%10.6m",  0., 360., 0., 0., 0, 0, 0}, */
-/*     {"ZD",  "ZD D:M:S", "%10.6m", 0., 90., 0., 0., 0, 0, 0}, */
-/* }; */
-
-/* INumberVectorProperty StarAZNP = { */
-/*     mydev, "STARAZIMUT", "Star s Azimut", CONTROL_GROUP, IP_RO, 0, IPS_IDLE, StarAZN, NARRAY(StarAZN), "", 0}; */
-
-/* INumber DomeAZSetPointN[] = { */
-/*     {"Az",  "Az  D:M:S", "%10.6m",  0., 360., 0., 0., 0, 0, 0}, */
-/*     {"ZD",  "ZD D:M:S", "%10.6m", 0., 90., 0., 0., 0, 0, 0}, */
-/* }; */
-
-/* INumberVectorProperty DomeAZSetPointNP = { */
-/*     mydev, "DOMEAZIMUTSETPOINT", "Dome Azimut Set Point", CONTROL_GROUP, IP_RW, 0, IPS_IDLE, DomeAZSetPointN, NARRAY(DomeAZSetPointN), "", 0}; */
+double DomeRadius ;
 
 
-/* Observatory parameters  */
-/* the origin is the dome center, pos. x is South, y East and z zenith. */
-
-/* static ITextVectorProperty OriginTP= {  */
-/*   mydev, "ORIGIN", "Toshimi Taki", OBSERVATORY_GROUP, IP_RO, ISR_1OFMANY, IPS_IDLE, OriginT, NARRAY(OriginT), "", 0 */
-/* } ; */
-
-/* static INumber DomeRadiusN[] = { */
-/*     { "DomeRadius", "radius [m]", "%6.3f", 0., 100.0, 0., 1.265, 0, 0, 0},}; */
+struct pier {
+  double radius ;
+  double wedge ;
+  double floor ;
+  double danger_zone_above ;
+  double danger_zone_below ;
+}  pr;
 
 
-    double DomeRadius ;
+struct mount {
+  double xd ;
+  double zd ;
+  double rdec ;
+  
+} mt ;
 
-/* static INumber PierN[] = { */
-/*      { "RADIUS",   "radius [m]", "%6.3f", 0., 100.0, 0., 0.135, 0, 0, 0}, */
-/*      { "WEDGE",   "wedge [m]", "%6.3f", -100., 100.0, 0., -0.6684, 0, 0, 0}, */
-/*      { "FLOOR",   "floor [m]", "%6.3f", -100., 100.0, 0., -1.9534, 0, 0, 0}, */
-/*      { "DANGERBELOW",   "Danger zone below [m]", "%6.3f", -100., 100.0, 0., -2.9534, 0, 0, 0}, */
-/*      { "DANGERABOVE",   "Danger zone above [m]", "%6.3f", -100., 100.0, 0., -0.1, 0, 0, 0}, */
+struct telescope {
+  double radius ;
+  double rear_length ;
+  
+} tel ;
 
-
-    struct pier {
-      double radius ;
-      double wedge ;
-      double floor ;
-      double danger_zone_above ;
-      double danger_zone_below ;
-    }  pr;
-
-/* static INumber MountingN[] = { */
-/*      { "xd",   "xd [m]", "%6.3f", -100., 100.0, 0., -0.0684, 0, 0, 0}, */
-/*      { "zd",   "zd [m]", "%6.3f", -100., 100.0, 0., -0.1934, 0, 0, 0}, */
-/*      { "rdec", "rdec [m]", "%6.3f", -100., 100.0, 0., 0.338, 0, 0, 0},}; */
-
-    struct mount {
-      double xd ;
-      double zd ;
-      double rdec ;
-
-     } mt ;
-/* static INumber TelescopeN[] = { */
-/*      { "RADIUS",   "radius [m]", "%6.3f", 0., 100.0, 0., 0.123, 0, 0, 0}, */
-/*      { "LENGTH",   "rear length [m]", "%6.3f", 0., 100.0, 0., 0.8, 0, 0, 0}, */
-
-/* }; */
-
-
-    struct telescope {
-      double radius ;
-      double rear_length ;
-      
-    } tel ;
-
-    /* the main entry function */
-    double dome_target_az( struct ln_equ_posn tel_eq)
+/* the main entry function */
+double dome_target_az( struct ln_equ_posn tel_eq, int angle, struct ln_lnlat_posn *obs)
+{
+  double ret ;
+  double target_az ;
+  double target_ZD ;
+  double longitude=  obs->lng ; // positive to the East
+  double latitude =  obs->lat; 
+  
+  if( angle== IS_WEST)
     {
-      double ret ;
-      double target_az ;
-      double target_ZD ;
-      double longitude= 7.5 ; //wildi ToDo value,  positive to the East
-      double latitude= 47.2 ; //wildi ToDo
-
+      dapm.east= IS_NOT_EAST ;
+      dapm.west= IS_WEST ;
+    }
+  else if( angle== IS_EAST)
+    {
       dapm.east= IS_EAST ;
       dapm.west= IS_NOT_WEST ;
-
-      DomeRadius= 1.3 ;
-
-      pr.radius= 0.135;
-      pr.wedge= -0.6684;
-      pr.floor= -1.9534;
-      pr.danger_zone_above= -2.9534;
-      pr.danger_zone_below= -0.1;
-
-      mt.xd= -0.0684;
-      mt.zd= -0.1934;
-      mt.rdec= 0.338;
-
-      tel.radius= 0.123;
-      tel.rear_length= 0.8;
-
-
-      ret= LDRAtoDomeAZ( longitude, latitude, tel_eq.ra, tel_eq.dec, &target_az, &target_ZD) ;
-
-      return target_az ;
     }
+  else
+    {
+      return UNDEFINED_DEC_AXIS_POSITION ;
+    }
+  
+  DomeRadius= 1.3 ;
+  
+  pr.radius= 0.135;
+  pr.wedge= -0.6684;
+  pr.floor= -1.9534;
+  pr.danger_zone_above= -2.9534;
+  pr.danger_zone_below= -0.1;
+  
+  mt.xd= -0.0684;
+  mt.zd= -0.1934;
+  mt.rdec= 0.338;
+  
+  tel.radius= 0.123;
+  tel.rear_length= 0.8;
+  
+
+  ret= LDRAtoDomeAZ( longitude, latitude, tel_eq.ra, tel_eq.dec, &target_az, &target_ZD) ;
+
+  return target_az ;
+}
 
 /* The equation 5.6.1-1 os = ted . (pqe + k  qse ) + opd */
 /* was solved for os=( x, y, sqrt( Rdome^2- x^2 -y^2)). The */
@@ -350,21 +295,6 @@ double LDRAtoHA( double RA, double longitude)
 
     return HA ;
 } 
-double LDHAtoRA( double HA, double longitude)
-{
-    double RA ;
-    double JD ;
-    double theta_0= 0. ;
-    
-    JD=  ln_get_julian_from_sys() ;
-
-    theta_0= 15. /180. * M_PI * ln_get_mean_sidereal_time( JD) ;
-/* negative to the West, Kstars neg., XEpehem pos */
-    RA =  fmod(theta_0 + longitude - HA + 2. * M_PI,  2. * M_PI) ;
-
-    return RA ;
-} 
-
 int LDHAtoDomeAZ( double latitude, double HA, double dec, double *Az, double *ZD)
 {
 
