fe624b95f875e8e1274f960778ce680f9780e823
Petr Kubánek <petr@kubanek.net>
2010-02-26 17:01:34 +0000

    fixed solaris - stdint header file
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@6982 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- lib/xmlrpc++/urlencoding.cpp
+++ lib/xmlrpc++/urlencoding.cpp
@@ -21,7 +21,9 @@
 
 #include <ctype.h>
 #include <exception>
+#ifndef sun
 #include <stdint.h>
+#endif
 
 char hex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
