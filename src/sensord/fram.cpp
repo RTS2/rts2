2a434b75d9c1f44c3e80ded39db5c3e0c8f3be7e
Petr Kubánek <petr@kubanek.net>
2010-03-13 01:45:10 +0000

    added FRAM sensor
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@7036 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/sensord/fram.cpp
@@ -0,0 +1,391 @@
+/* 
+ * Driver for board for FRAM telescope, Pierre-Auger observatory, Argentina
+ * Copyright (C) 2005-2010 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "../utils/connford.h"
+#include "sensord.h"
+
+
+#define OPT_WDC_DEV		OPT_LOCAL + 130
+#define OPT_WDC_TIMEOUT		OPT_LOCAL + 131
+#define OPT_EXTRA_SWITCH	OPT_LOCAL + 132
+
+typedef enum
+{
+	SWITCH_BATBACK,
+	SPINAC_2,
+	SPINAC_3,
+	SPINAC_4,
+	PLUG_4,
+	PLUG_3,
+	PLUG_PHOTOMETER,
+	PLUG_1
+} extraOuts;
+
+namespace rts2sensord
+{
+
+class Fram:public Sensor
+{
+	private:
+		rts2core::ConnSerial *wdcConn;
+		char *wdc_file;
+
+		rts2core::FordConn *extraSwitch;
+		
+		char *extraSwitchFile;
+
+		Rts2ValueDouble *wdcTimeOut;
+		Rts2ValueDouble *wdcTemperature;
+
+		Rts2ValueBool *switchBatBack;
+		Rts2ValueBool *plug1;
+		Rts2ValueBool *plug_photometer;
+		Rts2ValueBool *plug3;
+		Rts2ValueBool *plug4;
+
+		int openWDC ();
+		void closeWDC ();
+
+		int resetWDC ();
+		int getWDCTimeOut ();
+		int setWDCTimeOut (int on, double timeout);
+		int getWDCTemp (int id);
+
+		int setValueSwitch (int sw, bool new_state);
+
+	protected:
+		virtual int processOption (int in_opt);
+
+		virtual int init ();
+		virtual int idle ();
+
+		virtual int changeMasterState (int new_state);
+
+		virtual int setValue (Rts2Value *oldValue, Rts2Value *newValue);
+
+	public:
+		Fram (int argc, char **argv);
+		virtual ~Fram (void);
+		virtual int info ();
+};
+
+}
+
+using namespace rts2sensord;
+
+int Fram::openWDC ()
+{
+	int ret;
+	wdcConn = new rts2core::ConnSerial (wdc_file, this, rts2core::BS9600, rts2core::C8, rts2core::NONE, 100);
+	ret = wdcConn->init ();
+	if (ret)
+		return ret;
+
+	return setWDCTimeOut (1, wdcTimeOut->getValueDouble ());
+}
+
+void Fram::closeWDC ()
+{
+	setWDCTimeOut (1, 120.0);
+	delete wdcConn;
+}
+
+int Fram::resetWDC ()
+{
+	wdcConn->writePort ("~**\r", 4);
+	wdcConn->flushPortO ();
+	return 0;
+}
+
+int Fram::getWDCTimeOut ()
+{
+	int i, r, t;
+	char q;
+	char reply[128];
+
+	wdcConn->writePort ("~012\r", 5); /*,i,q */;
+	wdcConn->flushPortO ();
+
+	q = i = t = 0;
+	do
+	{
+		r = wdcConn->readPort (q);
+		if (r == 1)
+			reply[i++] = q;
+		else
+		{
+			t++;
+			if (t > 10)
+				break;
+		}
+	}
+	while (q > 20);
+
+	reply[i] = 0;
+
+	if (reply[0] == '!')
+		logStream (MESSAGE_ERROR) << "Fram::getWDCTimeOut reply " << reply + 1 << sendLog;
+
+	return 0;
+
+}
+
+int Fram::setWDCTimeOut (int on, double timeout)
+{
+	int i, r, t, timeo;
+	char q;
+	char reply[128];
+
+	timeo = (int) (timeout / 0.03);
+	if (timeo > 0xffff)
+		timeo = 0xffff;
+	if (timeo < 0)
+		timeo = 0;
+	if (on)
+		i = '1';
+	else
+		i = '0';
+
+	t = sprintf (reply, "~013%c%04X\r", i, timeo);
+	wdcConn->writePort (reply, t);
+	wdcConn->flushPortO ();
+
+	q = i = t = 0;
+	do
+	{
+		r = wdcConn->readPort (q);
+		if (r == 1)
+			reply[i++] = q;
+		else
+		{
+			t++;
+			if (t > 10)
+				break;
+		}
+	}
+	while (q > 20);
+
+	logStream (MESSAGE_DEBUG) << "Fram::setWDCTimeOut on: " << on << " timeout: " << timeout <<
+		" q: " << q << sendLog;
+
+	reply[i] = 0;
+
+	if (reply[0] == '!')
+		logStream (MESSAGE_DEBUG) << "Fram::setWDCTimeOut reply: " <<
+			reply + 1 << sendLog;
+
+	return 0;
+}
+
+int Fram::getWDCTemp (int id)
+{
+	int i, r, t;
+	char q;
+	char reply[128];
+
+	if ((id < 0) || (id > 2))
+		return -1;
+
+	t = sprintf (reply, "~017%X\r", id + 5);
+	wdcConn->writePort (reply, t);
+	wdcConn->flushPortO ();
+
+	q = i = t = 0;
+	do
+	{
+		r = wdcConn->readPort (q);
+		if (r == 1)
+			reply[i++] = q;
+		else
+		{
+			t++;
+			if (t > 10)
+				return -1;
+		}
+	}
+	while (q > 20);
+
+	reply[i] = 0;
+
+	if (reply[0] != '!')
+		return -1;
+
+	i = atoi (reply + 1);
+
+	return i;
+}
+
+int Fram::changeMasterState (int new_state)
+{
+
+	if ((new_state & SERVERD_STANDBY_MASK) == SERVERD_STANDBY
+		|| (new_state & SERVERD_STANDBY_MASK) == SERVERD_HARD_OFF
+		|| (new_state & SERVERD_STANDBY_MASK) == SERVERD_HARD_OFF)
+	{
+		if (extraSwitch)
+			extraSwitch->ZAP (SWITCH_BATBACK);
+	}
+	else
+		if (extraSwitch)
+			extraSwitch->VYP (SWITCH_BATBACK);
+	return Sensor::changeMasterState (new_state);
+}
+
+int Fram::processOption (int in_opt)
+{
+	switch (in_opt)
+	{
+		case OPT_WDC_DEV:
+			wdc_file = optarg;
+			break;
+		case OPT_WDC_TIMEOUT:
+			wdcTimeOut->setValueDouble (atof (optarg));
+			break;
+		case OPT_EXTRA_SWITCH:
+			extraSwitchFile = optarg;
+			break;
+		default:
+			return Sensor::processOption (in_opt);
+	}
+	return 0;
+}
+
+int Fram::init ()
+{
+	int ret = Sensor::init ();
+	if (ret)
+		return ret;
+
+	if (extraSwitchFile)
+	{
+		extraSwitch = new rts2core::FordConn (extraSwitchFile, this, rts2core::BS9600, rts2core::C8, rts2core::NONE, 40);
+		ret = extraSwitch->init ();
+		if (ret)
+			return ret;
+
+		extraSwitch->VYP (SWITCH_BATBACK);
+
+		createValue (switchBatBack, "bat_backup", "state of batter backup switch", false, RTS2_VALUE_WRITABLE);
+		switchBatBack->setValueBool (false);
+
+		createValue (plug1, "plug_1", "1st plug", false, RTS2_VALUE_WRITABLE);
+		createValue (plug_photometer, "plug_photometer", "1st plug", false, RTS2_VALUE_WRITABLE);
+		extraSwitch->ZAP (PLUG_PHOTOMETER);
+		plug_photometer->setValueBool (true);
+
+		createValue (plug3, "plug_3", "3rd plug", false, RTS2_VALUE_WRITABLE);
+		createValue (plug4, "plug_4", "4th plug", false, RTS2_VALUE_WRITABLE);
+	}
+	if (wdc_file)
+	{
+		ret = openWDC ();
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
+int Fram::idle ()
+{
+	// resetWDC
+	if (wdc_file)
+		resetWDC ();
+	return Sensor::idle ();
+}
+
+int Fram::setValue (Rts2Value *oldValue, Rts2Value *newValue)
+{
+	if (oldValue == switchBatBack)
+	{
+		return setValueSwitch (SWITCH_BATBACK, ((Rts2ValueBool *) newValue)->getValueBool ());
+	}
+	if (oldValue == plug_photometer)
+	{
+		return setValueSwitch (PLUG_PHOTOMETER, ((Rts2ValueBool *) newValue)->getValueBool ());
+	}
+	if (oldValue == plug1)
+	{
+		return setValueSwitch (PLUG_1, ((Rts2ValueBool *) newValue)->getValueBool ());
+	}
+	if (oldValue == plug3)
+	{
+		return setValueSwitch (PLUG_3, ((Rts2ValueBool *) newValue)->getValueBool ());
+	}
+	if (oldValue == plug4)
+	{
+		return setValueSwitch (PLUG_4, ((Rts2ValueBool *) newValue)->getValueBool ());
+	}
+	return Sensor::setValue (oldValue, newValue);
+}
+
+int Fram::setValueSwitch (int sw, bool new_state)
+{
+	if (new_state == true)
+	{
+		return extraSwitch->ZAP (sw) == 0 ? 0 : -2;
+	}
+	return extraSwitch->VYP (sw) == 0 ? 0 : -2;
+}
+
+Fram::Fram (int argc, char **argv):Sensor (argc, argv)
+{
+	createValue (wdcTimeOut, "watchdog_timeout", "timeout of the watchdog card (in seconds)", false);
+	wdcTimeOut->setValueDouble (30.0);
+
+	createValue (wdcTemperature, "watchdog_temp1", "first temperature of the watchedog card", false);
+
+	wdc_file = NULL;
+	wdcConn = NULL;
+
+	extraSwitchFile = NULL;
+	extraSwitch = NULL;
+
+	plug1 = NULL;
+	plug_photometer = NULL;
+	plug3 = NULL;
+	plug4 = NULL;
+
+	addOption (OPT_WDC_DEV, "wdc-dev", 1, "/dev file with watch-dog card");
+	addOption (OPT_WDC_TIMEOUT, "wdc-timeout", 1, "WDC timeout (default to 30 seconds)");
+	addOption (OPT_EXTRA_SWITCH, "extra-switch", 1, "/dev entery for extra switches, handling baterry etc..");
+}
+
+Fram::~Fram (void)
+{
+	if (extraSwitch)
+		extraSwitch->ZAP (SWITCH_BATBACK);
+	if (wdc_file)
+		closeWDC ();
+}
+
+int Fram::info ()
+{
+	if (wdcConn > 0)
+	{
+	  	wdcTemperature->setValueDouble (getWDCTemp (2));
+	}
+	return Sensor::info ();
+}
+
+int main (int argc, char **argv)
+{
+	Fram device = Fram (argc, argv);
+	return device.run ();
+}
