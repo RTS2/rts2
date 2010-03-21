dfb0c236a9ed98dccd93e8965aeb20e1f465e829
Petr Kubánek <petr@kubanek.net>
2010-03-21 14:09:55 +0000

    readdded multiple daemon fix
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@7090 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/dome/dome.cpp
+++ src/dome/dome.cpp
@@ -134,7 +134,7 @@ int Dome::init ()
 	// check for presense of state-master
 	if (getCentraldConns ()->size () > 1)
 	{
- 		if (stateMaster->getValueString ().length () == 0)
+ 		if (stateMaster == NULL || stateMaster->getValueString ().length () == 0)
 		{
 			logStream (MESSAGE_ERROR) << "multiple central server specified, but none identified as master - please add state-master option" << sendLog;
 			return -1;
