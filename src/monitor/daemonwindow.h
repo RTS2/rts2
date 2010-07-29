3c9061a79a7d8b3063d4064aa1552c5d51566c06
Petr Kubánek <petr@kubanek.net>
2010-07-29 19:03:44 +0000

    changed limit of variables to 500
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@7521 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/monitor/daemonwindow.h
+++ src/monitor/daemonwindow.h
@@ -39,7 +39,7 @@ namespace rts2ncurses
 class NSelWindow:public NWindow
 {
 	public:
-		NSelWindow (int x, int y, int w, int h, int border = 1, int sw = 300, int sh = 100);
+		NSelWindow (int x, int y, int w, int h, int border = 1, int sw = 300, int sh = 500);
 		virtual ~ NSelWindow (void);
 		virtual keyRet injectKey (int key);
 		virtual void winrefresh ();
