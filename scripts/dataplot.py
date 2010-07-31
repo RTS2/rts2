53f8166ddb1090f11883e253ddd234f40b8d382e
Petr Kubánek <petr@kubanek.net>
2010-07-31 15:17:08 +0000

    added TSTART offsets
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@7530 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- scripts/dataplot.py
+++ scripts/dataplot.py
@@ -12,7 +12,7 @@ a_amp1 = numpy.array ([])
 
 fig = matplotlib.pyplot.figure()
 
-p1 = fig.add_subplot(221,title='AMP0', xlabel='time (s)', ylabel='pA')
+p1 = fig.add_subplot(221, title='AMP0', xlabel='time (s)', ylabel='pA')
 p2 = fig.add_subplot(222, title='AMP1', xlabel='time (s)')
 h1 = fig.add_subplot(223)
 h2 = fig.add_subplot(224)
@@ -21,6 +21,14 @@ for arg in sys.argv[1:]:
 	print arg,
 	try:
 		hdulist = pyfits.open(arg)
+		tdiff = 0
+		try:
+			dat1 = hdulist['AMP0.MEAS_TIMES'].header['TSTART']
+			dat2 = hdulist['AMP1.MEAS_TIMES'].header['TSTART']
+			tdiff = dat2 - dat1
+		except Exception as ex:
+			print 'failed to read header', ex,
+
 		amp0 = hdulist['AMP0.MEAS_TIMES'].data
 		amp1 = hdulist['AMP1.MEAS_TIMES'].data
 
@@ -28,7 +36,7 @@ for arg in sys.argv[1:]:
 		a_amp1 = numpy.concatenate((a_amp1, amp1.field(1)))
 
 		p1.plot(amp0.field(0),amp0.field(1))
-		p2.plot(amp1.field(0),amp1.field(1))
+		p2.plot(amp1.field(0) + tdiff,amp1.field(1))
 		print ' ok'
 	except Exception as ex:
 		print ' failed ', ex
