649d09ec4627eb0722202317f79c1f036574b011
Petr Kubánek <petr@kubanek.net>
2010-08-02 03:14:28 +0000

    reportit classes of messages
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@7544 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- include/message.h
+++ include/message.h
@@ -1,22 +1,21 @@
 #ifndef __RTS2_MESSAGE_T__
 #define __RTS2_MESSAGE_T__
 
-typedef enum
-{
-	MESSAGE_CRITICAL = 0x10,
-	MESSAGE_ERROR    = 0x01,
-	MESSAGE_WARNING  = 0x02,
-	MESSAGE_INFO     = 0x04,
-	MESSAGE_DEBUG    = 0x08
-} messageType_t;
+typedef int messageType_t;
 
-typedef enum
-{
-	CRITICAL_TELESCOPE_FAILURE = 0x0110,
-	CRITICAL_DOME_FAILURE      = 0x0210,
-	CRITICAL_CAMERA_FAILURE    = 0x0310,
-	CRITICAL_REQUIRED_FAILURE  = 0x0410
-} criticalMessage_t;
+#define MESSAGE_CRITICAL                0x0010
+#define MESSAGE_ERROR                   0x0001
+#define MESSAGE_WARNING                 0x0002
+#define MESSAGE_INFO                    0x0004
+#define MESSAGE_DEBUG                   0x0008
 
-#define MESSAGE_MASK_ALL  0xFFFF
+#define MESSAGE_REPORTIT                0x1000
+
+#define CRITICAL_TELESCOPE_FAILURE      0x0110
+#define CRITICAL_DOME_FAILURE           0x0210
+#define CRITICAL_CAMERA_FAILURE         0x0310
+#define CRITICAL_REQUIRED_FAILURE       0x0410
+
+#define MESSAGE_MASK_ALL                0xFFFF
+
 #endif							 /* ! __RTS2_MESSAGE_T__ */
