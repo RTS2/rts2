2d4dcb3af51c368bab18160888b580db107f8259
Petr Kubánek <petr@kubanek.net>
2010-08-18 22:22:33 +0000

    removed parseDate
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@7611 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/rts2cliapp.cpp
+++ src/utils/rts2cliapp.cpp
@@ -21,20 +21,15 @@
 
 #include <iostream>
 
-Rts2CliApp::Rts2CliApp (int in_argc, char **in_argv):
-Rts2App (in_argc, in_argv)
+Rts2CliApp::Rts2CliApp (int in_argc, char **in_argv):Rts2App (in_argc, in_argv)
 {
 }
 
-
-void
-Rts2CliApp::afterProcessing ()
+void Rts2CliApp::afterProcessing ()
 {
 }
 
-
-int
-Rts2CliApp::run ()
+int Rts2CliApp::run ()
 {
 	int ret;
 	ret = init ();
@@ -43,45 +38,4 @@ Rts2CliApp::run ()
 	ret = doProcessing ();
 	afterProcessing ();
 	return ret;
-}
-
-
-int
-Rts2CliApp::parseDate (const char *in_date, struct ln_date *out_time)
-{
-	int ret;
-	int ret2;
-	out_time->hours = out_time->minutes = 0;
-	out_time->seconds = 0;
-	ret =
-		sscanf (in_date, "%d-%d-%d%n", &out_time->years, &out_time->months,
-		&out_time->days, &ret2);
-	if (ret == 3)
-	{
-		in_date += ret2;
-		// we end with is T, let's check if it contains time..
-		if (*in_date == 'T' || isspace (*in_date))
-		{
-			in_date++;
-			ret2 =
-				sscanf (in_date, "%u:%u:%lf", &out_time->hours,
-				&out_time->minutes, &out_time->seconds);
-			if (ret2 == 3)
-				return 0;
-			ret2 =
-				sscanf (in_date, "%u:%u", &out_time->hours, &out_time->minutes);
-			if (ret2 == 2)
-				return 0;
-			ret2 = sscanf (in_date, "%d", &out_time->hours);
-			if (ret2 == 1)
-				return 0;
-			std::cerr << "Cannot parse time of the date: " << in_date << std::
-				endl;
-			return -1;
-		}
-		// only year..
-		return 0;
-	}
-	std::cerr << "Cannot parse date: " << in_date << std::endl;
-	return -1;
 }
--- src/utils/rts2cliapp.h
+++ src/utils/rts2cliapp.h
@@ -34,6 +34,10 @@
  */
 class Rts2CliApp:public Rts2App
 {
+	public:
+		Rts2CliApp (int in_argc, char **in_argv);
+
+		virtual int run ();
 	protected:
 		/**
 		 * Called for application processing. This method shall be used instead of
@@ -48,20 +52,5 @@ class Rts2CliApp:public Rts2App
 		 * summary of processed data etc.
 		 */
 		virtual void afterProcessing ();
-
-	public:
-		Rts2CliApp (int in_argc, char **in_argv);
-
-		virtual int run ();
-
-		/**
-		 * Parses and initialize tm structure from char.
-		 *
-		 * String can contain either date, in that case it will be converted to night
-		 * starting on that date, or full date with time (hour, hour:min, or hour:min:sec).
-		 *
-		 * @return -1 on error, 0 on succes
-		 */
-		int parseDate (const char *in_date, struct ln_date *out_time);
 };
 #endif							 /* !__RTS2_CLIAPP__ */
