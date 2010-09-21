95a72512cfb5585b3cc3bfde384f8a7af6995fa1
Petr Kubánek <petr@kubanek.net>
2010-09-21 20:41:36 +0000

    minimal length in expand format
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/branches/rts-2/REL_0_8_1@7785 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/utils/expander.h
+++ src/utils/expander.h
@@ -1,6 +1,6 @@
 /* 
  * Expanding mechanism.
- * Copyright (C) 2007-2009 Petr Kubanek <petr@kubanek.net>
+ * Copyright (C) 2007-2010 Petr Kubanek <petr@kubanek.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -115,6 +115,11 @@ class Expander
 		int getNightMonth () { return nightDate.tm_mon + 1; }
 
 		int getNightDay () { return nightDate.tm_mday; }
+
+		/**
+		 * Retrieves formating parameters.
+		 */
+		void getFormating (const std::string &expression, std::string::iterator &iter, std::ostringstream &ret);
 };
 
 };
