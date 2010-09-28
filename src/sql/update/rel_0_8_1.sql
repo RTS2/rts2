241a95739af058451493a625a5529af526cac4fb
Petr Kubánek <petr@kubanek.net>
2010-09-28 21:39:15 +0000

    delete labels, scripts
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@7805 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sql/update/rel_0_8_1.sql
+++ src/sql/update/rel_0_8_1.sql
@@ -17,6 +17,8 @@ CREATE OR REPLACE FUNCTION delete_target (integer) RETURNS integer AS
 '
 DELETE FROM images WHERE EXISTS (SELECT * FROM observations WHERE observations.obs_id = images.obs_Id AND observations.tar_id = $1);
 DELETE FROM observations WHERE tar_id = $1;
+DELETE FROM scripts WHERE tar_id = $1;
+DELETE FROM target_labels WHERE tar_id = $1;
 DELETE FROM targets WHERE tar_id = $1;
 SELECT 1;
 ' LANGUAGE 'SQL';
