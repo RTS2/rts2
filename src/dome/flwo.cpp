f4f2591e7df678e3d412758d24ac1ca81465beb8
Petr Kubánek <petr@kubanek.net>
2010-10-28 02:34:05 +0000

    check slit file
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@7926 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/dome/flwo.cpp
+++ src/dome/flwo.cpp
@@ -22,6 +22,7 @@
 
 #define OPT_OPEN     OPT_LOCAL + 620
 #define OPT_CLOSE    OPT_LOCAL + 621
+#define OPT_SLITST   OPT_LOCAL + 622
 
 using namespace rts2dome;
 
@@ -33,6 +34,7 @@ class FLWO:public Dome
 	protected:
 		virtual int processOption (int opt);
 		virtual int init ();
+		virtual int info ();
 
 		virtual int startOpen ();
 		virtual long isOpened ();
@@ -49,6 +51,9 @@ class FLWO:public Dome
 
 		const char *opendome;
 		const char *closedome;
+		const char *slitfile;
+
+		void getSlitStatus ();
 };
 
 FLWO::FLWO (int argc, char **argv):Dome (argc, argv)
@@ -58,9 +63,11 @@ FLWO::FLWO (int argc, char **argv):Dome (argc, argv)
 
 	opendome = NULL;
 	closedome = NULL;
+	slitfile = NULL;
 
 	addOption (OPT_OPEN, "bin-open", 1, "path to program for opening the dome");
 	addOption (OPT_CLOSE, "bin-close", 1, "path to program to close the dome");
+	addOption (OPT_SLITST, "slit-file", 1, "path to slit file (/Rea...)");
 }
 
 int FLWO::processOption (int opt)
@@ -72,6 +79,9 @@ int FLWO::processOption (int opt)
 			break;
 		case OPT_CLOSE:
 			closedome = optarg;
+			break;
+		case OPT_SLITST:
+			slitfile = optarg;
 			break;
 		default:
 			return Dome::processOption (opt);
@@ -90,6 +100,20 @@ int FLWO::init ()
 		return -1;
 	}
 	return 0;
+}
+
+int FLWO::info ()
+{
+	try
+	{
+		getSlitStatus ();
+	}
+	catch (rts2core::Error &er)
+	{
+		logStream (MESSAGE_ERROR) << er << sendLog;
+		return -1;
+	}
+	return Dome::info ();
 }
 
 int FLWO::startOpen ()
@@ -159,6 +183,25 @@ int FLWO::deleteConnection (Rts2Conn * conn)
 	if (conn == domeExe)
 		domeExe = NULL;
 	return Dome::deleteConnection (conn);
+}
+
+void FLWO::getSlitStatus ()
+{
+	if (slitfile == NULL)
+		throw rts2core::Error ("slitfile not specified");
+	std::ifstream sf (slitfile);
+	std::string slst;
+	sf >> slst;
+	if (sf.fail ())
+	{
+		throw rts2core::Error (std::string ("cannot read from slitfile") + strerror (errno));
+	}
+	if (slst == "Open")
+		maskState (DOME_DOME_MASK, DOME_OPENED, "dome detected opened");
+	else if (slst == "Close")
+		maskState (DOME_DOME_MASK, DOME_CLOSED, "dome detected closed");
+	else
+		throw rts2core::Error (std::string ("unknow string in slitfile") + slst);
 }
 
 int main (int argc, char **argv)
