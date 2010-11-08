f35eefb952a5aabbbd4e7c16c95e58bb6ecece12
Petr Kubánek <petr@kubanek.net>
2010-11-08 04:50:31 +0000

    sextractor, focusing tests
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@8000 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ scripts/sextractor.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+import sys
+import subprocess
+import os
+
+class Sextractor:
+    """Class for a catalogue (SExtractor result)"""
+    def __init__(self, filename, fields=['NUMBER', 'FLUXERR_ISO', 'FLUX_AUTO', 'X_IMAGE', 'Y_IMAGE'], sexconfig='/usr/share/sextractor/default.sex', threshold=2.7):
+        self.filename = filename
+	self.sexconfig = sexconfig
+
+	self.fields = fields
+        self.objects = []
+	self.threshold = threshold
+
+    def runSExtractor(self):
+    	pfn = '/tmp/pysex_{0}'.format(os.getpid())
+	output = '/tmp/pysex_out_{0}'.format(os.getpid())
+    	pf = open(pfn,'w')
+	for f in self.fields:
+		pf.write(f + '\n')
+	pf.close()
+
+        cmd = ['sextractor', self.filename, '-c ', self.sexconfig, '-PARAMETERS_NAME', pfn, '-DETECT_THRESH', str(self.threshold), '-FILTER', 'N', '-STARNNW_NAME', '/usr/share/sextractor/default.nnw', '-CATALOG_NAME', output, '-VERBOSE_TYPE', 'QUIET']
+       	proc = subprocess.Popen(cmd)
+	proc.wait()
+
+	# parse output
+	of = open(output,'r')
+	while (True):
+	 	x=of.readline()
+		if x == '':
+			break
+		if x[0] == '#':
+			continue
+		self.objects.append(map(float,x.split()))
+	
+	# unlink tmp files
+	os.unlink(pfn)
+	os.unlink(output)
+
+    def sortObjects(self,col):
+        """Sort objects by given collumn."""
+    	self.objects.sort(cmp=lambda x,y: cmp(x[col],y[col]))
+
+    def reverseObjects(self,col):
+	"""Reverse sort objects by given collumn."""
+	self.objects.sort(cmp=lambda x,y: cmp(x[col],y[col]))
+	self.objects.reverse()
+
+def getFWHM(fn,starsn):
+	"""Returns average FWHM of first starsn stars from the image"""
+	c = Sextractor(fn,['X_IMAGE','Y_IMAGE','MAG_BEST','FLAGS','CLASS_STAR','FWHM_IMAGE','A_IMAGE','B_IMAGE'])
+	c.runSExtractor()
+
+	# sort by magnitude
+	c.sortObjects(2)
+
+	# display in ds9
+	i = 0
+	fwhm = 0
+	a = 0
+	b = 0
+	for x in c.objects:
+	  	if (x[3] == 0 and x[4] != 0):
+			fwhm += x[5]
+			a += x[6]
+			b += x[7]
+			i += 1
+			if i > starsn:
+				break 
+	if i > 0:
+		return float(fwhm) / i
+
+	raise Exception('cannot find any stars on the image')
+
+if __name__ == "__main__":
+  	# test method
+	from ds9 import *
+  	d = ds9()
+  	for fn in sys.argv[1:]:
+		c = Sextractor(fn,['X_IMAGE','Y_IMAGE','MAG_BEST','FLAGS','CLASS_STAR','FWHM_IMAGE','A_IMAGE','B_IMAGE'])
+		c.runSExtractor()
+
+		# sort by magnitude
+		c.sortObjects(2)
+
+		# display in ds9
+		d.set('file {0}'.format(fn))
+		for x in c.objects:
+			d.set('regions', 'image; circle {0} {1} 10'.format(x[0],x[1]))
