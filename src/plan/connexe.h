2b6a3bf8f26175fa6ae907536450cc98f434d12f
Petr Kubánek <petr@kubanek.net>
2010-11-08 05:58:04 +0000

    added ConnExe header
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@8003 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/plan/connexe.h
@@ -0,0 +1,52 @@
+/*
+ * Abstract class for executor connection.
+ * Copyright (C) 2010 Petr Kubanek, Institute of Physics <kubanek@fzu.cz>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __RTS2_CONNEXE__
+#define __RTS2_CONNEXE__
+
+#include "element.h"
+#include "../utils/connfork.h"
+
+namespace rts2script {
+
+/**
+ * Abstract class for communicating with external script.
+ *
+ * @author Petr Kubanek, Institute of Physics <kubanek@fzu.cz>
+ */
+class ConnExe:public rts2core::ConnFork
+{
+	public:
+		ConnExe (Rts2Block * _master, const char *_exec, bool fillConnEnv, int timeout = 0);
+		virtual ~ConnExe ();
+
+		virtual void processLine ();
+
+	protected:
+		virtual void processCommand (char *cmd);
+
+		virtual void processErrorLine (char *errbuf);
+
+		bool isCentraldName (const char *_name) { return !strcmp (_name, "..") || !strcmp (_name, "centrald"); }
+		Rts2Conn *getConnectionForScript (const char *name);
+		int getDeviceType (const char *name);
+};
+
+}
+#endif /* !__RTS2_CONNEXE__ */
