ae121ccb43b5e5c1f9d0fa7dc99151be56b3d4a2
Petr Kubánek <petr@kubanek.net>
2010-11-09 05:09:10 +0000

    put zenith FWHM estimate
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@8015 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- scripts/fwhm.py
+++ scripts/fwhm.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
-# prints FWHM as double parameter, usable for imgproc.
-
 import sextractor
 import sys
 
-c = sextractor.Sextractor(sys.argv[1],['X_IMAGE','Y_IMAGE','MAG_BEST','FLAGS','CLASS_STAR','FWHM_IMAGE','A_IMAGE','B_IMAGE'])
+fn = sys.argv[1]
+
+c = sextractor.Sextractor(fn,['X_IMAGE','Y_IMAGE','MAG_BEST','FLAGS','CLASS_STAR','FWHM_IMAGE','A_IMAGE','B_IMAGE'])
 c.runSExtractor()
 c.sortObjects(2)
 
@@ -20,8 +20,17 @@ for x in c.objects:
 		a += x[6]
 		b += x[7]
 		i += 1
-		if i > 9:
-			print 'double fwhm "calculated FWHM" {0}'.format(fwhm/i)
-			break 
 
-
+if i > 9:
+	import pyfits
+	import math
+	ff = pyfits.fitsopen(fn)
+	fwhm /= i
+	print 'double fwhm "calculated FWHM" {0}'.format(fwhm)
+	zd = 90 - ff[0].header['TEL_ALT']
+	print 'double fwhm_zenith "estimated zenith FWHM" {0}'.format(fwhm * (math.cos(math.radians(zd)) ** 0.6))
+	print 'double fwhm_foc "focuser positon" {0}'.format(ff[0].header['FOC_POS'])
+	print 'double fwhm_nstars "number of stars for FWHM calculation" {0}'.format(i)
+	print 'double fwhm_zd "[deg] zenith distance of the FWHM measurement" {0}'.format(zd)
+	print 'double fwhm_az "[deg] azimuth of the FWHM measurement" {0}'.format(ff[0].header['TEL_AZ'])
+	print 'double fwhm_airmass "airmass of the FWHM measurement" {0}'.format(ff[0].header['AIRMASS'])
