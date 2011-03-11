c24740147e3204d440aec68c735844c8bb8308e4
Petr Kubánek <petr@kubanek.net>
2011-03-11 23:13:22 +0000

    fixed update script
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@8586 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sql/auger/rel_0_8_1.sql
+++ src/sql/auger/rel_0_8_1.sql
@@ -1,5 +1,5 @@
 --- add columns for new variables
 
-ALTER TABLE auger ADD COLUMN DGHXmax1;
-ALTER TABLE auger ADD COLUMN DGHXmax2;
-ALTER TABLE auger ADD COLUMN DGHChi2Improv;
+ALTER TABLE auger ADD COLUMN DGHXmax1 float8;
+ALTER TABLE auger ADD COLUMN DGHXmax2 float8;
+ALTER TABLE auger ADD COLUMN DGHChi2Improv float8;
