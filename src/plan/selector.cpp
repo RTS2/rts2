0c54c91703219af7bfb1d4cdd44ce2ca84042738
Petr Kubánek <petr@kubanek.net>
2011-04-26 22:54:00 +0000

    changed selector values
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@8706 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/plan/selector.cpp
+++ src/plan/selector.cpp
@@ -155,10 +155,10 @@ SelectorDev::SelectorDev (int argc, char **argv):Rts2DeviceDb (argc, argv, DEVIC
 	createValue (interrupt, "interrupt", "if next target soft-interrupt current observations", false, RTS2_VALUE_WRITABLE);
 	interrupt->setValueBool (false);
 
-	createValue (idle_select, "idle_select", "interval in seconds in which for selection of next target", false, RTS2_VALUE_WRITABLE | RTS2_DT_INTERVAL);
+	createValue (idle_select, "idle_select", "delay in sec for selection", false, RTS2_VALUE_WRITABLE | RTS2_DT_INTERVAL);
 	idle_select->setValueInteger (300);
 
-	createValue (night_idle_select, "night_idle_select", "interval in seconds for selection of next target during night", false, RTS2_VALUE_WRITABLE | RTS2_DT_INTERVAL);
+	createValue (night_idle_select, "night_idle_select", "delay in sec for selection of next target during night", false, RTS2_VALUE_WRITABLE | RTS2_DT_INTERVAL);
 	night_idle_select->setValueInteger (300);
 
 	createValue (selEnabled, "selector_enabled", "if selector should select next targets", false, RTS2_VALUE_WRITABLE);
