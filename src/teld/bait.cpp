316c42e7a60a5622ea1e54992f28f467bd2ce65c
Petr Kubánek <petr@kubanek.net>
2011-06-27 01:38:28 +0000

    BAIT uses old iterface to read MDM values fix offseting problem
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@8915 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/teld/bait.cpp
+++ src/teld/bait.cpp
@@ -20,6 +20,7 @@
 #include "teld.h"
 #include "../utils/rts2config.h"
 #include "../utils/connbait.h"
+#include "tcsutils.h"
 
 #define OPT_BAITHOST     OPT_LOCAL + 521
 
@@ -56,9 +57,19 @@ class BAIT:public Telescope
 		virtual int isMovingFixed ();
 		virtual int isParking ();
 
+		virtual int setValue (rts2core::Value * old_value, rts2core::Value * new_value);
+
 	private:
 		HostString *baitServer;
 		rts2core::BAIT *connBait;
+
+		int tcssock;
+		int missock;
+
+		rts2core::ValueInteger *probeX;
+		rts2core::ValueInteger *probeY;
+
+		int setGP (int x, int y);
 };
 
 }
@@ -70,21 +81,53 @@ BAIT::BAIT (int argc, char **argv):Telescope (argc, argv)
 	baitServer = NULL;
 	connBait = NULL;
 
+	tcssock = -1;
+	missock = -1;
+
+	createValue (probeX, "GP_X", "guider probe X position", true, RTS2_VALUE_WRITABLE);
+	createValue (probeY, "GP_Y", "guider probe Y position", true, RTS2_VALUE_WRITABLE);
+
 	addOption (OPT_BAITHOST, "bait-server", 1, "BAIT server hostname (and port)");
 }
 
 BAIT::~BAIT ()
 {
+	close (missock);
+	close (tcssock);
+
 	delete connBait;
 }
 
 int BAIT::startResync ()
 {
+	char buf[500];
 	struct ln_equ_posn pos;
-	getTarget (&pos);
-	char buf[256];
 
-	snprintf (buf, 255, "point ra=%f dec=%f decimal equinox=2000 nodome", pos.ra, pos.dec);
+	if (!originChangedFromLastResync () && getTargetDistance () > 1)
+	{
+		// match target and telescope if there is a huge difference between tel and target
+		logStream (MESSAGE_WARNING) << "matching telescope and target coordinates," << sendLog;
+
+		connBait->writeRead ("where decimal equinox=2000", buf, 500);
+		double ra, dec, equinox, ha, secz, alt, az;
+		int ret = sscanf (buf, "done where ra=%lf dec=%lf equinox=%lf ha=%lf secz=%lf alt=%lf az=%lf", &ra, &dec, &equinox, &ha, &secz, &alt, &az);
+		if (ret != 7)
+		{
+			logStream (MESSAGE_ERROR) << "cannot parse BAIT reply: " << buf << sendLog;
+			return -1;
+		}
+		setOrigin (ra, dec);
+		pos.ra = ra;
+		pos.dec = dec;
+		applyOffsets (&pos);
+		setTarget (pos.ra, pos.dec);
+	}
+	else
+	{
+		getTarget (&pos);
+	}
+
+	snprintf (buf, 255, "point ra=%f dec=%f decimal equinox=2000", pos.ra, pos.dec);
 	connBait->writeRead (buf, buf, 256);
 
 	return 0;
@@ -156,24 +199,42 @@ int BAIT::init ()
 		logStream (MESSAGE_ERROR) << er << sendLog;
 		return -1;
 	}
+
+	tcssock = tcss_opensock_clnt ((char *) "mcgraw");	
+	if (tcssock < 0)
+	{
+		logStream (MESSAGE_ERROR) << "Cannot open connection to MDM TCS at mcgraw, error " << strerror (errno) << sendLog;
+		return -1;
+	}
+
+	missock = miss_opensock_clnt  ((char *) "mcgraw");
+	if (missock < 0)
+	{
+		logStream (MESSAGE_ERROR) << "Cannot open connection to MDM MIS at mcgraw error " << strerror (errno) << sendLog;
+		return -1;
+	}
 	
 	return info ();
 }
 
 int BAIT::info ()
 {
-	char buf[500];
-	connBait->writeRead ("where decimal", buf, 500);
-	double ra, dec, equinox, ha, secz, alt, az;
-	int ret = sscanf (buf, "done where ra=%lf dec=%lf equinox=%lf ha=%lf secz=%lf alt=%lf az=%lf", &ra, &dec, &equinox, &ha, &secz, &alt, &az);
-	if (ret != 7)
-	{
-		logStream (MESSAGE_ERROR) << "cannot parse BAIT reply: " << buf << sendLog;
-		return -1;
-	}
+	int ret;
+	tcsinfo_t tcsi;
+	ret = tcss_reqcoords (tcssock, &tcsi, 0, 1);
+	if (ret < 0)
+		return ret;
 
-	setTelRaDec (ra, dec);
-	
+	setTelRaDec (tcsi.ra * 15.0, tcsi.dec);
+
+	misinfo_t misi;
+	ret = miss_reqinfo (missock, &misi, 0, 1);
+	if (ret < 0)
+		return ret;
+
+	probeX->setValueInteger (misi.guidex);
+	probeY->setValueInteger (misi.guidey);
+
 	return Telescope::info ();
 }
 
@@ -190,6 +251,28 @@ int BAIT::isMovingFixed ()
 int BAIT::isParking ()
 {
 	return isMoving ();
+}
+
+int BAIT::setValue (rts2core::Value * old_value, rts2core::Value * new_value)
+{
+	if (old_value == probeX)
+		return setGP (new_value->getValueInteger (), probeY->getValueInteger ()) < 0 ? -2 : 0;
+	else if (old_value == probeY)
+		return setGP (probeX->getValueInteger (), new_value->getValueInteger ()) < 0 ? -2 : 0;
+
+	return Telescope::setValue (old_value, new_value);
+}
+
+int BAIT::setGP (int x, int y)
+{
+	char pos[256];
+	if (x < 0 || x > 25000 || y < 0 || y > 25000)
+	{
+		logStream (MESSAGE_ERROR) << "invalid probe position " << x << " " << y << sendLog;
+		return -1;
+	}
+	snprintf (pos, 255, "PMOVE %#3.1f %#3.1f", (float) x, (float) y);
+	return miss_makereq (missock, pos, MIS_MSG_REQPMOVE, MIS_MSG_PMOVE);
 }
 
 int main (int argc, char **argv)
