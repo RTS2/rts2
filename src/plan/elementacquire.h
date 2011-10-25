8b11c9d1e2922b0ca76a175fac66fd81c2f9fe7c
Petr Kubánek <petr@kubanek.net>
2011-10-25 00:58:49 +0000

    acquire script produces proper XML output
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@9453 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/plan/elementacquire.h
+++ src/plan/elementacquire.h
@@ -1,3 +1,24 @@
+/*
+ * Script for acqustion of star.
+ * Copyright (C) 2005-2008 Petr Kubanek <petr@kubanek.net>
+ * Copyright (C) 2011 Petr Kubanek, Institute of Physics <kubanek@fzu.cz>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
 #ifndef __RTS2_SCRIPT_ELEMENT_ACQUIRE__
 #define __RTS2_SCRIPT_ELEMENT_ACQUIRE__
 
@@ -16,7 +37,9 @@ class ElementAcquire:public Element
 		virtual int processImage (rts2image::Image * image);
 		virtual void cancelCommands ();
 
+		virtual void printXml (std::ostream &os) { os << "  <acquire length='" << expTime << "' precision='" << reqPrecision << "'/>"; }
 		virtual void printScript (std::ostream &os) { os << COMMAND_ACQUIRE << " " << reqPrecision << " " << expTime; }
+		virtual void printJson (std::ostream &os) { os << "\"cmd\":\"" << COMMAND_ACQUIRE << "\",\"duration\":" << expTime << ",\"precision\":" << reqPrecision; }
 	protected:
 		double reqPrecision;
 		double lastPrecision;
