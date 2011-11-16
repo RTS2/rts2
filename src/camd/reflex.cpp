1a0d4210758eb48828e524f9a7df0645cf97d55e
Petr Kubánek <petr@kubanek.net>
2011-11-16 18:38:31 +0000

    all registers read from reflex
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@9583 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/camd/reflex.cpp
+++ src/camd/reflex.cpp
@@ -1,6 +1,7 @@
 /* 
  * STA Reflex controller
  * Copyright (C) 2011 Petr Kubanek, Institute of Physics <kubanek@fzu.cz>
+ * portions from STA code, Copyright 2011 Semiconductor Technology Associates, Inc.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -17,10 +18,14 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#include "camd.h"
-#include "edtsao/interface.h"
+#include <iomanip>
 
+#include "camd.h"
 #include "valuearray.h"
+#include "rts2configraw.h"
+
+// only constants; class is kept in reflex.cpp
+#include "reflex.h"
 
 // select type of driver
 #define CL_EDT
@@ -31,14 +36,14 @@
 #define hSerRef void *
 #elif defined(CL_EDT)
 #include <edtinc.h>
-#define CL_BAUDRATE_9600                        1
-#define CL_BAUDRATE_19200                       2
-#define CL_BAUDRATE_38400                       4
-#define CL_BAUDRATE_57600                       8
-#define CL_BAUDRATE_115200                      16
-#define CL_BAUDRATE_230400                      32
-#define CL_BAUDRATE_460800                      64
-#define CL_BAUDRATE_921600                      128
+#define CL_BAUDRATE_9600                        9600
+#define CL_BAUDRATE_19200                       19200
+#define CL_BAUDRATE_38400                       38400
+#define CL_BAUDRATE_57600                       57600
+#define CL_BAUDRATE_115200                      115200
+#define CL_BAUDRATE_230400                      230400
+#define CL_BAUDRATE_460800                      460800
+#define CL_BAUDRATE_921600                      921600
 #else
 #include <clallserial.h>
 #endif
@@ -104,6 +109,8 @@ class Reflex:public Camera
 
 		virtual int info ();
 
+		virtual void signaledHUP ();
+
 		virtual int startExposure ();
 		virtual int stopExposure ();
 		virtual long isExposing ();
@@ -125,7 +132,7 @@ class Reflex:public Camera
 		 *
 		 * @return 
 		 */
-		RRegister * createRegister (uint32_t address, const char *name, const char * desc, bool writable, bool infoupdate);
+		RRegister * createRegister (uint32_t address, const char *name, const char * desc, bool writable, bool infoupdate, bool hexa);
 
 		int openInterface (int port);
 		int closeInterface ();
@@ -145,6 +152,15 @@ class Reflex:public Camera
 		 */
 		int readRegister(uint32_t addr, uint32_t& data);
 		int writeRegister(uint32_t addr, uint32_t data);
+
+		// related to configuration file..
+		const char *configFile;
+		Rts2ConfigRaw *config;
+
+		void reloadConfig ();
+
+		// create board values
+		void createBoards ();
 
 		rts2core::ValueSelection *baudRate;
 #ifdef CL_EDT
@@ -173,21 +189,75 @@ Reflex::Reflex (int in_argc, char **in_argv):Camera (in_argc, in_argv)
 	baudRate->addSelVal ("921600", (rts2core::Rts2SelData *) CL_BAUDRATE_921600);
 
 	// interface registers
-	createRegister (0x00000000, "int.error_code", "holds the error code fomr the most recent error", false, true);
-	createRegister (0x00000001, "int.error_source", "holds an integer identifying the source of the most recent error", false, true);
-	createRegister (0x00000002, "int.error_line", "holds the line number in the interface CPUs", false, true);
-	createRegister (0x00000003, "int.status_index", "a counter that increments every time the interface CPU polss the system for its status", false, true);
-	createRegister (0x00000004, "int.power", "current CCD power state", true, true);
-	createRegister (0x00000005, "int.backplane_type", "board type field for the backplane board", false, false);
-	createRegister (0x00000006, "int.interface_type", "board type field for the interface board", false, false);
+	createRegister (0x00000000, "int.error_code", "holds the error code fomr the most recent error", false, true, true);
+	createRegister (0x00000001, "int.error_source", "holds an integer identifying the source of the most recent error", false, true, false);
+	createRegister (0x00000002, "int.error_line", "holds the line number in the interface CPUs", false, true, false);
+	createRegister (0x00000003, "int.status_index", "a counter that increments every time the interface CPU polss the system for its status", false, true, false);
+	createRegister (0x00000004, "int.power", "current CCD power state", true, true, false);
+
+	createRegister (0x00000005, "int.backplane_type", "board type field for the backplane board", false, false, true);
+	createRegister (0x00000006, "int.interface_type", "board type field for the interface board", false, false, true);
+	createRegister (0x00000007, "int.powerA_type", "PowerA board type", false, false, true);
+	createRegister (0x00000008, "int.powerB_type", "PowerB board type", false, false, true);
+	createRegister (0x00000009, "int.daughter1_type", "Daughter 1 board type", false, false, true);
+	createRegister (0x0000000A, "int.daughter2_type", "Daughter 2 board type", false, false, true);
+	createRegister (0x0000000B, "int.daughter3_type", "Daughter 3 board type", false, false, true);
+	createRegister (0x0000000C, "int.daughter4_type", "Daughter 4 board type", false, false, true);
+	createRegister (0x0000000D, "int.daughter5_type", "Daughter 5 board type", false, false, true);
+	createRegister (0x0000000E, "int.daughter6_type", "Daughter 6 board type", false, false, true);
+	createRegister (0x0000000F, "int.daughter7_type", "Daughter 7 board type", false, false, true);
+	createRegister (0x00000010, "int.daughter8_type", "Daughter 8 board type", false, false, true);
+
+	createRegister (0x00000011, "int.backplane_ROM", "Backplane ROM ID", false, false, true);
+	createRegister (0x00000012, "int.interface_ROM", "Backplane ROM ID", false, false, true);
+	createRegister (0x00000013, "int.powerA_ROM", "PowerA ROM ID", false, false, true);
+	createRegister (0x00000014, "int.powerB_ROM", "PowerB ROM ID", false, false, true);
+	createRegister (0x00000015, "int.daughter1_ROM", "Daughter 1 ROM ID", false, false, true);
+	createRegister (0x00000016, "int.daughter2_ROM", "Daughter 2 ROM ID", false, false, true);
+	createRegister (0x00000017, "int.daughter3_ROM", "Daughter 3 ROM ID", false, false, true);
+	createRegister (0x00000018, "int.daughter4_ROM", "Daughter 4 ROM ID", false, false, true);
+	createRegister (0x00000019, "int.daughter5_ROM", "Daughter 5 ROM ID", false, false, true);
+	createRegister (0x0000001A, "int.daughter6_ROM", "Daughter 6 ROM ID", false, false, true);
+	createRegister (0x0000001B, "int.daughter7_ROM", "Daughter 7 ROM ID", false, false, true);
+	createRegister (0x0000001C, "int.daughter8_ROM", "Daughter 8 ROM ID", false, false, true);
+
+	createRegister (0x0000001D, "int.backplane_build", "Backplane firmware build number", false, false, false);
+	createRegister (0x0000001E, "int.interface_build", "Backplane firmware build number", false, false, false);
+	createRegister (0x0000001F, "int.powerA_build", "PowerA firmware build number", false, false, false);
+	createRegister (0x00000020, "int.powerB_build", "PowerB firmware build number", false, false, false);
+	createRegister (0x00000021, "int.daughter1_build", "Daughter 1 firmware build number", false, false, false);
+	createRegister (0x00000022, "int.daughter2_build", "Daughter 2 firmware build number", false, false, false);
+	createRegister (0x00000023, "int.daughter3_build", "Daughter 3 firmware build number", false, false, false);
+	createRegister (0x00000024, "int.daughter4_build", "Daughter 4 firmware build number", false, false, false);
+	createRegister (0x00000025, "int.daughter5_build", "Daughter 5 firmware build number", false, false, false);
+	createRegister (0x00000026, "int.daughter6_build", "Daughter 6 firmware build number", false, false, false);
+	createRegister (0x00000027, "int.daughter7_build", "Daughter 7 firmware build number", false, false, false);
+	createRegister (0x00000028, "int.daughter8_build", "Daughter 8 firmware build number", false, false, false);
+
+	createRegister (0x00000029, "int.backplane_flags", "Backplane feature flags", false, false, true);
+	createRegister (0x0000002A, "int.interface_flags", "Backplane feature flags", false, false, true);
+	createRegister (0x0000002B, "int.powerA_flags", "PowerA feature flags", false, false, true);
+	createRegister (0x0000002C, "int.powerB_flags", "PowerB feature flags", false, false, true);
+	createRegister (0x0000002D, "int.daughter1_flags", "Daughter 1 feature flags", false, false, true);
+	createRegister (0x0000002E, "int.daughter2_flags", "Daughter 2 feature flags", false, false, true);
+	createRegister (0x0000002F, "int.daughter3_flags", "Daughter 3 feature flags", false, false, true);
+	createRegister (0x00000030, "int.daughter4_flags", "Daughter 4 feature flags", false, false, true);
+	createRegister (0x00000031, "int.daughter5_flags", "Daughter 5 feature flags", false, false, true);
+	createRegister (0x00000032, "int.daughter6_flags", "Daughter 6 feature flags", false, false, true);
+	createRegister (0x00000033, "int.daughter7_flags", "Daughter 7 feature flags", false, false, true);
+	createRegister (0x00000034, "int.daughter8_flags", "Daughter 8 feature flags", false, false, true);
+
+	configFile = NULL;
+	config = NULL;
+
+	addOption ('c', NULL, 1, "configuration file (.rcf)");
 }
 
 Reflex::~Reflex (void)
 {
-	for (std::map <uint32_t, RRegister *>::iterator iter = registers.begin (); iter != registers.end (); iter++)
-		delete iter->second;
-	
 	registers.clear ();
+
+	delete config;
 
 #ifdef CL_EDT
 	edt_close (CLHandle);
@@ -238,6 +308,9 @@ int Reflex::processOption (int in_opt)
 {
 	switch (in_opt)
 	{
+		case 'c':
+			configFile = optarg;
+			break;
 		default:
 			return Camera::processOption (in_opt);
 	}
@@ -255,6 +328,22 @@ int Reflex::initHardware ()
 	ret = openInterface (0);
 	if (ret)
 		return ret;
+
+	// read all registers
+	for (std::map <uint32_t, RRegister *>::iterator iter=registers.begin (); iter != registers.end (); iter++)
+	{
+		uint32_t rval;
+		if (readRegister (iter->first, rval))
+		{
+			logStream (MESSAGE_ERROR) << "error reading register 0x" << std::setw (8) << std::setfill ('0') << std::hex << iter->first << sendLog;
+			return -1;
+		}
+		iter->second->setValueInteger (rval);
+	}
+
+	createBoards ();
+
+	reloadConfig ();
 	return 0;
 }
 
@@ -267,13 +356,19 @@ int Reflex::info ()
 			uint32_t rval;
 			if (readRegister (iter->first, rval))
 			{
-				logStream (MESSAGE_ERROR) << "error reading register " << iter->first << sendLog;
+				logStream (MESSAGE_ERROR) << "error reading register 0x" << std::setw (8) << std::setfill ('0') << std::hex << iter->first << sendLog;
 				return -1;
 			}
 			iter->second->setValueInteger (rval);
 		}
 	}
 	return Camera::info ();
+}
+
+void Reflex::signaledHUP ()
+{
+	Camera::signaledHUP ();
+	reloadConfig ();
 }
 
 int Reflex::initValues ()
@@ -292,10 +387,11 @@ int Reflex::commandAuthorized (Rts2Conn * conn)
 	return Camera::commandAuthorized (conn);
 }
 
-RRegister * Reflex::createRegister (uint32_t address, const char *name, const char * desc, bool writable, bool infoupdate)
+RRegister * Reflex::createRegister (uint32_t address, const char *name, const char * desc, bool writable, bool infoupdate, bool hexa)
 {
 	RRegister *regval;
-	createValue (regval, name, desc, true, writable ? (RTS2_VALUE_WRITABLE | RTS2_DT_HEX)  : RTS2_DT_HEX);
+	int32_t flags = (writable ? RTS2_VALUE_WRITABLE : 0) | (hexa ? RTS2_DT_HEX : 0);
+	createValue (regval, name, desc, true, flags);
 	regval->setRegisterAddress (address);
 	if (infoupdate)
 		regval->setInfoUpdate ();
@@ -312,7 +408,7 @@ int Reflex::openInterface (int CLport)
 	//int CLBaudrate = (int) (baudRate->getData ());
 	int CLBaudrate = 115200;
 #ifdef CL_EDT
-	CLHandle = pdv_open (EDT_INTERFACE, 0);
+	CLHandle = pdv_open ((char *) EDT_INTERFACE, 0);
 	if (!CLHandle)
 	{
 		logStream (MESSAGE_ERROR) << "Error opening CameraLink port" << sendLog;
@@ -326,7 +422,7 @@ int Reflex::openInterface (int CLport)
 		logStream (MESSAGE_ERROR) << "Error setting CameraLink baud rate" << sendLog;
 		return -1;
 	}
-	pdv_set_serial_delimiters (CLHandle, "", "");
+	pdv_set_serial_delimiters (CLHandle, (char *) "", (char *) "");
 #else
 	int err = clSerialInit (CLport, &CLHandle);
 	if (err != CL_ERR_NO_ERR)
@@ -517,6 +613,122 @@ int Reflex::writeRegister(unsigned addr, unsigned data)
 	if (interfaceCommand (s, ret, 100))
 		return -1;
 	return 0;
+}
+
+void Reflex::reloadConfig ()
+{
+	if (!configFile)
+	{
+		logStream (MESSAGE_WARNING) << "empty configuration file (missing -c option?), camera is assumed to be configured" << sendLog;
+		return;
+	}
+
+	delete config;
+
+	config = new Rts2ConfigRaw ();
+	if (config->loadFile (configFile))
+		throw rts2core::Error ("cannot load .rcf configuration file");
+		
+	
+}
+
+void Reflex::createBoards ()
+{
+	uint32_t ba;
+	for (int bt = BOARD_TYPE_BP; bt < BOARD_TYPE_D8; bt++)
+	{
+		std::ostringstream biss;
+		biss << "board" << (bt - BOARD_TYPE_PB) << ".";
+		std::string bn = biss.str ();
+		switch (registers[bt]->getValueInteger () >> 24)
+		{
+			case BT_NONE:
+				// empty board
+				break;
+			case BT_BPX6:
+				ba = 0x00010000;
+				createRegister (ba, "back.temperature", "[mK] backplane module temperature", false, true, false);
+				createRegister (ba + 1, "back.status", "backplane status", false, true, true);
+				break;
+			case BT_CLIF:
+				ba = 0x00020000;
+				createRegister (ba, "clif.temperature", "[mK] camera link module temperature", false, true, false);
+				break;
+			case BT_PA:
+				ba = 0x00030000;
+				createRegister (ba++, "pwrA.temperature", "[mK] power module temperature", false, true, false);
+
+				createRegister (ba++, "pwrA.p5VD_V", "[mV] +5V digital supply voltage reading", false, true, false);
+				createRegister (ba++, "pwrA.p5VD_A", "[mA] +5V digital supply current reading", false, true, false);
+
+				createRegister (ba++, "pwrA.p5VA_V", "[mV] +5V analog supply voltage reading", false, true, false);
+				createRegister (ba++, "pwrA.p5VA_A", "[mA] +5V analog supply current reading", false, true, false);
+
+				createRegister (ba++, "pwrA.m5VA_V", "[mV] -5V analog supply voltage reading", false, true, false);
+				createRegister (ba++, "pwrA.m5VA_A", "[mA] -5V analog supply current reading", false, true, false);
+				break;
+			case BT_PB:
+				ba = 0x00040000;
+				createRegister (ba++, "pwrB.temperature", "[mK] power module temperature", false, true, false);
+
+				createRegister (ba++, "pwrB.p30VA_V", "[mV] +30V analog supply voltage reading", false, true, false);
+				createRegister (ba++, "pwrB.p30VA_A", "[mA] +30V analog supply current reading", false, true, false);
+
+				createRegister (ba++, "pwrB.p15VA_V", "[mV] +51V analog supply voltage reading", false, true, false);
+				createRegister (ba++, "pwrB.p15VA_A", "[mA] +15V analog supply current reading", false, true, false);
+
+				createRegister (ba++, "pwrB.m15VA_V", "[mV] -15V analog supply voltage reading", false, true, false);
+				createRegister (ba++, "pwrB.m15VA_A", "[mA] -15V analog supply current reading", false, true, false);
+
+				createRegister (ba++, "pwrB.TEC_set", "[mK] monitored value of TEC setpoint", false, true, false);
+				createRegister (ba++, "pwrB.TEC_actual", "[mK] current TEC temperature", false, true, false);
+				break;
+			case BT_AD8X120:
+			case BT_AD8X100:
+			case BT_DRIVER:
+				ba = (bt - POWER) << 16;
+				createRegister (ba++, (bn + "temperature").c_str (), "[mK] module temperature", false, true, false);
+				createRegister (ba++, (bn + "status").c_str (), "module status", false, true, true);
+				break;
+			case BT_BIAS:
+				{
+					ba = (bt - POWER) << 16;
+					createRegister (ba++, (bn + "temperature").c_str (), "[mK] module temperature", false, true, false);
+					int i;
+					for (i = 1; i < 9; i++)
+					{
+						std::ostringstream vname, comment;
+						vname << bn << "LV" << i;
+						comment << "[mv] Low-voltage bias #" << i << " voltage";
+						createRegister (ba++, vname.str ().c_str (), comment.str ().c_str (), false, true, false);
+					}
+					for (i = 1; i < 9; i++)
+					{
+						std::ostringstream vname, comment;
+						vname << bn << "HV" << i;
+						comment << "[mv] High-voltage bias #" << i << " voltage";
+						createRegister (ba++, vname.str ().c_str (), comment.str ().c_str (), false, true, false);
+					}
+					for (i = 1; i < 9; i++)
+					{
+						std::ostringstream vname, comment;
+						vname << bn << "LC" << i;
+						comment << "[uA] Low voltage bias #" << i << " current";
+						createRegister (ba++, vname.str ().c_str (), comment.str ().c_str (), false, true, false);
+					}
+					for (i = 1; i < 9; i++)
+					{
+						std::ostringstream vname, comment;
+						vname << bn << "HC" << i;
+						comment << "[uA] High-voltage bias #" << i << " current";
+						createRegister (ba++, vname.str ().c_str (), comment.str ().c_str (), false, true, false);
+					}
+				}
+				break;
+			default:
+				logStream (MESSAGE_ERROR) << "unknow board type " << std::hex << (registers[bt]->getValueInteger () >> 24) << sendLog;
+		}
+	}
 }
 
 int main (int argc, char **argv)
---
+++ src/camd/reflex.h
@@ -0,0 +1,101 @@
+/**
+ * STA Reflex constants, definitions, ..
+ * Copyright (C) 2011 Petr Kubanek, Institute of Physics <kubanek@fzu.cz>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __RTS2_REFLEX_H__
+#define __RTS2_REFLEX_H__
+
+#define FIXED_BOARD_COUNT          4
+#define MAX_DAUGHTER_COUNT         8
+#define TOTAL_BOARD_COUNT         12
+
+#define SYSTEM_STATUS_ADDR        0x00000000
+#define ERROR_SYSTEM_STATUS_ADDR  0x40000000
+
+// Board type values
+#define BT_NONE                   0x00
+#define BT_BPX6                   0x10
+#define BT_CLIF                   0x20
+#define BT_PA                     0x30
+#define BT_PB                     0x40
+#define BT_AD8X120                0x50
+#define BT_AD8X100                0x51
+#define BT_DRIVER                 0x60
+#define BT_BIAS                   0x70
+#define BT_HS                     0x80
+
+// System status registers, starting at address 0x00000000
+enum SYSTEM_STATUS_FIELDS
+{
+	ERROR_CODE,
+	ERROR_SRC,
+	ERROR_LINE,
+	STATUS_INDEX,
+	POWER,
+	BOARD_TYPE_BP,
+	BOARD_TYPE_IF,
+	BOARD_TYPE_PA,
+	BOARD_TYPE_PB,
+	BOARD_TYPE_D1,
+	BOARD_TYPE_D2,
+	BOARD_TYPE_D3,
+	BOARD_TYPE_D4,
+	BOARD_TYPE_D5,
+	BOARD_TYPE_D6,
+	BOARD_TYPE_D7,
+	BOARD_TYPE_D8,
+	ROMID_BP,
+	ROMID_IF,
+	ROMID_PA,
+	ROMID_PB,
+	ROMID_D1,
+	ROMID_D2,
+	ROMID_D3,
+	ROMID_D4,
+	ROMID_D5,
+	ROMID_D6,
+	ROMID_D7,
+	ROMID_D8,
+	BUILD_BP,
+	BUILD_IF,
+	BUILD_PA,
+	BUILD_PB,
+	BUILD_D1,
+	BUILD_D2,
+	BUILD_D3,
+	BUILD_D4,
+	BUILD_D5,
+	BUILD_D6,
+	BUILD_D7,
+	BUILD_D8,
+	FLAGS_BP,
+	FLAGS_IF,
+	FLAGS_PA,
+	FLAGS_PB,
+	FLAGS_D1,
+	FLAGS_D2,
+	FLAGS_D3,
+	FLAGS_D4,
+	FLAGS_D5,
+	FLAGS_D6,
+	FLAGS_D7,
+	FLAGS_D8,
+	SYSTEM_STATUS_SIZE
+};
+
+#endif // !__RTS2_REFLEX_H__
