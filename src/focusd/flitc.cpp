cdce1763de22ab04913622945a66dcd959f3828a
Petr Kubánek <petr@kubanek.net>
2012-01-15 10:42:39 +0000

    Rts2Address fix
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@9845 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/focusd/flitc.cpp
+++ src/focusd/flitc.cpp
@@ -49,7 +49,7 @@ class Fli:public Focusd
 		virtual int commandAuthorized (rts2core::Connection * conn);
 
 	protected:
-                virtual int willConnect (NetworkAddress * in_addr);
+                virtual int willConnect (rts2core::NetworkAddress * in_addr);
 		virtual int isFocusing ();
 		virtual bool isAtStartPosition ();
 
@@ -197,7 +197,7 @@ int Fli::processOption (int in_opt)
 	}
 	return 0;
 }
-int Fli::willConnect (NetworkAddress * in_addr)
+int Fli::willConnect (rts2core::NetworkAddress * in_addr)
 {
     if (meteoDevice && in_addr->getType () == DEVICE_TYPE_SENSOR) {
       logStream (MESSAGE_DEBUG) << "FLI::willConnect to DEVICE_TYPE_SENSOR: "<< meteoDevice << ", variable: "<< meteoVariable<< sendLog;
