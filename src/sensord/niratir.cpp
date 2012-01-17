538d4e67aa569d6f9565f602c3076d52ad299bfd
Petr Kubánek <petr@kubanek.net>
2012-01-17 17:11:04 +0000

    4 axis support
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@9860 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/sensord/niratir.cpp
+++ src/sensord/niratir.cpp
@@ -46,6 +46,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define MAX_AXIS      4
+
 using namespace rts2sensord;
 
 /**
@@ -70,17 +72,17 @@ class NIRatir:public Sensor
 	private:
 		const char *boardPCI;
 
-		rts2core::ValueInteger *ax1target;
-		rts2core::ValueInteger *ax1position;
+		rts2core::ValueInteger *axtarget[MAX_AXIS];
+		rts2core::ValueInteger *axposition[MAX_AXIS];
 
-		rts2core::ValueInteger *ax1velocity;
-		rts2core::ValueInteger *ax1maxv;
-		rts2core::ValueInteger *ax1basev;
+		rts2core::ValueInteger *axvelocity[MAX_AXIS];
+		rts2core::ValueInteger *axmaxv[MAX_AXIS];
+		rts2core::ValueInteger *axbasev[MAX_AXIS];
 
-		rts2core::ValueLong *ax1acceleration;
-		rts2core::ValueLong *ax1deceleration;
+		rts2core::ValueLong *axacceleration[MAX_AXIS];
+		rts2core::ValueLong *axdeceleration[MAX_AXIS];
 
-		rts2core::ValueBool *ax1enabled;
+		rts2core::ValueBool *axenabled[MAX_AXIS];
 
 		rts2core::BoolArray *ports;
 
@@ -92,28 +94,33 @@ class NIRatir:public Sensor
 
 NIRatir::NIRatir (int argc, char **argv):Sensor (argc, argv)
 {
-	createValue (ax1target, "AX1_TAR", "1st axis target position", false, RTS2_VALUE_WRITABLE);
-	ax1target->setValueInteger (0);
+	for (int i = 0; i < MAX_AXIS; i++)
+	{
+		std::ostringstream pr;
+		pr << "AX" << i << "_";
+		std::string prs = pr.str ();
+		createValue (axtarget[i], (prs + "TAR").c_str (), "axis target position", false, RTS2_VALUE_WRITABLE);
+		axtarget[i]->setValueInteger (0);
 
-	createValue (ax1position, "AX1_POS", "current 1st axis position", true);
+		createValue (axposition[i], (prs + "POS").c_str (), "current axis position", true);
 
-	createValue (ax1velocity, "AX1_VEL", "1st axis current velocity", false);
+		createValue (axvelocity[i], (prs + "VEL").c_str (), "axis current velocity", false);
 
-	createValue (ax1maxv, "AX1_MAX_VEL", "1st axis maximal velocity", false, RTS2_VALUE_WRITABLE);
-	createValue (ax1basev, "AX1_BASE_VEL", "1st axis base velocity", false, RTS2_VALUE_WRITABLE);
+		createValue (axmaxv[i], (prs + "MAX_VEL").c_str (), "axis maximal velocity", false, RTS2_VALUE_WRITABLE);
+		createValue (axbasev[i], (prs + "BASE_VEL").c_str (), "axis base velocity", false, RTS2_VALUE_WRITABLE);
 
-	ax1maxv->setValueInteger (2000);
-	ax1basev->setValueInteger (0);
+		axmaxv[i]->setValueInteger (2000);
+		axbasev[i]->setValueInteger (0);
 
-	createValue (ax1acceleration, "AX1_ACC", "1st axis acceleration", false, RTS2_VALUE_WRITABLE);
-	createValue (ax1deceleration, "AX1_DEC", "1st axis deceleration", false, RTS2_VALUE_WRITABLE);
+		createValue (axacceleration[i], (prs + "ACC").c_str (), "axis acceleration", false, RTS2_VALUE_WRITABLE);
+		createValue (axdeceleration[i], (prs + "DEC").c_str (), "axis deceleration", false, RTS2_VALUE_WRITABLE);
 
-	ax1acceleration->setValueLong (20);
-	ax1deceleration->setValueLong (20);
+		axacceleration[i]->setValueLong (20);
+		axdeceleration[i]->setValueLong (20);
 
-	createValue (ax1enabled, "AX1_ENB", "1st axis enabled", false, RTS2_VALUE_WRITABLE | RTS2_DT_ONOFF);
-	ax1enabled->setValueBool (true);
-
+		createValue (axenabled[i], (prs + "ENB").c_str (), "axis enabled", false, RTS2_VALUE_WRITABLE | RTS2_DT_ONOFF);
+		axenabled[i]->setValueBool (true);
+	}
 
 	int i;
 	createValue (ports, "PORTS", "I/O ports status", false, RTS2_VALUE_WRITABLE | RTS2_DT_ONOFF);
@@ -166,17 +173,22 @@ int NIRatir::initHardware ()
 	}
 
 	flex_enable_axis (0, 0);
-	flex_config_axis (NIMC_AXIS1, 0, 0, NIMC_STEP_OUTPUT1, 0);
-	flex_enable_axis (0x02, NIMC_PID_RATE_250);
-	flex_configure_stepper_output (NIMC_AXIS1, NIMC_STEP_AND_DIRECTION, NIMC_ACTIVE_HIGH, 0);
-	flex_load_counts_steps_rev (NIMC_AXIS1, NIMC_STEPS, 24);
-	flex_config_inhibit_output (NIMC_AXIS1, 0, 0, 0);
+	for (int i = 0; i < MAX_AXIS; i++)
+		flex_config_axis (NIMC_AXIS1 + i, 0, 0, NIMC_STEP_OUTPUT1, 0);
+	flex_enable_axis (0x1e, NIMC_PID_RATE_250);
 
-	flex_load_base_vel (NIMC_AXIS1, ax1basev->getValueInteger ());
-	flex_load_velocity (NIMC_AXIS1, ax1maxv->getValueInteger (), 0xff);
+	for (int i = 0; i < MAX_AXIS; i++)
+	{
+		flex_configure_stepper_output (NIMC_AXIS1 + i, NIMC_STEP_AND_DIRECTION, NIMC_ACTIVE_HIGH, 0);
+		flex_load_counts_steps_rev (NIMC_AXIS1 + i, NIMC_STEPS, 24);
+		flex_config_inhibit_output (NIMC_AXIS1 + i, 0, 0, 0);
 
-	flex_load_acceleration (NIMC_AXIS1, NIMC_ACCELERATION, ax1acceleration->getValueLong (), 0xff);
-	flex_load_acceleration (NIMC_AXIS1, NIMC_DECELERATION, ax1deceleration->getValueLong (), 0xff);
+		flex_load_base_vel (NIMC_AXIS1 + i, axbasev[i]->getValueInteger ());
+		flex_load_velocity (NIMC_AXIS1 + i, axmaxv[i]->getValueInteger (), 0xff);
+
+		flex_load_acceleration (NIMC_AXIS1 + i, NIMC_ACCELERATION, axacceleration[i]->getValueLong (), 0xff);
+		flex_load_acceleration (NIMC_AXIS1 + i, NIMC_DECELERATION, axdeceleration[i]->getValueLong (), 0xff);
+	}
 
 	// enable first 8 ADCs
 	flex_enable_adcs (0x00ff);
@@ -187,11 +199,14 @@ int NIRatir::initHardware ()
 int NIRatir::info ()
 {
 	int32_t cp;
-	flex_read_position_rtn (NIMC_AXIS1, &cp);
-	ax1position->setValueInteger (cp);
+	for (int i = 0; i < MAX_AXIS; i++)
+	{
+		flex_read_position_rtn (NIMC_AXIS1 + i, &cp);
+		axposition[i]->setValueInteger (cp);
 
-	flex_read_velocity_rtn (NIMC_AXIS1, &cp);
-	ax1velocity->setValueInteger (cp);
+		flex_read_velocity_rtn (NIMC_AXIS1 + i, &cp);
+		axvelocity[i]->setValueInteger (cp);
+	}
 
 	uint16_t portS;
 	int i;
@@ -215,37 +230,48 @@ int NIRatir::info ()
 
 int NIRatir::setValue (rts2core::Value *old_value, rts2core::Value *new_value)
 {
-	if (old_value == ax1target)
+	for (int i = 0; i < MAX_AXIS; i++)
 	{
-		moveAbs (NIMC_AXIS1, new_value->getValueInteger ());
-		return 0;
+		if (old_value == axtarget[i])
+		{
+			moveAbs (NIMC_AXIS1 + i, new_value->getValueInteger ());
+			return 0;
+		}
+		else if (old_value == axbasev[i])
+		{
+			flex_load_base_vel (NIMC_AXIS1 + i, new_value->getValueInteger ());
+			return 0;
+		}
+		else if (old_value == axmaxv[i])
+		{
+			flex_load_velocity (NIMC_AXIS1 + i, new_value->getValueInteger (), 0xff);
+			return 0;
+		}
+		else if (old_value == axacceleration[i])
+		{
+			flex_load_acceleration (NIMC_AXIS1 + i, NIMC_ACCELERATION, new_value->getValueLong (), 0xff);
+			return 0;
+		}
+		else if (old_value == axdeceleration[i])
+		{
+			flex_load_acceleration (NIMC_AXIS1 + i, NIMC_DECELERATION, new_value->getValueLong (), 0xff);
+			return 0;
+		}
+		else if (old_value == axenabled[i])
+		{
+			int16_t enable_map = 0;
+			for (int j = 0; j < MAX_AXIS; j++)
+			{
+				if (j != i && axenabled[j]->getValueBool ())
+					enable_map |= (0x02 << j);
+			}
+			if (((rts2core::ValueBool *) new_value)->getValueBool ())
+				enable_map |= (0x02 << i);
+			flex_enable_axis (enable_map, NIMC_PID_RATE_250);
+			return 0;
+		}
 	}
-	else if (old_value == ax1basev)
-	{
-		flex_load_base_vel (NIMC_AXIS1, new_value->getValueInteger ());
-		return 0;
-	}
-	else if (old_value == ax1maxv)
-	{
-		flex_load_velocity (NIMC_AXIS1, new_value->getValueInteger (), 0xff);
-		return 0;
-	}
-	else if (old_value == ax1acceleration)
-	{
-		flex_load_acceleration (NIMC_AXIS1, NIMC_ACCELERATION, new_value->getValueLong (), 0xff);
-		return 0;
-	}
-	else if (old_value == ax1deceleration)
-	{
-		flex_load_acceleration (NIMC_AXIS1, NIMC_DECELERATION, new_value->getValueLong (), 0xff);
-		return 0;
-	}
-	else if (old_value == ax1enabled)
-	{
-		flex_enable_axis (((rts2core::ValueBool *) new_value)->getValueBool () ? 0x02 : 0x0, NIMC_PID_RATE_250);
-		return 0;
-	}
-	else if (old_value == ports)
+	if (old_value == ports)
 	{
 		rts2core::BoolArray *nv = (rts2core::BoolArray *) new_value;
 		uint8_t mon = 0;
