8a01abd8b72d394d30da3ed94e0ed5d7772beca2
Petr Kubánek <petr@kubanek.net>
2012-02-20 06:12:41 +0000

    fixed astrometry.py, so it becomes usefull with non-FLWO images
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@9983 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- scripts/astrometry.py
+++ scripts/astrometry.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # (C) 2010, Markus Wildi, markus.wildi@one-arcsec.org
-# (C) 2011, Petr Kubanek, Institute of Physics <kubanek@fzu.cz>
+# (C) 2011-2012, Petr Kubanek, Institute of Physics <kubanek@fzu.cz>
 #   usage 
-#   img_astrometry.py fits_filename
+#   astrometry.py fits_filename
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 #   Or visit http://www.gnu.org/licenses/gpl.html.
 #
 
-__author__ = 'petr@fzu.cz'
+__author__ = 'kubanek@fzu.cz'
 
 import os
 import shutil
@@ -36,117 +36,106 @@ import pynova
 
 import dms
 
+def xy2wcs(x,y,fitsh):
+	"""Transform XY pixel coordinates to WCS coordinates"""
+	pass
+
 class imgAstrometryScript:
-    """calibrate a fits image with astrometry.net."""
-    def __init__(self,fits_file,odir=None,scale_relative_error=0.05,astrometry_bin='/home/petr/astrometry/bin'):
-        self.scale_relative_error=scale_relative_error
-	self.astrometry_bin=astrometry_bin
+	"""calibrate a fits image with astrometry.net."""
+	def __init__(self,fits_file,odir=None,scale_relative_error=0.05,astrometry_bin='/usr/local/astrometry/bin'):
+		self.scale_relative_error=scale_relative_error
+		self.astrometry_bin=astrometry_bin
 
-	self.odir = odir
-	if self.odir is None:
-		self.odir=tempfile.mkdtemp()
+		self.odir = odir
+		if self.odir is None:
+			self.odir=tempfile.mkdtemp()
 
-        self.infpath=self.odir + '/input.fits'
-        shutil.copy(fits_file, self.infpath)
+		self.infpath=self.odir + '/input.fits'
+		shutil.copy(fits_file, self.infpath)
 
-    def run(self,scale=None,ra=None,dec=None):
+	def run(self,scale=None,ra=None,dec=None):
 
-        solve_field=[self.astrometry_bin + '/solve-field', '-D', self.odir,'--no-plots', '--no-fits2fits']
+		solve_field=[self.astrometry_bin + '/solve-field', '-D', self.odir,'--no-plots', '--no-fits2fits']
 
-	if scale is not None:
-            scale_low=scale*(1-self.scale_relative_error)
-            scale_high=scale*(1+self.scale_relative_error)
-	    solve_field.append('-u')
-	    solve_field.append('app')
-	    solve_field.append('-L')
-	    solve_field.append(str(scale_low))
-	    solve_field.append('-H')
-	    solve_field.append(str(scale_high))
+		if scale is not None:
+			scale_low=scale*(1-self.scale_relative_error)
+			scale_high=scale*(1+self.scale_relative_error)
+			solve_field.append('-u')
+			solve_field.append('app')
+			solve_field.append('-L')
+			solve_field.append(str(scale_low))
+			solve_field.append('-H')
+			solve_field.append(str(scale_high))
 
-	if ra is not None and dec is not None:
-	    solve_field.append('--ra')
-	    solve_field.append(ra)
-	    solve_field.append('--dec')
-	    solve_field.append(dec)
-	    solve_field.append('--radius')
-	    solve_field.append('5')
+		if ra is not None and dec is not None:
+			solve_field.append('--ra')
+			solve_field.append(str(ra))
+			solve_field.append('--dec')
+			solve_field.append(str(dec))
+			solve_field.append('--radius')
+			solve_field.append('5')
 
-	solve_field.append(self.infpath)
+		solve_field.append(self.infpath)
 	    
-        proc=subprocess.Popen(solve_field,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		proc=subprocess.Popen(solve_field,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
-	radecline=re.compile('Field center: \(RA H:M:S, Dec D:M:S\) = \(([^,]*),(.*)\).')
+		radecline=re.compile('Field center: \(RA H:M:S, Dec D:M:S\) = \(([^,]*),(.*)\).')
 
-	ret = None
+		ret = None
 
-	while True:
-	    a=proc.stdout.readline()
-	    if a == '':
-	       break
-	    match=radecline.match(a)
-	    if match:
-	       ret=[dms.parseDMS(match.group(1)),dms.parseDMS(match.group(2))]
+		while True:
+			a=proc.stdout.readline()
+			print a,
+			if a == '':
+				break
+			match=radecline.match(a)
+			if match:
+				ret=[dms.parseDMS(match.group(1)),dms.parseDMS(match.group(2))]
 	       
-        # cleanup
-        shutil.rmtree(self.odir)
-
-	return ret
+		# cleanup
+		shutil.rmtree(self.odir)
+		
+		return ret
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 2:
-        print 'Usage: %s <odir> <fits filename> <xoffs> <yoffs> <origname>' % (sys.argv[0])
-        sys.exit(1)
+	if len(sys.argv) <= 1:
+		print 'Usage: %s <fits filename>' % (sys.argv[0])
+		sys.exit(1)
 
-    a = imgAstrometryScript(sys.argv[2],sys.argv[1])
+	a = imgAstrometryScript(sys.argv[1])
 
-    ra = dec = None
+	ra = dec = None
 
-    ff=pyfits.fitsopen(sys.argv[2],'readonly')
-    ra=ff[0].header['RA']
-    dec=ff[0].header['DEC']
-    object=ff[0].header['OBJECT']
-    ff.close()
+	ff=pyfits.fitsopen(sys.argv[1],'readonly')
+	ra=ff[0].header['TELRA']
+	dec=ff[0].header['TELDEC']
+	object=ff[0].header['OBJECT']
+	num=ff[0].header['IMGID']
+	ff.close()
 
-    xoffs = 0
-    yoffs = 0
+    	ret=a.run(scale=0.6,ra=ra,dec=dec)
 
-    if len(sys.argv) >= 5:
-	xoffs = float(sys.argv[3])
-	yoffs = float(sys.argv[4])
+	if ret:
+		raorig=ra
+		decorig=dec
 
-    num = -1
-    try:
-    	    fn = os.path.basename(sys.argv[5])
-	    num = int(fn.split('.')[0])
-    except Exception,ex:
-    	    pass
+		raastr=float(ret[0])*15.0
+		decastr=float(ret[1])
 
-    ret=a.run(scale=0.67,ra=ra,dec=dec)
+		err = pynova.angularSeparation(raorig,decorig,raastr,decastr)
 
-    if ret:
-	    raorig=dms.parseDMS(ra)*15.0
-	    decorig=dms.parseDMS(dec)
+		print "corrwerr 1 {0:.10f} {1:.10f} {2:.10f} {3:.10f} {4:.10f}".format(raastr, decastr, raorig-raastr, decorig-decastr, err)
 
-	    raastr=float(ret[0])*15.0
-	    decastr=float(ret[1])
+		import rts2comm
+		c = rts2comm.Rts2Comm()
+		c.doubleValue('real_ra','[hours] image ra as calculated from astrometry',raastr)
+		c.doubleValue('real_dec','[deg] image dec as calculated from astrometry',decastr)
 
-	    err = pynova.angularSeparation(raorig,decorig,raastr-xoffs/3600,decastr-yoffs/3600.0)
+		c.doubleValue('tra','[hours] telescope ra',raorig)
+		c.doubleValue('tdec','[deg] telescope dec',decorig)
 
-	    print "corrwerr 1 {0:.10f} {1:.10f} {2:.10f} {3:.10f} {4:.10f}".format(raastr + xoffs/3600.0, decastr + yoffs/3600.0, ((raorig-raastr)-xoffs/3600.0), decorig-decastr-yoffs/3600.0, err)
+		c.doubleValue('ora','[arcdeg] offsets ra ac calculated from astrometry',raorig-raastr)
+		c.doubleValue('odec','[arcdeg] offsets dec as calculated from astrometry',decorig-decastr)
 
-	    import rts2comm
-	    c = rts2comm.Rts2Comm()
-	    c.doubleValue('real_ra','[hours] image ra ac calculated from astrometry',raastr)
-	    c.doubleValue('real_dec','[deg] image dec as calculated from astrometry',decastr)
-
-	    c.doubleValue('tra','[hours] image ra ac calculated from astrometry',raorig)
-	    c.doubleValue('tdec','[deg] image dec as calculated from astrometry',decorig)
-
-	    c.doubleValue('xoffs','[arcsec] x offset',xoffs)
-	    c.doubleValue('yoffs','[arcsec] y offset',yoffs)
-
-	    c.doubleValue('ora','[arcsec] offsets ra ac calculated from astrometry',(raorig-raastr)*3600.0-xoffs)
-	    c.doubleValue('odec','[arcsec] offsets dec as calculated from astrometry',(decorig-decastr)*3600.0-yoffs)
-
-	    c.stringValue('object','astrometry object',object)
-	    c.integerValue('img_num','last astrometry number',num)
+		c.stringValue('object','astrometry object',object)
+		c.integerValue('img_num','last astrometry number',num)
