dd04119bd82a3c2acc47a2d1da1e92e40252e6ff
Petr Kubánek <petr@kubanek.net>
2012-05-15 11:57:23 +0000

    apply LGPL
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@10419 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- python/lib/centering.py
+++ python/lib/centering.py
@@ -2,16 +2,28 @@
 #
 # Copyright (C) 2012 Petr Kubanek <petr@kubanek.net>
 #
-# Implements intelligend starfield search.
-# Take an exposure, run image processing on it. If the field is not matched, offset in cross pattern around center and try again.
-# Search in spiral pattern around expected position.
-
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
 import imgprocess
 import scriptcomm
 
 sc = scriptcomm.Rts2Comm()
 
 class Centering(scriptcomm.Rts2Comm):
+	"""Implements intelligend starfield search. Take an exposure, run image processing on it. If the field is not matched, offset in cross pattern around center and try again. Search in spiral pattern around expected position."""
+
 	def __init__(self):
 		scriptcomm.Rts2Comm.__init__(self)
 		self.telname = self.getDeviceByType(scriptcomm.DEVICE_TELESCOPE)
