786068e10a9df64eee61dc4298279a04e2ca6517
Petr Kubánek <petr@kubanek.net>
2012-07-30 04:12:35 +0000

    added matching script
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@10695 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ scripts/match.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# match telescope on given position
+
+import rts2
+
+sc = rts2.Rts2Comm()
+t = sc.getDeviceByType(rts2.scriptcomm.DEVICE_TELESCOPE)
+
+sc.setValue('filter','C')
+
+c = rts2.Centering()
+c.run (0,0,30)
+
+sc.sendCommand('syncorr',t)
