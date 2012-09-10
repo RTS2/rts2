4914945e05c67aad2fd2b62b1b2f610ddf55cd37
Petr Kubánek <petr@kubanek.net>
2012-09-10 09:35:44 +0000

    added BB tables
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@10838 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ src/sql/bb/create.sql
@@ -0,0 +1,26 @@
+CREATE TABLE observatories (
+	observatory_id	integer PRIMARY KEY,
+	longitude	float8, -- geographical informations
+	latitude	float8,
+	altitude	float8,
+	apiurl		VARCHAR(100)
+);
+
+CREATE TABLE targets_observatories (
+	observatory_id	integer REFERENCES observatories(observatory_id),
+	tar_id		integer REFERENCES targets (tar_id),
+	obs_tar_id	integer NOT NULL
+);
+
+CREATE TABLE observatory_observations (
+	observatory_id	integer REFERENCES observatories (observatory_id),
+	obs_id 		integer NOT NULL,
+	tar_id		integer REFERENCES targets (tar_id),
+	obs_ra		float8,
+	obs_dec		float8,
+	obs_slew	timestamp,
+	obs_start	timestamp,
+	obs_end		timestamp,
+	good_images	integer,
+	bad_images	integer
+);
