178ef814462546b106b59811f4e97c74972ae492
Petr Kubánek <petr@kubanek.net>
2012-11-01 13:57:33 +0000

    added schedule
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@10942 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ include/rts2db/schedule.h
@@ -0,0 +1,41 @@
+/* 
+ * Schedule class.
+ * Copyright (C) 2012 Petr Kubanek <petr@kubanek.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __RTS2_SCHEDULE__
+#define __RTS2_SCHEDULE__
+
+namespace rts2db
+{
+
+class QueueEntry
+{
+	public:
+		Schedule ();
+
+		void load ();
+	private:
+		int quid;
+		int tar_id;
+		double start;
+		double end;
+};
+
+}
+
+#endif // __RTS2_SCHEDULE__
