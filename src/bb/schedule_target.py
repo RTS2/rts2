ed08a7484738970dc4dc16ee02fc1d8a85680ba9
Petr Kubánek <petr@kubanek.net>
2012-12-06 17:57:26 +0000

    bb_schedule call
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@11104 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/bb/schedule_target.py
+++ src/bb/schedule_target.py
@@ -4,19 +4,20 @@ import sys
 import rts2.json
 from optparse import OptionParser
 
-parser = OptionParser(usage='usage: %prog --obs-tar-id <id> [--create <target_name> <ra> <dec>] <observatory API url>')
+parser = OptionParser(usage='usage: %prog --obs-tar-id <id> [--create <target_name> <ra> <dec>] <observatory API urls..>')
 parser.add_option('--tar-id', help='target ID (on central node)', action='store', dest='tar_id', default=None)
-parser.add_option('--create', help='Create target', action='store_true', dest='create', default=False)
-parser.add_option('--verbose', help='Report progress', action='store_true', dest='verbose', default=False)
+parser.add_option('--create', help='create target', action='store_true', dest='create', default=False)
+parser.add_option('--verbose', help='report progress', action='store_true', dest='verbose', default=False)
 
 (options, args) = parser.parse_args()
 
-obs_url = args[1]
+for obs_urls in sys.argv[1:]:
+	rts2.json.createProxy(obs_url, 'petr', 'test', False)
 
-rts2.json.createProxy(obs_url, 'petr', 'test', False)
+	if options.create:
+		ret = rts2.json.getProxy().loadJson('/api/create_target', {'tn':tar_name, 'ra':tar_ra, 'dec':tar_dec})
+		print 'mapping', options.tar_id, ret["id"]
 
-if options.create:
-	ret = rts2.json.getProxy().loadJson('/api/create_target', {'tn':tar_name, 'ra':tar_ra, 'dec':tar_dec})
-	print 'mapping',options.tar_id,ret["id"]
-
+	# contact about when the target will be able to schedule..
+	ret = rts2.json.getProxy().loadJson('/api/bb_schedule', {'id':options.tar_id})
 
