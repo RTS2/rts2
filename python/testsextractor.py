acdfb949d5ed7cd8cc11702ae41130d79d13722d
Petr Kubánek <petr@kubanek.net>
2013-04-30 11:10:56 +0000

    added FWHM
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@11429 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- python/testsextractor.py
+++ python/testsextractor.py
@@ -5,7 +5,7 @@ from rts2 import Sextractor
 import ds9
 
 def measure(fn):
-	s = Sextractor(fields=['X_IMAGE','Y_IMAGE','X_WORLD','Y_WORLD','ERRX2_IMAGE','ERRY2_IMAGE','MAG_BEST'])
+	s = Sextractor(fields=['X_IMAGE','Y_IMAGE','ALPHA_J2000','DELTA_J2000','ERRX2_IMAGE','ERRY2_IMAGE','MAG_BEST','FWHM_IMAGE'])
 	s.runSExtractor(fn)
 
 	print s.objects
@@ -16,6 +16,7 @@ def measure(fn):
 	d.set('file ' + fn)
 	for o in s.objects:
 		print o
-		d.set('regions','circle {0} {1} {2}'.format(o[0],o[1],int(abs(o[6]))))
+		d.set('regions','J2000; circle {0} {1} {2}p'.format(o[2],o[3],int(abs(o[6]))))
+		d.set('regions','J2000; circle {0} {1} {2}p # color=red'.format(o[2],o[3],o[7]))
 
 measure(sys.argv[1])
