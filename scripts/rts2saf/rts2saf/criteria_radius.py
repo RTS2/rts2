d5045c02d6f1446a57068b21918555489cbc67d6
Markus Wildi <markus.wildi@one-arcsec.org>
2013-10-30 21:33:32 +0000

    analysis criteria prototype, missing docu file
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@11652 65ab8f4e-f147-0410-b3a9-f14133ecfe55

---
+++ scripts/rts2saf/rts2saf/criteria_radius.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# (C) 2013, Markus Wildi, markus.wildi@bluewin.ch
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2, or (at your option)
+#   any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software Foundation,
+#   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+#   Or visit http://www.gnu.org/licenses/gpl.html.
+#
+
+__author__ = 'markus.wildi@bluewin.ch'
+
+import math
+
+
+class Criteria(object):
+    def __init__(self, dataSex=None, rt=None):
+        self.dataSex=dataSex
+        self.rt=rt
+
+        self.i_x = self.dataSex[0].fields.index('X_IMAGE')
+        self.i_y = self.dataSex[0].fields.index('Y_IMAGE')
+        self.center=[ self.dataSex[0].naxis1/2.,self.dataSex[0].naxis2/2. ] 
+        rds= self.rt.cfg['RADIUS'] 
+        if self.dataSex[0].binning:
+            self.radius= rds/self.dataSex[0].binning 
+        elif self.dataSex[0].binningXY:
+            # ToDo (bigger): only x value is used
+            self.radius= rds/self.dataSex[0].binningXY[0] 
+        else:
+            # everything should come
+            self.radius=pow(self.dataSex[0].naxis1, 2) + pow(self.dataSex[0].naxis2, 2)
+        
+
+    def decide(self, catalogEntry=None):
+        rd= math.sqrt(pow(catalogEntry[self.i_x]-self.center[0],2)+ pow(catalogEntry[self.i_y]-self.center[1],2))
+        if rd < self.radius:
+            return True
+        else:
+            return False
