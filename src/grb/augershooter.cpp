d60bf198cd589b4a11328e2c3f87fab8f96e6957
Petr Kubánek <petr@kubanek.net>
2013-12-11 22:11:02 +0000

    autosave values
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@11725 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/grb/augershooter.cpp
+++ src/grb/augershooter.cpp
@@ -35,127 +35,127 @@ DevAugerShooter::DevAugerShooter (int in_argc, char **in_argv):DeviceDb (in_argc
 	addOption (OPT_TRIGERING, "disable-triggering", 0, "only record triggers, do not pass them to executor");
 	addOption ('t', NULL, 1, "test parsing of auger data");
 
-	createValue (triggeringEnabled, "triggering_enabled", "if true, shooter will trigger executor", false, RTS2_VALUE_WRITABLE);
+	createValue (triggeringEnabled, "triggering_enabled", "if true, shooter will trigger executor", false, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	triggeringEnabled->setValueBool (true);
 
-	createValue (lastAugerSeen, "last_rejected", "date of last rejected shower", false);	
-	createValue (lastAugerDate, "last_date", "date of last accepted shower", false);
-	createValue (lastAugerMessage, "last_message", "date of last message", false);
-	createValue (lastAugerRa, "last_ra", "RA of last shower", false, RTS2_DT_RA);
-	createValue (lastAugerDec, "last_dec", "DEC of last shower", false, RTS2_DT_DEC);
+	createValue (lastAugerSeen, "last_rejected", "date of last rejected shower", false, RTS2_VALUE_AUTOSAVE);
+	createValue (lastAugerDate, "last_date", "date of last accepted shower", false, RTS2_VALUE_AUTOSAVE);
+	createValue (lastAugerMessage, "last_message", "date of last message", false, RTS2_VALUE_AUTOSAVE);
+	createValue (lastAugerRa, "last_ra", "RA of last shower", false, RTS2_DT_RA | RTS2_VALUE_AUTOSAVE);
+	createValue (lastAugerDec, "last_dec", "DEC of last shower", false, RTS2_DT_DEC | RTS2_VALUE_AUTOSAVE);
 
 
-	createValue (EyeId1, "Eye_Id_cut1", "Eye Id, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (EyeId1, "Eye_Id_cut1", "Eye Id, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	EyeId1->setValueInteger (1);
 
-	createValue (minEnergy, "min_energy_cut1", "minimal shower energy, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (minEnergy, "min_energy_cut1", "minimal shower energy, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minEnergy->setValueDouble (10);
 
-	createValue (maxXmaxErr, "max_xmax_err_cut1", "Maximal XmaxErr value, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (maxXmaxErr, "max_xmax_err_cut1", "Maximal XmaxErr value, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxXmaxErr->setValueDouble (40);
 
-	createValue (maxEnergyDiv, "max_energy_div_cut1", "Maximal energy div (EnergyErr / Energy), cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (maxEnergyDiv, "max_energy_div_cut1", "Maximal energy div (EnergyErr / Energy), cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxEnergyDiv->setValueDouble (0.2);
 
-	createValue (maxGHChiDiv, "max_GHChi_div_cut1", "Maximal GHChi div (GHChi2 / GHNdf), cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (maxGHChiDiv, "max_GHChi_div_cut1", "Maximal GHChi div (GHChi2 / GHNdf), cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxGHChiDiv->setValueDouble (2.5);
 
-	createValue (minLineFitDiff, "min_LineFit_diff_cut1", "Minimal line fit difference (LineFitChi2 - GHChi2), cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (minLineFitDiff, "min_LineFit_diff_cut1", "Minimal line fit difference (LineFitChi2 - GHChi2), cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minLineFitDiff->setValueDouble (4);
 
-	createValue (maxAxisDist, "min_Axis_dist_cut1", "Maximal axis distance, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (maxAxisDist, "min_Axis_dist_cut1", "Maximal axis distance, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxAxisDist->setValueDouble (2000.0);
 
-	createValue (minRp, "min_Rp_cut1", "Minimal Rp Value, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (minRp, "min_Rp_cut1", "Minimal Rp Value, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minRp->setValueDouble (0);
 
-	createValue (minChi0, "min_Chi0_cut1", "Minimal Chi0 value, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (minChi0, "min_Chi0_cut1", "Minimal Chi0 value, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minChi0->setValueDouble (0);
 
-	createValue (maxSPDDiv, "max_SPDDiv_cut1", "Maximal SPD Div, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (maxSPDDiv, "max_SPDDiv_cut1", "Maximal SPD Div, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxSPDDiv->setValueDouble (7);
 	
-	createValue (maxTimeDiv, "max_TimeDiv_cut1", "Maximal time difference, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (maxTimeDiv, "max_TimeDiv_cut1", "Maximal time difference, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxTimeDiv->setValueDouble (8);
 
-	createValue (maxTheta, "max_Theta_cut1", "Maximal Theta value, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (maxTheta, "max_Theta_cut1", "Maximal Theta value, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxTheta->setValueDouble (70);
 
-	createValue (maxTime, "max_time_cut1", "maximal time between shower and its observations, cut1", true, RTS2_VALUE_WRITABLE);
+	createValue (maxTime, "max_time_cut1", "maximal time between shower and its observations, cut1", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxTime->setValueInteger (600);
 
  /*       second set of cuts         */
-	createValue (EyeId2, "Eye_Id_cut2", "Eye Id, cut2", true, RTS2_VALUE_WRITABLE);
+	createValue (EyeId2, "Eye_Id_cut2", "Eye Id, cut2", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	EyeId2->setValueInteger (1);
 
-	createValue (minEnergy2, "min_energy_cut2", "minimal shower energy, cut2", true, RTS2_VALUE_WRITABLE);
+	createValue (minEnergy2, "min_energy_cut2", "minimal shower energy, cut2", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minEnergy2->setValueDouble (10.);
 
-	createValue (minPix2, "min_Pix_cut2", "Minimal number of pixels, cut2", true, RTS2_VALUE_WRITABLE);
+	createValue (minPix2, "min_Pix_cut2", "Minimal number of pixels, cut2", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minPix2->setValueInteger (6);
 
-	createValue (maxAxisDist2, "min_Axis_dist_cut2", "Maximal axis distance, cut2", true, RTS2_VALUE_WRITABLE);
+	createValue (maxAxisDist2, "min_Axis_dist_cut2", "Maximal axis distance, cut2", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxAxisDist2->setValueDouble (1500.0);
 
-	createValue (maxTimeDiff2, "max_Time_Diff_cut2", "Maximal FD/SD time difference, cut2", true, RTS2_VALUE_WRITABLE);
+	createValue (maxTimeDiff2, "max_Time_Diff_cut2", "Maximal FD/SD time difference, cut2", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxTimeDiff2->setValueDouble (300.0);
 
-	createValue (maxGHChiDiv2, "max_GHChi_div_cut2", "Maximal GHChi div (GHChi2 / GHNdf), cut2", true, RTS2_VALUE_WRITABLE);
+	createValue (maxGHChiDiv2, "max_GHChi_div_cut2", "Maximal GHChi div (GHChi2 / GHNdf), cut2", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxGHChiDiv2->setValueDouble (6.);
 
-	createValue (maxLineFitDiv2, "max_LineFit_div_cut2", "Minimal line fit difference (GHChi2 / LineFitChi2), cut2", true, RTS2_VALUE_WRITABLE);
+	createValue (maxLineFitDiv2, "max_LineFit_div_cut2", "Minimal line fit difference (GHChi2 / LineFitChi2), cut2", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxLineFitDiv2->setValueDouble (0.9);
 
-	createValue (minViewAngle2, "min_View_Angle_cut2", "Minimal viewing angle, cut2", true, RTS2_VALUE_WRITABLE);
+	createValue (minViewAngle2, "min_View_Angle_cut2", "Minimal viewing angle, cut2", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minViewAngle2->setValueDouble (15.0);
  /*       second set of cuts - end   */
 
  /*       third set of cuts          */
-	createValue (EyeId3, "Eye_Id_cut3", "Eye Id, cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (EyeId3, "Eye_Id_cut3", "Eye Id, cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	EyeId3->setValueInteger (1);
 
-	createValue (minEnergy3, "min_energy_cut3", "minimal shower energy, cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (minEnergy3, "min_energy_cut3", "minimal shower energy, cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minEnergy3->setValueDouble (10.);
 
-	createValue (maxXmaxErr3, "max_xmax_err_cut3", "Maximal XmaxErr value, cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (maxXmaxErr3, "max_xmax_err_cut3", "Maximal XmaxErr value, cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxXmaxErr3->setValueDouble (80.);
 
-	createValue (maxEnergyDiv3, "max_energy_div_cut3", "Maximal energy div (EnergyErr / Energy), cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (maxEnergyDiv3, "max_energy_div_cut3", "Maximal energy div (EnergyErr / Energy), cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxEnergyDiv3->setValueDouble (0.4);
 
-	createValue (maxGHChiDiv3, "max_GHChi_div_cut3", "Maximal GHChi div (GHChi2 / GHNdf), cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (maxGHChiDiv3, "max_GHChi_div_cut3", "Maximal GHChi div (GHChi2 / GHNdf), cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxGHChiDiv3->setValueDouble (10.);
 
-	createValue (maxLineFitDiv3, "max_LineFit_div_cut3", "Minimal line fit difference (GHChi2 / LineFitChi2), cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (maxLineFitDiv3, "max_LineFit_div_cut3", "Minimal line fit difference (GHChi2 / LineFitChi2), cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxLineFitDiv3->setValueDouble (1.);
 
-	createValue (minPix3, "min_Pix_cut3", "Minimal number of pixels, cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (minPix3, "min_Pix_cut3", "Minimal number of pixels, cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minPix3->setValueInteger (10);
 
-	createValue (maxAxisDist3, "min_Axis_dist_cut3", "Maximal axis distance, cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (maxAxisDist3, "min_Axis_dist_cut3", "Maximal axis distance, cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxAxisDist3->setValueDouble (2000.0);
 
-	createValue (minRp3, "min_Rp_cut3", "Minimal Rp Value, cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (minRp3, "min_Rp_cut3", "Minimal Rp Value, cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minRp3->setValueDouble (0.);
 
-	createValue (minChi03, "min_Chi0_cut3", "Minimal Chi0 value, cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (minChi03, "min_Chi0_cut3", "Minimal Chi0 value, cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minChi03->setValueDouble (0.);
 
-	createValue (maxSPDDiv3, "max_SPDDiv_cut3", "Maximal SPD Div, cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (maxSPDDiv3, "max_SPDDiv_cut3", "Maximal SPD Div, cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxSPDDiv3->setValueDouble (7.);
 	
-	createValue (maxTimeDiv3, "max_TimeDiv_cut3", "Maximal time difference, cut3", true, RTS2_VALUE_WRITABLE);
+	createValue (maxTimeDiv3, "max_TimeDiv_cut3", "Maximal time difference, cut3", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxTimeDiv3->setValueDouble (8.);
  /*       third set of cuts - end    */
 
  /*       fourth set of cuts         */
-	createValue (EyeId4, "Eye_Id_cut4", "Eye Id, cut4", true, RTS2_VALUE_WRITABLE);
+	createValue (EyeId4, "Eye_Id_cut4", "Eye Id, cut4", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	EyeId4->setValueInteger (1);
 
-	createValue (minNPix4, "min_npix_cut4", "minimal number of pixels, cut4", true, RTS2_VALUE_WRITABLE);
+	createValue (minNPix4, "min_npix_cut4", "minimal number of pixels, cut4", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	minNPix4->setValueDouble (10.);
 
-	createValue (maxDGHChi2Improv4, "max_dghchi2_improv4", "maximal DGHChi2 Improv, cut4", true, RTS2_VALUE_WRITABLE);
+	createValue (maxDGHChi2Improv4, "max_dghchi2_improv4", "maximal DGHChi2 Improv, cut4", true, RTS2_VALUE_WRITABLE | RTS2_VALUE_AUTOSAVE);
 	maxDGHChi2Improv4->setValueDouble (-10.);
 
  /*       fourth set of cuts - end   */
