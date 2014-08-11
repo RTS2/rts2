6c029b8c39e5d685320be03434a4d824282a501b
Guangyu Zhang <zguangyu@mail.ustc.edu.cn>
2014-08-11 05:16:01 +0000

    rts2-mon: make the helpbox center in the terminal and fit the content.
    git-svn-id: http://svn.code.sf.net/p/rts-2/code/trunk/rts-2@12103 65ab8f4e-f147-0410-b3a9-f14133ecfe55

--- src/monitor/nmsgbox.cpp
+++ src/monitor/nmsgbox.cpp
@@ -84,11 +84,11 @@ void NMsgBox::printMessage ()
 	mvwprintw (window, 1, 2, "%s", query);
 }
 
-NMsgBoxWin::NMsgBoxWin (const char *in_query, const char *in_buttons[], int in_butnum):NMsgBox (in_query, in_buttons, in_butnum, COLS / 2 - 15, LINES / 2 - 15, 80, 30)
+NMsgBoxWin::NMsgBoxWin (const char *in_query, const char *in_buttons[], int in_butnum):NMsgBox (in_query, in_buttons, in_butnum, COLS / 2 - 25, LINES / 2 - 15, 50, 30)
 {
 	but_lo = 25;
 
-	msgw = newwin (28, 48, LINES / 2 - 14, COLS / 2 - 14);
+	msgw = newwin (28, 48, LINES / 2 - 14, COLS / 2 - 24);
 	if (!msgw)
 		errorMove ("newwin msg", COLS / 2 - 24, LINES / 2 - 14, 48, 3);
 }
