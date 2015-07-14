cbc50245d8a9ca26d6f370ac4dd1763649a8b2d0
Petr Kubánek <petr@kubanek.net>
2015-07-14 10:17:48 +0200

    add comments, ..

--- scripts/telmodel.py
+++ scripts/telmodel.py
@@ -4,10 +4,16 @@ from astropy import units as u
 from astropy.time import Time
 import random
 
+# Location of the observatory. Longitutes are measured increasing to the east,
+# so west longitudes are negative.
 obs = EarthLocation(lat=20*u.deg,lon=-110*u.deg,height=3000*u.m)
+
+# Time for which to build poitings. Default to now.
+#obstime = Time('2015-07-04 22:33:00')
+
 obstime = Time('2015-07-04 22:33:00')
 
-# get random alt/az
+# get random alt/az, separated by 20 degrees, +- 3 deg off, at 45 degrees altitude
 for az in range(0,360,20):
 	t_alt = (45 + random.random() * 3) * u.deg
 	t_az = (az + random.random() * 3) * u.deg
