59370033639eeafb95276230a51219580484e2ae
Petr Kubánek <petr@kubanek.net>
2015-12-26 11:01:48 +0100

    use center of chip

--- scripts/model-input.py
+++ scripts/model-input.py
@@ -11,12 +11,12 @@ def print_model_input(filename):
 	hdulist = fits.open(filename)
 	h = hdulist[0].header
 	w = wcs.WCS(h)
-	ra,dec = w.all_pix2world(2000,2000,0)
+	ra,dec = w.all_pix2world(float(h['NAXIS1'])/2,float(h['NAXIS2'])/2,0)
 	tar_telra = float(h['TAR_TELRA'])
 	tar_teldec = float(h['TAR_TELDEC'])
 	print(h['IMGID'],h['JD'],h['LST'],tar_telra,tar_teldec,h['AXRA'],h['AXDEC'],ra,dec)
 
 if __name__ == '__main__':
-	print('#  Observation	  MJD	   RA-MNT   DEC-MNT LST-MNT	  AXRA	  AXDEC   RA-TRUE  DEC-TRUE')
+	print('#  Observation	  MJD	MNT-LST   RA-MNT   DEC-MNT 	  AXRA	  AXDEC   RA-TRUE  DEC-TRUE')
 	for img in sys.argv[1:]:
 		print_model_input(img)
