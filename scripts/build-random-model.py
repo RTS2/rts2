411f07e970c5fe04cab7c895dc94a5030ff120b3
Petr Kubánek <petr@kubanek.net>
2016-02-20 00:04:36 +0100

    use single array to hold altaz points

--- scripts/build-random-model.py
+++ scripts/build-random-model.py
@@ -5,32 +5,24 @@ import numpy as np
 import random
 
 polar = pylab.subplot(projection='polar')
-az=[]
-alt=[]
+azalt=[]
 for al in [25.3,36.2,45.1,54.7,63.2,73.1,82.2]:
 	for a in np.arange(0 + al/10.0, 360.0, 36.3 * (np.sin(np.radians(al)))):
-		az.append(a+random.random() * 3)
-		alt.append(al + random.random() * 2)
+		azalt.append([a+random.random() * 3, al + random.random() * 2])
 
-alt.append(89.2)
-az.append(123.34)
+azalt.append([89.2,123.34])
 
-print len(az)
+print len(azalt)
 
-azz = []
-altt = []
+path = []
 
-while len(az) > 1:
-	i = int(round(random.random() * len(az)))
-	if i == len(az):
-		i = len(az) - 1
-	a = az[i]
-	al = alt[i]
-	azz.append(a)
-	altt.append(al)
-	print "{0},{1}".format(round(az[i],2),round(alt[i],2)),
-	az = az[0:i] + az[i+1:]
-	alt = alt[0:i] + alt[i+1:]
+while len(azalt) > 0:
+	i = int(round(random.random() * len(azalt)))
+	if i == len(azalt):
+		i = len(azalt) - 1
+	path.append(azalt[i])
+	print "{0},{1}".format(round(azalt[i][0],2),round(azalt[i][1],2)),
+	azalt = azalt[0:i] + azalt[i+1:]
 
-polar.plot(np.radians(azz), 90 - np.array(altt), 'g')
+polar.plot(np.radians(map(lambda x:x[0],path)), 90 - np.array(map(lambda x:x[1],path)), 'g')
 pylab.show()
