fda9eae0bc30cb6aa23264dd2915c50b5b5243a7
Petr Kubánek <petr@kubanek.net>
2016-03-12 23:20:26 +0100

    temperature focusing values marked writable

--- include/focusd.h
+++ include/focusd.h
@@ -133,10 +133,10 @@ class Focusd:public rts2core::Device
 
 		void createLinearOffset ()
 		{
-			createValue (linearOffset, "linear_offset", "linear offset", false, RTS2_DT_ONOFF);
+			createValue (linearOffset, "linear_offset", "linear offset", false, RTS2_DT_ONOFF | RTS2_VALUE_WRITABLE);
 			linearOffset->setValueBool (false);
-			createValue (slope, "linear_slope", "slope parameter for linear function to fit temperature sensor", false);
-			createValue (intercept, "linear_intercept", "intercept parameter for 0 value of temperature sensor", false);
+			createValue (slope, "linear_slope", "slope parameter for linear function to fit temperature sensor", false, RTS2_VALUE_WRITABLE);
+			createValue (intercept, "linear_intercept", "intercept parameter for 0 value of temperature sensor", false, RTS2_VALUE_WRITABLE);
 		}
 
 		/**
