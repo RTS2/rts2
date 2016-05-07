50f5d6ff434c313fc642ca349d0bf63419a51a47
Petr Kubánek <petr@kubanek.net>
2016-05-07 10:48:39 -0700

    XMM visibility tests

--- checks/check_tle.cpp
+++ checks/check_tle.cpp
@@ -2,6 +2,10 @@
 #include <check.h>
 #include <check_utils.h>
 
+#include "pluto/norad.h"
+#include "pluto/observe.h"
+#include <libnova/libnova.h>
+
 void setup_tle (void)
 {
 }
@@ -12,7 +16,51 @@ void teardown_tle (void)
 
 START_TEST(XMM)
 {
+	const char *tle1 = "1 25989U 99066A   16126.72024749 -.00000083  00000-0  00000+0 0  9995";
+	const char *tle2 = "2 25989  67.4812  25.2476 8203967  94.8547 359.5975  0.50170988 18843";
+	tle_t tle;
 
+	// test 1, 2016-05-07T19:09:47 CEST = 2016-05-07U17:09:47
+	struct ln_date test_t;
+	test_t.years = 2016;
+	test_t.months = 5;
+	test_t.days = 7;
+	test_t.hours = 17;
+	test_t.minutes = 9;
+	test_t.seconds = 47;
+
+	double JD = ln_get_julian_day (&test_t);
+	ck_assert_dbl_eq (JD, 2457516.215127, 10e-5);
+
+	double longitude = -4.4643;
+	double latitude = 40.4610;
+	double rho_cos;
+	double rho_sin;
+	double observer_loc[3];
+
+	lat_alt_to_parallax (ln_deg_to_rad (latitude), 791, &rho_cos, &rho_sin);
+
+	int ret = parse_elements (tle1, tle2, &tle);
+	ck_assert_int_eq (ret, 0);
+
+	int ephem = 1;
+	int is_deep = select_ephemeris (&tle);
+	if (is_deep && (ephem == 1 || ephem == 2))
+		ephem += 2;	/* switch to an SDx */
+	if (!is_deep && (ephem == 3 || ephem == 4))
+		ephem -= 2;	/* switch to an SGx */
+
+	double sat_pos[3]; /* Satellite position vector */
+	double sat_params[N_SAT_PARAMS];
+
+	observer_cartesian_coords (JD, ln_deg_to_rad (longitude), rho_cos, rho_sin, observer_loc);
+	double t_since = (JD - tle.epoch) * 1440.;
+/*
+Vychází	18:18:40	0°	271° (Z)	18 309	11,6	32,8°
+Dosahuje výšku nad obzorem 10°	18:38:41	10°	286° (ZSZ)	12 988	11,1	29,0°
+Nejvyšší poloha	19:09:47	38°	4° (S)	6 974	8,6	23,1°
+Klesne pod výšku nad obzorem 10°	19:39:02	10°	81° (V)	12 332	9,1	17,6°
+Zapadá	19:55:46	0°	95° (V)	16 852	9,7	14,5° */
 }
 END_TEST
 
--- checks/check_utils.h
+++ checks/check_utils.h
@@ -1,6 +1,8 @@
 #ifndef __CHECK_UTILS__
 #define __CHECK_UTILS__
 
+#include <math.h>
+
 #define ck_assert_dbl_eq(v1,v2,alow)  ck_assert_msg(fabs(v1-v2) < alow, "difference %f and %f > %f", v1, v2, alow)
 
 #endif //!__CHECK_UTILS__
