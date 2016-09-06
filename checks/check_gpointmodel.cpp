ad4d800ba05b2b1949fbc47bc02b22f4067fedfe
Petr Kubánek <petr@kubanek.net>
2016-09-06 18:36:28 +0200

    more gpoint tests

--- checks/check_gpointmodel.cpp
+++ checks/check_gpointmodel.cpp
@@ -12,7 +12,7 @@ void setup_gpoint (void)
 	testGPoint_34.load (iss1);
 
 
-	std::istringstream iss2 ("# test #2\nRTS2_ALTAZ 0.01 0 0 0 -0.02 0 0 0 0\n# extras..\nAZ 0.02 SiN el 2\nEL 0.02 cos az 2");
+	std::istringstream iss2 ("# test #2\nRTS2_ALTAZ 0.017453292519943295 0 0 0 0 -0.03490658503988659 0 0 0\n# extras..\nAZ 0.05235987755982989 SiN el 2\nEL 0.06981317007977318 cos az 2");
 	testGPoint_n39.load (iss2);
 
 }
@@ -24,13 +24,37 @@ void teardown_gpoint (void)
 START_TEST(model_1)
 {
 	struct ln_hrz_posn test_hrz;
-	test_hrz.alt = 25;
-	test_hrz.az = 0;
 	struct ln_hrz_posn test_err;
+
+	test_hrz.az = 0;
+	test_hrz.alt = 45;
 	testGPoint_n39.getErrAltAz (&test_hrz, &test_err);
 
-	ck_assert_dbl_eq (test_hrz.az, -0.959513, 10e-4);
-	ck_assert_dbl_eq (test_hrz.alt, 26.145916, 10e-4);
+	ck_assert_dbl_eq (test_hrz.az, 2, 10e-4);
+	ck_assert_dbl_eq (test_hrz.alt, 51, 10e-4);
+
+	ck_assert_dbl_eq (test_err.az, 2, 10e-4);
+	ck_assert_dbl_eq (test_err.alt, 6, 10e-4);
+
+	test_hrz.az = 90;
+	test_hrz.alt = 45;
+	testGPoint_n39.getErrAltAz (&test_hrz, &test_err);
+
+	ck_assert_dbl_eq (test_hrz.az, 92, 10e-4);
+	ck_assert_dbl_eq (test_hrz.alt, 43, 10e-4);
+
+	ck_assert_dbl_eq (test_err.az, 2, 10e-4);
+	ck_assert_dbl_eq (test_err.alt, -2, 10e-4);
+
+	test_hrz.az = 30;
+	test_hrz.alt = 15;
+	testGPoint_n39.getErrAltAz (&test_hrz, &test_err);
+
+	ck_assert_dbl_eq (test_hrz.az, 30.5, 10e-4);
+	ck_assert_dbl_eq (test_hrz.alt, 19, 10e-4);
+
+	ck_assert_dbl_eq (test_err.az, 0.5, 10e-4);
+	ck_assert_dbl_eq (test_err.alt, 4, 10e-4);
 }
 END_TEST
 
