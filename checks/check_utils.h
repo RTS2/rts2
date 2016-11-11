8fe20454004d0f438a4c5a1974b5af42eb467834
Petr Kubánek <petr@kubanek.net>
2016-11-11 09:20:05 +0100

    macro expansion for double check

--- checks/check_utils.h
+++ checks/check_utils.h
@@ -6,9 +6,7 @@
 #include <check.h>
 #include <check_utils.h>
 
-inline void ck_assert_dbl_eq(long double v1, long double v2, long double alow)
-{
-	ck_assert_msg(fabs(v1-v2) < alow, "difference between %.10Lf and %.10Lf > %.10Lf", v1, v2, alow);
-}
+#define ck_assert_dbl_eq(v1,v2,alow)  ck_assert_msg(fabs(v1-v2) < alow, "difference between %.10f and %.10f > %.10f", v1, v2, alow);
+#define ck_assert_lng_dbl_eq(v1,v2,alow)  ck_assert_msg(fabs(v1-v2) < alow, "difference between %.10f and %.10f > %.10f", v1, v2, alow);
 
 #endif //!__CHECK_UTILS__
