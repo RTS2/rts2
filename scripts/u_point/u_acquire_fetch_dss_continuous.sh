f694302514d3ab8e4bef7fee5fd3e75999b74153
Markus Wildi <wildi.markus@bluewin.ch>
2016-11-13 18:28:30 +0100

    chain completed, initial release

--- scripts/u_point/u_acquire_fetch_dss_continuous.sh
+++ scripts/u_point/u_acquire_fetch_dss_continuous.sh
@@ -4,7 +4,7 @@
 #
 # Helper script used by
 #
-# rts2-scriptexec -d C0 -s " exe ./u_acquire_simulate_continuous.sh "
+# rts2-scriptexec -d C0 -s " exe ./u_acquire_fetch_dss_continuous.sh"
 #
 # exe can not deal with arguments
 #
@@ -13,9 +13,6 @@
 # 
 cd $HOME/rts2/scripts/u_point/
 #
-# fastest demonstration/DEBUG mode, no DSS images retrieval
-#                         
-#./u_acquire.py --level DEBUG --simulate-image --mode-continues  
 #
 # same with DSS image retrieval
 ./u_acquire.py --level DEBUG --mode-continues --fetch-dss-image
