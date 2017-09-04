abc7118685ff46b5230ba340c5404e1776babfa6
Petr Kubanek <petr@rts2.org>
2017-09-04 23:29:54 +0200

    fixed non-erfa check

--- checks/check_tel_corr.cpp
+++ checks/check_tel_corr.cpp
@@ -516,7 +516,7 @@ START_TEST(test_mean2apparent)
 	ck_assert_dbl_eq (pos.ra, 155.2880397611, 10e-7);
 	ck_assert_dbl_eq (pos.dec, 9.8789411273, 10e-7);
 #else
-	ck_assert_dbl_eq (pos.ra, 155.4655661547, 10e-7);
+	ck_assert_dbl_eq (pos.ra, 155.4611594137, 10e-7);
 	ck_assert_dbl_eq (pos.dec, 9.8767953610, 10e-7);
 #endif
 
