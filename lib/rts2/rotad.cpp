16b0222070189a02bc0e66c20e4e14d0ab929410
Petr Kubanek <petr@rts2.org>
2017-09-10 01:40:45 +0200

    take into account offset when computing zenith angle

--- lib/rts2/rotad.cpp
+++ lib/rts2/rotad.cpp
@@ -318,9 +318,9 @@ void Rotator::setPAOffset (double paOff)
 double Rotator::getZenithAngleFromTarget(double angle)
 {
 	if (leftRotator)
-		return angle + 90 - telAltAz->getAlt();
+		return angle + getOffset () + 90 - telAltAz->getAlt();
 	else
-		return angle - 90 + telAltAz->getAlt();
+		return angle + getOffset () - 90 + telAltAz->getAlt();
 }
 
 void Rotator::updateToGo ()
