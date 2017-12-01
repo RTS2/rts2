e8c3fa00d8d9fa1d81736bd6db6a75a462af17b4
Petr Kubánek <petr@rts2.fzu.cz>
2017-12-01 15:54:09 +0100

    removed include for checks

--- checks/check_utils.h
+++ checks/check_utils.h
@@ -4,7 +4,6 @@
 #include <math.h>
 
 #include <check.h>
-#include <check_utils.h>
 
 #define ck_assert_dbl_eq(v1,v2,alow)  ck_assert_msg(fabs((v1)-(v2)) < (alow), "difference between %.10f and %.10f > %.10f", (double)(v1), (double)(v2), (double)(alow));
 //to find out errors..
