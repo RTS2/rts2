a029cc191458f2d9116a4d85532fdb607b4e4ba5
Eduardo Alonso <eduardo.alonso@quasarsr.com>
2023-08-22 14:00:28 +0200

    sql: targets: Don't use 0 for targets with undefined position
    Some target types, such as DarkTarget, will prevent the telescope from moving no matter which coordinates the target specifies. For some other targets, such as target 3 (Focusing Frames) we don't want the telescope to move to 00:00:00 +00:00:00, although it does until the focusing script kicks in:
    
    EXEC executing now target Focusing frames(#3) at RA DEC 00:00:00.000 +00:00:00.00
    T0 moving from (...) to 00:00:00.000 +000:00:00.00 (altaz +026:24:16.86 +176:14:00.00)
    T0 moving from (..) to 23:55:29.804 +063:31:42.24 (altaz +089:00:00.00 +090:00:00.00)
    
    Moreover, I find this behaviour a bit inconsistent, since DarkTarget sets the position to NAN but ConstTarget and CalibrationTarget set it to 0 (which is a valid position in the sky).
    
    In the focusing example I highlighted earlier, it's a OportunityTarget, so it simply moves until the script instructs to move to another position.
    
    Instead of having target 3 as a CalibrationTarget, I propose setting the position of these objects to null, instead of zero.
    
    Signed-off-by: Eduardo Alonso <eduardo.alonso@quasarsr.com>

--- src/sql/data/targets.sql
+++ src/sql/data/targets.sql
@@ -1,14 +1,14 @@
 -- standart targets (tar_id < 100)
 
 COPY targets (tar_id, type_id, tar_name, tar_ra, tar_dec, tar_comment, tar_enabled, tar_priority, tar_bonus, tar_bonus_time) FROM stdin;
-1	d	Dark frames	0	0	Used to produce darks	t	0	0	\N
-2	f	Flat frames	0	0	Used to produce flatfields	t	0	0	\N
-3	o	Focusing frames	0	0	Used to focusc cameras	t	0	0	\N
-4	m	Default model	0	0	Used to model get enought pictures for mount model	t	0	0	\N
-6	c	Master calibration target	0	0	Used to calibration frames	t	0	0	\N
-7	p	Master plan	0	0	Observe by plan	t	1	0	\N
-10	W	Swift FOV	0	0	Swift Field of View, based on GCN	t	0	0	\N
-11	I	Integral FOV	0	0	Integral Field of View, based on GCN	t	0	0	\N
+1	d	Dark frames	\N	\N	Used to produce darks	t	0	0	\N
+2	f	Flat frames	\N	\N	Used to produce flatfields	t	0	0	\N
+3	c	Focusing frames	\N  \N	Used to focusc cameras	t	0	0	\N
+4	m	Default model	\N	\N	Used to model get enought pictures for mount model	t	0	0	\N
+6	c	Master calibration target	\N	\N	Used to calibration frames	t	0	0	\N
+7	p	Master plan	\N	\N	Observe by plan	t	1	0	\N
+10	W	Swift FOV	\N	\N	Swift Field of View, based on GCN	t	0	0	\N
+11	I	Integral FOV	\N	\N	Integral Field of View, based on GCN	t	0	0	\N
 \.
 
 COPY target_model (tar_id, alt_start, alt_stop, alt_step, az_start, az_stop, az_step, noise, step) FROM stdin;
