882a2e2489c3ab0c3d0c10a2e6e75f86da43806d
Eduardo Alonso <eduardo.alonso@quasarsr.com>
2023-10-07 21:45:31 +0200

    python: Fix focusing (#70)
    The __sexFindFWHM was accidentally renamed at ( 3adf817 ), giving as a result, no __sexFindFWHM function and two findBestFWHM functions.
    
    Also, sextractor class is updated for python 3.
    
    Signed-off-by: Eduardo Alonso <eduardo.alonso@quasarsr.com>

--- python/rts2/sextractor.py
+++ python/rts2/sextractor.py
@@ -39,7 +39,7 @@ import traceback
 
 class Sextractor:
 	"""Class for a catalogue (SExtractor result)"""
-	def __init__(self, fields=['NUMBER', 'FLUXERR_ISO', 'FLUX_AUTO', 'X_IMAGE', 'Y_IMAGE', 'MAG_BEST', 'FLAGS', 'CLASS_STAR', 'FWHM_IMAGE', 'A_IMAGE', 'B_IMAGE','EXT_NUMBER'], sexpath='sextractor', sexconfig='/usr/share/sextractor/default.sex', starnnw='/usr/share/sextractor/default.nnw', threshold=2.7, deblendmin = 0.03, saturlevel=65535, verbose=False):
+	def __init__(self, fields=['NUMBER', 'FLUXERR_ISO', 'FLUX_AUTO', 'X_IMAGE', 'Y_IMAGE', 'MAG_BEST', 'FLAGS', 'CLASS_STAR', 'FWHM_IMAGE', 'A_IMAGE', 'B_IMAGE','EXT_NUMBER'], sexpath='sex', sexconfig='/usr/share/sextractor/default.sex', starnnw='/usr/share/sextractor/default.nnw', threshold=2.7, deblendmin = 0.03, saturlevel=65535, verbose=False):
 		self.sexpath = sexpath
 		self.sexconfig = sexconfig
 		self.starnnw = starnnw
@@ -96,11 +96,11 @@ class Sextractor:
 
 	def sortObjects(self,col):
 		"""Sort objects by given collumn."""
-		self.objects.sort(cmp=lambda x,y: cmp(x[col],y[col]))
+		self.objects.sort(key=lambda x: x[col])
 
 	def reverseObjects(self,col):
 		"""Reverse sort objects by given collumn."""
-		self.objects.sort(cmp=lambda x,y: cmp(x[col],y[col]))
+		self.objects.sort(key=lambda x: x[col])
 		self.objects.reverse()
 
 	def filter_galaxies(self,limit=0.2):
@@ -132,7 +132,7 @@ class Sextractor:
 		try:
 			# sort by magnitude
 			i_mag_best = self.get_field('MAG_BEST')
-			obj.sort(cmp=lambda x,y: cmp(x[i_mag_best],y[i_mag_best]))
+			obj.sort(key=lambda x: x[i_mag_best])
 			fwhmlist = []
 
 			a = 0
